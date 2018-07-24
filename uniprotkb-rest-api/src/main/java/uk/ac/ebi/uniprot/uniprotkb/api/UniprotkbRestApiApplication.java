5ad271c2b5131657279a8134ef81ab8eadd381b7
jluo <jluo@ebi.ac.uk>
2018-07-24 17:03:42 +0100

    refactoring modules

---
+++ uniprotkb-rest-api/src/main/java/uk/ac/ebi/uniprot/uniprotkb/api/UniprotkbRestApiApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.uniprot.uniprotkb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniprotkbRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniprotkbRestApiApplication.class, args);
+	}
+}
