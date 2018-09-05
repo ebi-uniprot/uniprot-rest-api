830e7cfc869df7415901a51d43e89ddf06f837a4
Edward Turner <eddturner@ebi.ac.uk>
2018-09-05 11:55:29 +0100

    add allow all origins to advanced search rest

--- advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/AdvancedSearchREST.java
+++ advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/AdvancedSearchREST.java
@@ -2,7 +2,9 @@ package uk.ac.ebi.uniprot.uuw.advanced.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
+import uk.ac.ebi.uniprot.rest.http.HttpCommonHeaderConfig;
 
 /**
  * Starts advanced-search REST application.
@@ -10,9 +12,9 @@ import org.springframework.data.solr.repository.config.EnableSolrRepositories;
  * @author lgonzales
  */
 @SpringBootApplication
-@EnableSolrRepositories(basePackages = { "uk.ac.ebi.uniprot.uuw.advanced.search.repository.impl" })
+@EnableSolrRepositories(basePackages = {"uk.ac.ebi.uniprot.uuw.advanced.search.repository.impl"})
+@Import({HttpCommonHeaderConfig.class})
 public class AdvancedSearchREST {
-
     public static void main(String[] args) {
         SpringApplication.run(AdvancedSearchREST.class, args);
     }
