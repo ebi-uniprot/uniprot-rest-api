296c25d898ec634689d56005ab0c673a2237b530
Edward Turner <eddturner@ebi.ac.uk>
2018-09-05 11:33:20 +0100

    refactored common http headers into common-rest module

--- configure-rest/src/main/java/uk/ac/ebi/uniprot/configure/api/ConfigureApplication.java
+++ configure-rest/src/main/java/uk/ac/ebi/uniprot/configure/api/ConfigureApplication.java
@@ -2,10 +2,12 @@ package uk.ac.ebi.uniprot.configure.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import uk.ac.ebi.uniprot.rest.http.HttpCommonHeaderConfig;
 
 @SpringBootApplication
+@Import({HttpCommonHeaderConfig.class})
 public class ConfigureApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigureApplication.class, args);
 	}
