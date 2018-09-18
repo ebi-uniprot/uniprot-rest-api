76abba69eb85d73dfa79710fbdb32301b393c630
Edward Turner <eddturner@ebi.ac.uk>
2018-09-18 14:37:47 +0100

    formatting

--- advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/repository/RepositoryConfig.java
+++ advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/repository/RepositoryConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.solr.core.SolrTemplate;
 
 /**
- *  Configure spring-data-solr repository beans, that are used to retrieve data from a solr instance.
+ * Configure spring-data-solr repository beans, that are used to retrieve data from a solr instance.
  *
  * @author lgonzales
  */
@@ -29,28 +29,29 @@ public class RepositoryConfig {
         // I am creating HttpClient exactly in the same way it is created inside CloudSolrClient.Builder,
         // but here I am just adding Credentials
         ModifiableSolrParams param = null;
-        if(!config.getUsername().isEmpty() && !config.getPassword().isEmpty()){
+        if (!config.getUsername().isEmpty() && !config.getPassword().isEmpty()) {
             param = new ModifiableSolrParams();
-            param.add(HttpClientUtil.PROP_BASIC_AUTH_USER,config.getUsername());
-            param.add(HttpClientUtil.PROP_BASIC_AUTH_PASS,config.getPassword());
+            param.add(HttpClientUtil.PROP_BASIC_AUTH_USER, config.getUsername());
+            param.add(HttpClientUtil.PROP_BASIC_AUTH_PASS, config.getPassword());
         }
         return HttpClientUtil.createClient(param);
     }
 
     @Bean
-    public SolrClient solrClient(HttpClient httpClient,RepositoryConfigProperties config) {
-        if(!config.getZookeperhost().isEmpty()) {
-            return new CloudSolrClient.Builder().withHttpClient(httpClient).withZkHost(config.getZookeperhost()).build();
-        }else if(!config.getHttphost().isEmpty()){
-            return new HttpSolrClient.Builder().withHttpClient(httpClient).withBaseSolrUrl(config.getHttphost()).build();
-        }else{
+    public SolrClient solrClient(HttpClient httpClient, RepositoryConfigProperties config) {
+        if (!config.getZookeperhost().isEmpty()) {
+            return new CloudSolrClient.Builder().withHttpClient(httpClient).withZkHost(config.getZookeperhost())
+                    .build();
+        } else if (!config.getHttphost().isEmpty()) {
+            return new HttpSolrClient.Builder().withHttpClient(httpClient).withBaseSolrUrl(config.getHttphost())
+                    .build();
+        } else {
             throw new BeanCreationException("make sure your application.properties has eight solr zookeperhost or httphost properties");
         }
     }
 
     @Bean
-    public SolrTemplate solrTemplate(SolrClient solrClient) throws Exception {
+    public SolrTemplate solrTemplate(SolrClient solrClient) {
         return new SolrTemplate(solrClient);
     }
-
 }
--- advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/repository/SolrQueryRepository.java
+++ advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/repository/SolrQueryRepository.java
@@ -23,11 +23,10 @@ import java.util.Optional;
 
 /**
  * Solr Basic Repository class to enable the execution of dynamically build queries in a solr collections.
- *
+ * <p>
  * It was defined a common QueryResult object in order to be able to
  *
  * @param <T> Returned Solr entity
- *
  * @author lgonzales
  */
 public abstract class SolrQueryRepository<T> {
@@ -40,19 +39,19 @@ public abstract class SolrQueryRepository<T> {
     private Class<T> tClass;
     private FacetConfigConverter facetConverter;
 
-    protected SolrQueryRepository(SolrTemplate solrTemplate, SolrCollection collection, Class<T> tClass,FacetConfigConverter facetConverter){
+    protected SolrQueryRepository(SolrTemplate solrTemplate, SolrCollection collection, Class<T> tClass, FacetConfigConverter facetConverter) {
         this.solrTemplate = solrTemplate;
         this.collection = collection;
         this.tClass = tClass;
-        this.facetConverter =facetConverter;
+        this.facetConverter = facetConverter;
     }
 
     public QueryResult<T> searchPage(SimpleQuery query, Long offset, Integer size) {
         try {
-            if(size == null || size <= 0){
+            if (size == null || size <= 0) {
                 size = DEFAULT_PAGE_SIZE;
             }
-            if(offset == null){
+            if (offset == null) {
                 offset = 0L;
             }
 
@@ -61,31 +60,32 @@ public abstract class SolrQueryRepository<T> {
 
             Page<T> result = solrTemplate.queryForPage(collection.toString(), query, tClass);
             PageImpl resultPage = PageImpl.of(size, offset, result.getTotalElements());
-            return QueryResult.of(result.getContent(),resultPage);
-        }catch (Throwable e){
-            throw new QueryRetrievalException("Unexpected error retrieving data from our Repository",e);
-        }finally {
+            return QueryResult.of(result.getContent(), resultPage);
+        } catch (Throwable e) {
+            throw new QueryRetrievalException("Unexpected error retrieving data from our Repository", e);
+        } finally {
             logSolrQuery(query);
         }
     }
-    public QueryResult<T> searchCursorPage(SimpleQuery query, String cursor,Integer pageSize) {
-        if(pageSize == null || pageSize <=0){
+
+    public QueryResult<T> searchCursorPage(SimpleQuery query, String cursor, Integer pageSize) {
+        if (pageSize == null || pageSize <= 0) {
             pageSize = DEFAULT_PAGE_SIZE;
         }
         try {
-            CursorPage page = CursorPage.of(cursor,pageSize);
-            QueryResponse solrResponse = solrTemplate.execute(getSolrCursorCallback(query, page.getCursor(),pageSize));
+            CursorPage page = CursorPage.of(cursor, pageSize);
+            QueryResponse solrResponse = solrTemplate.execute(getSolrCursorCallback(query, page.getCursor(), pageSize));
 
-            List<T> resultList = solrTemplate.convertQueryResponseToBeans(solrResponse,tClass);
+            List<T> resultList = solrTemplate.convertQueryResponseToBeans(solrResponse, tClass);
             page.setNextCursor(solrResponse.getNextCursorMark());
             page.setTotalElements(solrResponse.getResults().getNumFound());
 
             List<Facet> facets = facetConverter.convert(solrResponse);
 
-            return QueryResult.of(resultList,page,facets);
-        }catch (Throwable e){
-            throw new QueryRetrievalException("Unexpected error retrieving data from our Repository",e);
-        }finally {
+            return QueryResult.of(resultList, page, facets);
+        } catch (Throwable e) {
+            throw new QueryRetrievalException("Unexpected error retrieving data from our Repository", e);
+        } finally {
             logSolrQuery(query);
         }
     }
@@ -93,32 +93,31 @@ public abstract class SolrQueryRepository<T> {
     public Optional<T> getEntry(SimpleQuery query) {
         try {
             return solrTemplate.queryForObject(collection.toString(), query, tClass);
-        }catch (Exception e){
-            throw new QueryRetrievalException("Error executing solr query",e);
-        }finally {
+        } catch (Throwable e) {
+            throw new QueryRetrievalException("Error executing solr query", e);
+        } finally {
             logSolrQuery(query);
         }
     }
 
     public Cursor<T> getAll(SimpleQuery query) {
         try {
-            return solrTemplate.queryForCursor(collection.toString(),query,tClass);
-        }catch (Throwable e){
-            throw new QueryRetrievalException("Error executing solr query",e);
-        }finally {
+            return solrTemplate.queryForCursor(collection.toString(), query, tClass);
+        } catch (Throwable e) {
+            throw new QueryRetrievalException("Error executing solr query", e);
+        } finally {
             logSolrQuery(query);
         }
     }
 
-
-    private SolrCallback<QueryResponse> getSolrCursorCallback(SimpleQuery query, String cursor,Integer pageSize) {
+    private SolrCallback<QueryResponse> getSolrCursorCallback(SimpleQuery query, String cursor, Integer pageSize) {
         return solrClient -> {
             DefaultQueryParser queryParser = new DefaultQueryParser();
             SolrQuery solrQuery = queryParser.doConstructSolrQuery(query);
 
-            if(cursor != null && !cursor.isEmpty()) {
+            if (cursor != null && !cursor.isEmpty()) {
                 solrQuery.set(CursorMarkParams.CURSOR_MARK_PARAM, cursor);
-            }else {
+            } else {
                 solrQuery.set(CursorMarkParams.CURSOR_MARK_PARAM, CursorMarkParams.CURSOR_MARK_START);
             }
             solrQuery.setRows(pageSize);
@@ -127,10 +126,10 @@ public abstract class SolrQueryRepository<T> {
         };
     }
 
-    private void logSolrQuery(SimpleQuery query){
-        if(query != null) {
+    private void logSolrQuery(SimpleQuery query) {
+        if (query != null) {
             DefaultQueryParser queryParser = new DefaultQueryParser();
-            LOGGER.debug("solrQuery: " + queryParser.getQueryString(query));
+            LOGGER.debug("solrQuery: {}", queryParser.getQueryString(query));
         }
     }
 
