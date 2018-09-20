4e54ac97932d10f9e084c59cd16dbf7be1e3c9da
Edward Turner <eddturner@ebi.ac.uk>
2018-09-20 14:22:20 +0100

    rearrange class

--- advanced-search-rest/src/test/java/uk/ac/ebi/uniprot/uuw/advanced/search/repository/DataStoreManager.java
+++ advanced-search-rest/src/test/java/uk/ac/ebi/uniprot/uuw/advanced/search/repository/DataStoreManager.java
@@ -24,7 +24,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @author Edd
  */
 public class DataStoreManager {
-    public enum StoreType {
+    enum StoreType {
         UNIPROT, UNIPARC, UNIREF
     }
 
