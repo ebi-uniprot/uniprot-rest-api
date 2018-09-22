44392e2196fc20466a372bd12b95e78452acd6bf
eddturner <eddturner@gmai.com>
2018-09-22 14:17:34 +0100

    single example test showing how to setup a controller test

--- advanced-search-rest/src/test/java/uk/ac/ebi/uniprot/uuw/advanced/search/controller/UniprotAdvancedSearchControllerIT.java
+++ advanced-search-rest/src/test/java/uk/ac/ebi/uniprot/uuw/advanced/search/controller/UniprotAdvancedSearchControllerIT.java
@@ -8,27 +8,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
 import uk.ac.ebi.uniprot.uuw.advanced.search.AdvancedSearchREST;
 import uk.ac.ebi.uniprot.uuw.advanced.search.mockers.UniProtEntryMocker;
 import uk.ac.ebi.uniprot.uuw.advanced.search.repository.DataStoreManager;
 import uk.ac.ebi.uniprot.uuw.advanced.search.repository.DataStoreTestConfig;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static uk.ac.ebi.uniprot.uuw.advanced.search.http.converter.FlatFileMessageConverter.FF_MEDIA_TYPE;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {AdvancedSearchREST.class,
-                           DataStoreTestConfig.class}, properties = "spring.http.encoding.enabled=false")
+@SpringBootTest(classes = {DataStoreTestConfig.class, AdvancedSearchREST.class})
 @WebAppConfiguration
 public class UniprotAdvancedSearchControllerIT {
+    private static final String BASE_RESOURCE = "/uniprot";
+    private static final String ACCESSION_RESOURCE = BASE_RESOURCE + "/accession/";
     @Autowired
     private DataStoreManager storeManager;
 
@@ -36,7 +37,7 @@ public class UniprotAdvancedSearchControllerIT {
     private WebApplicationContext webApplicationContext;
 
     private MockMvc mockMvc;
-
+    
     @Before
     public void setUp() {
         mockMvc = MockMvcBuilders.
@@ -45,21 +46,20 @@ public class UniprotAdvancedSearchControllerIT {
     }
 
     @Test
-    public void canReachDownloadEndpoint() throws Exception {
-        storeManager.save(DataStoreManager.StoreType.UNIPROT, UniProtEntryMocker.create(UniProtEntryMocker.Type.SP));
+    public void canReachAccessionEndpoint() throws Exception {
+        // given
+        UniProtEntry entry = UniProtEntryMocker.create(UniProtEntryMocker.Type.SP);
+        String acc = entry.getPrimaryUniProtAccession().getValue();
+        storeManager.save(DataStoreManager.StoreType.UNIPROT, entry);
 
+        // when
         ResultActions response = mockMvc.perform(
-                get("/uniprot/download?query=accession:Q8DIA7")
-                        .header(ACCEPT, FF_MEDIA_TYPE));
+                get(ACCESSION_RESOURCE + acc)
+                        .header(ACCEPT, APPLICATION_JSON_VALUE));
 
-        response.andExpect(
-                request().asyncStarted())
-                .andDo(MvcResult::getAsyncResult)
-                .andDo(print())
-                .andExpect(content().contentType(FF_MEDIA_TYPE))
-                .andExpect(content().string(containsString("AC   Q8DIA7;")))
-                .andExpect(header().stringValues(VARY, ACCEPT, ACCEPT_ENCODING))
-                .andExpect(header().exists(CONTENT_DISPOSITION));
-
+        // then
+        response.andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON_VALUE))
+                .andExpect(content().string(containsString("\"accession\":\"" + acc + "\"")));
     }
 }
