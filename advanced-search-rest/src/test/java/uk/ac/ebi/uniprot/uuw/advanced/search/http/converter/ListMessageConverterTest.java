2c85f441b81e301018650e40ea4488a5b64fbeda
Edward Turner <eddturner@ebi.ac.uk>
2018-09-27 11:39:46 +0100

    ff & list tests

--- advanced-search-rest/src/test/java/uk/ac/ebi/uniprot/uuw/advanced/search/http/converter/ListMessageConverterTest.java
+++ advanced-search-rest/src/test/java/uk/ac/ebi/uniprot/uuw/advanced/search/http/converter/ListMessageConverterTest.java
@@ -5,52 +5,47 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import uk.ac.ebi.kraken.ffwriter.line.impl.UniProtFlatfileWriter;
-import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
 import uk.ac.ebi.uniprot.uuw.advanced.search.http.context.MessageConverterContext;
-import uk.ac.ebi.uniprot.uuw.advanced.search.mockers.UniProtEntryMocker;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.Instant;
-import java.util.Collection;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
-import static java.util.Collections.singletonList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static uk.ac.ebi.uniprot.uuw.advanced.search.mockers.UniProtEntryMocker.Type.SP;
 
 /**
- * Created 25/09/18
+ * Created 21/09/18
  *
  * @author Edd
  */
 @RunWith(MockitoJUnitRunner.Silent.class)
-public class FlatFileMessageConverterTest {
+public class ListMessageConverterTest {
+
     @Mock
     private MessageConverterContext context;
 
     @Mock
     private OutputStream outputStream;
 
-    private FlatFileMessageConverter converter;
+    private ListMessageConverter converter;
     private Instant start;
     private AtomicInteger count;
 
     @Before
     public void setUp() {
-        this.converter = new FlatFileMessageConverter();
+        this.converter = new ListMessageConverter();
         this.start = Instant.now();
         this.count = new AtomicInteger();
     }
 
     @Test
     public void convertsEntrySuccessfully() throws IOException {
-        UniProtEntry entry = UniProtEntryMocker.create(SP);
-        Stream<Collection<UniProtEntry>> entries = Stream.of(singletonList(entry));
-        when((Stream<Collection<UniProtEntry>>)context.getEntities()).thenReturn(entries);
+        String entry = "P12345";
+        Stream<String> entries = Stream.of(entry);
+        when((Stream<String>)context.getEntities()).thenReturn(entries);
 
         doConversion();
 
@@ -67,12 +62,11 @@ public class FlatFileMessageConverterTest {
         verify(outputStream).close();
     }
 
-    private void doConversion() throws IOException {
-        converter.write(context, outputStream, start, count);
+    private byte[] bytesFor(String entry) {
+        return (entry + "\n").getBytes();
     }
 
-    private byte[] bytesFor(UniProtEntry entry) {
-        String entryStr = UniProtFlatfileWriter.write(entry) + "\n";
-        return entryStr.getBytes();
+    private void doConversion() throws IOException {
+        converter.write(context, outputStream, start, count);
     }
 }
