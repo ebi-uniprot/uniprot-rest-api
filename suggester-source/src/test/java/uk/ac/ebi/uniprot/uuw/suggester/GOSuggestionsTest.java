3130736747b2e55d90e497461d60678292a025f3
Edward Turner <eddturner@ebi.ac.uk>
2018-10-04 16:58:55 +0100

    update tests

--- suggester-source/src/test/java/uk/ac/ebi/uniprot/uuw/suggester/GOSuggestionsTest.java
+++ suggester-source/src/test/java/uk/ac/ebi/uniprot/uuw/suggester/GOSuggestionsTest.java
@@ -3,15 +3,15 @@ package uk.ac.ebi.uniprot.uuw.suggester;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.ac.ebi.uniprot.uuw.suggester.model.Suggestion;
 
-import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 
 /**
  * Created 03/10/18
@@ -21,13 +21,12 @@ import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class GOSuggestionsTest {
     private GOSuggestions goSuggestions;
-
-    @Mock
-    private PrintWriter out;
+    private Set<String> suggestionsSet;
 
     @Before
     public void setUp() {
         this.goSuggestions = new GOSuggestions();
+        suggestionsSet = new HashSet<>();
     }
 
     @Test
@@ -36,18 +35,32 @@ public class GOSuggestionsTest {
         String goName = "iron-sulfur cluster binding";
         String input = "A0A007    DR   GO; " + goId + "; F:" + goName + "; IEA:InterPro.";
 
-        goSuggestions.process(input, out);
+        goSuggestions.process(input, suggestionsSet);
 
         String suggestionLine = Suggestion.builder().id(goId).name(goName).build().toSuggestionLine();
-        verify(out, times(1)).println(suggestionLine);
+        assertThat(suggestionsSet, contains(suggestionLine));
     }
 
     @Test
     public void givenGOLineWithWrongDB_whenProcess_thenGenerateNothing() {
         String input = "A0A007   DR   GO; " + "ECO:0051536" + "; F:" + "iron-sulfur cluster binding" + "; IEA:InterPro.";
 
-        goSuggestions.process(input, out);
+        goSuggestions.process(input, suggestionsSet);
 
-        verify(out, times(0)).println(anyString());
+        assertThat(suggestionsSet, hasSize(0));
+    }
+
+    @Test
+    public void givenDuplicateGOLines_whenProcess_thenGenerateOnly1Suggestion() {
+        String goId = "GO:0051536";
+        String goName = "iron-sulfur cluster binding";
+        String input1 = "AAAAAA    DR   GO; " + goId + "; F:" + goName + "; IEA:InterPro.";
+        String input2 = "ZZZZZZ    DR   GO; " + goId + "; F:" + goName + "; IEA:InterPro.";
+
+        goSuggestions.process(input1, suggestionsSet);
+        goSuggestions.process(input2, suggestionsSet);
+
+        String suggestionLine = Suggestion.builder().id(goId).name(goName).build().toSuggestionLine();
+        assertThat(suggestionsSet, contains(suggestionLine));
     }
 }
