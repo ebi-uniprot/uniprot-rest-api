4ea28b54ee177f2f55f97872f61ca8e0ab031ad2
Edward Turner <eddturner@ebi.ac.uk>
2018-10-15 14:00:50 +0100

    refactor to allow configurable endpoint in accession retrieval (not hardcoded) -- since in practice, the endpoints keep changing

--- benchmark-rest/src/test/scala/advanced-search/accession/AccessionRetrievalSimulation.scala
+++ benchmark-rest/src/test/scala/advanced-search/accession/AccessionRetrievalSimulation.scala
@@ -18,25 +18,25 @@ object AccessionRetrievalSimulation {
     .doNotTrackHeader("1")
 
   object AccessionScenario {
-    val feeder = tsv(conf.getString("a.s.accession.retrieval.list")).random
+    val feeder = csv(conf.getString("a.s.accession.retrieval.list")).random
 
-    def getRequestWithFormat(format: String): ChainBuilder = {
-      val httpReqInfo: String = "accession=${accession}, format=" + format;
-      val requestStr: String = "/searchAccession?query=${accession}";
+    def getRequest(): ChainBuilder = {
+      val httpReqInfo: String = "url=${url}, format=${format}";
+      val requestStr: String = "${url}";
 
       val request =
         feed(feeder)
           .pause(5 seconds, 15 seconds)
           .exec(http(httpReqInfo)
             .get(requestStr)
-            .header("Accept", format)
+            .header("Accept", "${format}")
           )
 
       return request
     }
 
     val requestSeq = Seq(
-      AccessionScenario.getRequestWithFormat("application/json")
+      AccessionScenario.getRequest()
     )
 
     val instance = scenario("Accession Retrieval Scenario")
