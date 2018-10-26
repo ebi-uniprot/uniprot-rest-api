e66451cd56a5e563bbe589b682f7ceff608363f3
lgonzales <lgonzales@ebi.ac.uk>
2018-10-26 13:34:14 +0100

    - Add Integration tests for ValidReturnFields Constraint Validator

--- advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/model/request/SearchRequestDTO.java
+++ advanced-search-rest/src/main/java/uk/ac/ebi/uniprot/uuw/advanced/search/model/request/SearchRequestDTO.java
@@ -1,15 +1,16 @@
 package uk.ac.ebi.uniprot.uuw.advanced.search.model.request;
 
 import lombok.Data;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-
 import uk.ac.ebi.uniprot.dataservice.client.uniprot.UniProtField;
+import uk.ac.ebi.uniprot.uuw.advanced.search.validation.ValidReturnFields;
 import uk.ac.ebi.uniprot.uuw.advanced.search.validation.ValidSolrQueryFields;
 import uk.ac.ebi.uniprot.uuw.advanced.search.validation.ValidSolrQuerySyntax;
 import uk.ac.ebi.uniprot.uuw.advanced.search.validation.ValidSolrSortFields;
+import uk.ac.ebi.uniprot.uuw.advanced.search.validation.validator.impl.UniprotReturnFieldsValidator;
 import uk.ac.ebi.uniprot.uuw.advanced.search.validation.validator.impl.UniprotSolrQueryFieldValidator;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 /**
  * Search cursor request Entity
@@ -25,6 +26,7 @@ public class SearchRequestDTO {
     @ValidSolrQueryFields(fieldValidatorClazz = UniprotSolrQueryFieldValidator.class)
     private String query;
 
+    @ValidReturnFields(fieldValidatorClazz = UniprotReturnFieldsValidator.class)
     private String fields;
 
     @ValidSolrSortFields(sortFieldEnumClazz = UniProtField.Sort.class)
