4844873d736fddb5d61b60a8cd1a13bdc477bcba
jluo <jluo@ebi.ac.uk>
2019-03-29 11:32:38 +0000

    fix for test

--- common/src/test/java/uk/ac/ebi/uniprot/api/common/repository/search/mockers/PathwayRepoMocker.java
+++ common/src/test/java/uk/ac/ebi/uniprot/api/common/repository/search/mockers/PathwayRepoMocker.java
@@ -5,7 +5,6 @@ import uk.ac.ebi.uniprot.dataservice.source.impl.pathway.PathwayRepo;
 
 public class PathwayRepoMocker {
 	public static PathwayRepo getPathwayRepo() {
-		   String filePath= Thread.currentThread().getContextClassLoader().getResource("unipathway.txt").getFile();
-		   return new PathwayFileRepo(filePath);
+		return new PathwayFileRepo("unipathway.txt");
 	}
 }
