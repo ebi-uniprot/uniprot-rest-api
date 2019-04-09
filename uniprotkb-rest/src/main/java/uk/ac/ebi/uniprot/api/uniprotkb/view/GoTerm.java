e58c404a54b86d35266aa6649acda97b73ce271d
jluo <jluo@ebi.ac.uk>
2019-04-09 15:14:37 +0100

    goService is using QuckGoService to get children

---
+++ uniprotkb-rest/src/main/java/uk/ac/ebi/uniprot/api/uniprotkb/view/GoTerm.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.uniprot.api.uniprotkb.view;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class GoTerm {
+	private String id;
+	private String name;
+	private List<GoRelation> children;
+}
---
+++ uniprotkb-rest/src/main/java/uk/ac/ebi/uniprot/api/uniprotkb/view/GoTermResult.java
@@ -0,0 +1,10 @@
+package uk.ac.ebi.uniprot.api.uniprotkb.view;
+
+import java.util.List;
+
+import lombok.Data;
+@Data
+public class GoTermResult {
+	private List<GoTerm> results;
+	
+}
