497ce98f410df4efaf824e5e9f304037a4fc502c
lgonzales <lgonzales@ebi.ac.uk>
2019-05-23 14:20:29 +0100

    - fix error in boost field

--- uniprotkb-rest/src/main/java/uk/ac/ebi/uniprot/api/uniprotkb/configuration/UniprotKBConfig.java
+++ uniprotkb-rest/src/main/java/uk/ac/ebi/uniprot/api/uniprotkb/configuration/UniprotKBConfig.java
@@ -2,17 +2,25 @@ package uk.ac.ebi.uniprot.api.uniprotkb.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import uk.ac.ebi.uniprot.search.DefaultSearchHandler;
+import uk.ac.ebi.uniprot.search.field.SearchField;
 import uk.ac.ebi.uniprot.search.field.UniProtField;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class UniprotKBConfig {
 
     @Bean
     public DefaultSearchHandler defaultSearchHandler(){
+        List<SearchField> boostFields = Arrays.stream(UniProtField.Search.values())
+                .filter(UniProtField.Search::hasBoostValue)
+                .collect(Collectors.toList());
+
         return new DefaultSearchHandler(UniProtField.Search.content,
                 UniProtField.Search.accession
-                ,UniProtField.getBoostFields());
+                ,boostFields);
     }
 }
