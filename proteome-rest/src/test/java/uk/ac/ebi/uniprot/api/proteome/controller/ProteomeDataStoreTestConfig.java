fa0fd714a6d7bd124de7ce29bf78dd74f7adf7e9
jluo <jluo@ebi.ac.uk>
2019-06-19 13:41:30 +0100

    fix the test case config

--- proteome-rest/src/test/java/uk/ac/ebi/uniprot/api/proteome/controller/ProteomeDataStoreTestConfig.java
+++ proteome-rest/src/test/java/uk/ac/ebi/uniprot/api/proteome/controller/ProteomeDataStoreTestConfig.java
@@ -7,11 +7,14 @@ import java.net.URISyntaxException;
 
 import org.apache.http.client.HttpClient;
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
+import uk.ac.ebi.uniprot.api.common.repository.search.SolrRequest;
+import uk.ac.ebi.uniprot.api.common.repository.search.SolrRequestConverter;
 import uk.ac.ebi.uniprot.indexer.ClosableEmbeddedSolrClient;
 import uk.ac.ebi.uniprot.indexer.DataStoreManager;
 import uk.ac.ebi.uniprot.indexer.SolrDataStoreManager;
@@ -47,14 +50,22 @@ public class ProteomeDataStoreTestConfig {
         return solrClient;
     }
     
-//    @Bean("genecentric")
-//    @Profile("offline")
-//    public SolrClient genecentricSolrClient(DataStoreManager dataStoreManager) throws URISyntaxException {
-//        ClosableEmbeddedSolrClient solrClient = new ClosableEmbeddedSolrClient(SolrCollection.genecentric);
-//        addStoreInfoProteome(dataStoreManager, solrClient,  DataStoreManager.StoreType.GENECENTRIC );
-//        return solrClient;
-//    }
+    @Bean
+    @Profile("proteome_offline")
+    public SolrRequestConverter requestConverter() {
+        return new SolrRequestConverter() {
+            @Override
+            public SolrQuery toSolrQuery(SolrRequest request) {
+                SolrQuery solrQuery = super.toSolrQuery(request);
 
+                // required for tests, because EmbeddedSolrServer is not sharded
+                solrQuery.setParam("distrib", "false");
+                solrQuery.setParam("terms.mincount", "1");
+
+                return solrQuery;
+            }
+        };
+    }
     private void addStoreInfoProteome(DataStoreManager dsm, ClosableEmbeddedSolrClient solrClient, DataStoreManager.StoreType store ) throws URISyntaxException {
         dsm.addSolrClient(store, solrClient);
     }
