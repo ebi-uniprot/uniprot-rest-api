1dbd5f95ec955676c09762d8b152b1cfbe271efa
jluo <jluo@ebi.ac.uk>
2019-06-21 11:18:33 +0100

    updated  message converter for genecentric and proteome

--- proteome-rest/src/main/java/uk/ac/ebi/uniprot/api/proteome/output/converter/GeneCentricXmlMessageConverter.java
+++ proteome-rest/src/main/java/uk/ac/ebi/uniprot/api/proteome/output/converter/GeneCentricXmlMessageConverter.java
@@ -3,26 +3,26 @@ package uk.ac.ebi.uniprot.api.proteome.output.converter;
 import javax.xml.bind.Marshaller;
 
 import uk.ac.ebi.uniprot.api.rest.output.converter.AbstractXmlMessageConverter;
-import uk.ac.ebi.uniprot.domain.proteome.ProteomeEntry;
-import uk.ac.ebi.uniprot.xml.jaxb.proteome.Proteome;
-import uk.ac.ebi.uniprot.xml.proteome.ProteomeConverter;
+import uk.ac.ebi.uniprot.domain.proteome.CanonicalProtein;
+import uk.ac.ebi.uniprot.xml.jaxb.proteome.CanonicalGene;
+import uk.ac.ebi.uniprot.xml.proteome.CanonicalProteinConverter;
 
 /**
  *
  * @author jluo
- * @date: 29 Apr 2019
+ * @date: 21 Jun 2019
  *
- */
+*/
 
-public class ProteomeXmlMessageConverter extends AbstractXmlMessageConverter<ProteomeEntry, Proteome> {
-	private final ProteomeConverter converter;
+public class GeneCentricXmlMessageConverter  extends AbstractXmlMessageConverter<CanonicalProtein, CanonicalGene> {
+	private final CanonicalProteinConverter converter;
 	private final Marshaller marshaller;
 	private static final String XML_CONTEXT = "uk.ac.ebi.uniprot.xml.jaxb.proteome";
 	private static final String HEADER = "<uniprot xmlns=\"http://uniprot.org/uniprot\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://uniprot.org/uniprot http://www.uniprot.org/support/docs/proteome.xsd\">\n";
 
-	public ProteomeXmlMessageConverter() {
-		super(ProteomeEntry.class);
-		converter = new ProteomeConverter();
+	public GeneCentricXmlMessageConverter() {
+		super(CanonicalProtein.class);
+		converter = new CanonicalProteinConverter();
 		marshaller = createMarshaller(XML_CONTEXT);
 	}
 
@@ -32,15 +32,12 @@ public class ProteomeXmlMessageConverter extends AbstractXmlMessageConverter<Pro
 	}
 
 	@Override
-	protected Proteome toXml(ProteomeEntry entity) {
-		
-			return converter.toXml( entity);
-
+	protected CanonicalGene toXml(CanonicalProtein entity) {
+			return converter.toXml(entity);
 	}
 
 	@Override
 	protected Marshaller getMarshaller() {
 		return marshaller;
 	}
-
 }
