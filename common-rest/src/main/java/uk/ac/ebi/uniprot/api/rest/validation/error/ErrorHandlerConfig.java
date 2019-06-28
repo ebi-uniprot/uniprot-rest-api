562e5a430aa32a5164594f645c9718f1dacc9020
jluo <jluo@ebi.ac.uk>
2019-06-28 09:44:32 +0100

    common message test

--- common-rest/src/main/java/uk/ac/ebi/uniprot/api/rest/validation/error/ErrorHandlerConfig.java
+++ common-rest/src/main/java/uk/ac/ebi/uniprot/api/rest/validation/error/ErrorHandlerConfig.java
@@ -27,7 +27,7 @@ public class ErrorHandlerConfig {
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
-        bean.addBasenames("classpath:org.hibernate.validator.ValidationMessages", "classpath:message");
+        bean.addBasenames("classpath:org.hibernate.validator.ValidationMessages", "classpath:message", "classpath:common-message.properties");
         bean.setDefaultEncoding("UTF-8");
         return bean;
     }
