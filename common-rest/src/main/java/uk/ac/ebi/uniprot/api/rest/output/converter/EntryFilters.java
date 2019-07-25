22e436e156e2e8c7da0dc8ea0f00f5509006a5d7
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-07-25 09:50:35 +0100

    Use new jason converter

--- common-rest/src/main/java/uk/ac/ebi/uniprot/api/rest/output/converter/EntryFilters.java
+++ common-rest/src/main/java/uk/ac/ebi/uniprot/api/rest/output/converter/EntryFilters.java
@@ -5,125 +5,126 @@ import uk.ac.ebi.uniprot.domain.uniprot.builder.UniProtEntryBuilder;
 import uk.ac.ebi.uniprot.domain.uniprot.comment.Comment;
 import uk.ac.ebi.uniprot.domain.uniprot.feature.Feature;
 import uk.ac.ebi.uniprot.domain.uniprot.xdb.UniProtDBCrossReference;
+import uk.ac.ebi.uniprot.search.field.UniProtField;
 
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
 public class EntryFilters {
-	private static final String ALL = "all";
+    private static final String ALL = "all";
 
 
-	public static UniProtEntry filterEntry(UniProtEntry entry, Map<String, List<String>> filterParams) {
-		if((filterParams !=null ) && !filterParams.isEmpty()) {
-			UniProtEntryBuilder.ActiveEntryBuilder builder = new UniProtEntryBuilder().from(entry);
-			for (FilterComponentType component : FilterComponentType.values()) {
-				if (!filterParams.containsKey(component.name().toLowerCase())) {
-					remove(builder, component);
-				} else if (component == FilterComponentType.COMMENT) {
-					List<String> values = filterParams.get(component.name().toLowerCase());
-					Predicate<Comment> filter = createCommentFilter(values);
-					List<Comment> comments = entry.getComments();
-					comments.removeIf(comment -> !filter.test(comment));
-					builder.comments(comments);
-				} else if (component == FilterComponentType.FEATURE) {
-					List<String> values = filterParams.get(component.name().toLowerCase());
-					Predicate<Feature> filter = createFeatureFilter(values);
-					List<Feature> features = entry.getFeatures();
-					features.removeIf(feature -> !filter.test(feature));
-					builder.features(features);
-				} else if (component == FilterComponentType.XREF) {
-					List<String> values = filterParams.get(component.name().toLowerCase());
-					Predicate<UniProtDBCrossReference> filter = createDbReferenceFilter(values);
-					List<UniProtDBCrossReference> crossReferences = entry.getDatabaseCrossReferences();
-					crossReferences.removeIf(xref -> !filter.test(xref));
-					builder.databaseCrossReferences(crossReferences);
-				}
-			}
-			return builder.build();
-		}else {
-			return entry;
-		}
-	}
-	
-	
-	public static Predicate<UniProtDBCrossReference> createDbReferenceFilter(List<String> values) {
-		return v -> createXrefPredicate(v, values);
-	}
+    public static UniProtEntry filterEntry(UniProtEntry entry, Map<String, List<String>> filterParams) {
+        if ((filterParams != null) && !filterParams.isEmpty()) {
+            UniProtEntryBuilder.ActiveEntryBuilder builder = new UniProtEntryBuilder().from(entry);
+            for (UniProtField.ResultFields component : UniProtField.ResultFields.values()) {
+                if (component.isMandatoryJsonField() == false && !filterParams.containsKey(component.name())) {
+                    remove(builder, component);
+                } else if (component == UniProtField.ResultFields.comment) {
+                    List<String> values = filterParams.get(component.name().toLowerCase());
+                    Predicate<Comment> filter = createCommentFilter(values);
+                    List<Comment> comments = entry.getComments();
+                    comments.removeIf(comment -> !filter.test(comment));
+                    builder.comments(comments);
+                } else if (component == UniProtField.ResultFields.feature) {
+                    List<String> values = filterParams.get(component.name().toLowerCase());
+                    Predicate<Feature> filter = createFeatureFilter(values);
+                    List<Feature> features = entry.getFeatures();
+                    features.removeIf(feature -> !filter.test(feature));
+                    builder.features(features);
+                } else if (component == UniProtField.ResultFields.xref) {
+                    List<String> values = filterParams.get(component.name().toLowerCase());
+                    Predicate<UniProtDBCrossReference> filter = createDbReferenceFilter(values);
+                    List<UniProtDBCrossReference> crossReferences = entry.getDatabaseCrossReferences();
+                    crossReferences.removeIf(xref -> !filter.test(xref));
+                    builder.databaseCrossReferences(crossReferences);
+                }
+            }
+            return builder.build();
+        } else {
+            return entry;
+        }
+    }
 
-	private static boolean createXrefPredicate(UniProtDBCrossReference v, List<String> values) {
-		if (values.contains(ALL)) {
-			return true;
-		}
-		return values.contains(v.getDatabaseType().getName().toLowerCase());
-	}
 
-	public static Predicate<Feature> createFeatureFilter(List<String> values) {
-		return (Feature v) -> createFeaturePredicate(v, values);
-	}
+    public static Predicate<UniProtDBCrossReference> createDbReferenceFilter(List<String> values) {
+        return v -> createXrefPredicate(v, values);
+    }
 
-	private static boolean createFeaturePredicate(Feature v, List<String> values) {
-		if (values.contains(ALL)) {
-			return true;
-		}
-		return values.contains(v.getType().name().toLowerCase());
-	}
+    private static boolean createXrefPredicate(UniProtDBCrossReference v, List<String> values) {
+        if (values.contains(ALL)) {
+            return true;
+        }
+        return values.contains(v.getDatabaseType().getName().toLowerCase());
+    }
 
-	public static Predicate<Comment> createCommentFilter(List<String> values) {
-		return (Comment v) -> createCommentPredicate(v, values);
-	}
+    public static Predicate<Feature> createFeatureFilter(List<String> values) {
+        return (Feature v) -> createFeaturePredicate(v, values);
+    }
 
-	private static boolean createCommentPredicate(Comment v, List<String> values) {
-		if (values.contains(ALL)) {
-			return true;
-		}
-		return values.contains(v.getCommentType().name().toLowerCase());
-	}
+    private static boolean createFeaturePredicate(Feature v, List<String> values) {
+        if (values.contains(ALL)) {
+            return true;
+        }
+        return values.contains(v.getType().name().toLowerCase());
+    }
 
-	private static void remove(UniProtEntryBuilder.ActiveEntryBuilder builder, FilterComponentType type) {
-		switch (type) {
-		case PROTEIN_EXISTENCE:
-			builder.proteinExistence(null);
-			break;
-		case SECONDARY_ACCESSION:
-			builder.secondaryAccessions(null);
-			break;
-		case PROTEIN_NAME:
-			builder.proteinDescription(null);
-			break;
-		case GENE:
-			builder.genes(null);
-			break;
-		case ORGANISM:
-			builder.organism(null);
-			break;
-		case ORGANISM_HOST:
-			builder.organismHosts(null);
-			break;
-		case GENE_LOCATION:
-			builder.geneLocations(null);
-			break;
-		case COMMENT:
-			builder.comments(null);
-			break;
-		case KEYWORD:
-			builder.keywords(null);
-			break;
-		case FEATURE:
-			builder.features(null);
-			break;
-		case SEQUENCE:
-			builder.sequence(null);
-			break;
-		case XREF:
-			builder.databaseCrossReferences(null);
-			break;
-		case REFERENCE:
-			builder.references(null);
-			break;
-		default:
-			break;
-		}
-	}
+    public static Predicate<Comment> createCommentFilter(List<String> values) {
+        return (Comment v) -> createCommentPredicate(v, values);
+    }
+
+    private static boolean createCommentPredicate(Comment v, List<String> values) {
+        if (values.contains(ALL)) {
+            return true;
+        }
+        return values.contains(v.getCommentType().name().toLowerCase());
+    }
+
+    private static void remove(UniProtEntryBuilder.ActiveEntryBuilder builder, UniProtField.ResultFields type) {
+        switch (type) {
+            case protein_existence:
+                builder.proteinExistence(null);
+                break;
+            case secondary_accession:
+                builder.secondaryAccessions(null);
+                break;
+            case protein_name:
+                builder.proteinDescription(null);
+                break;
+            case gene:
+                builder.genes(null);
+                break;
+            case organism:
+                builder.organism(null);
+                break;
+            case organism_host:
+                builder.organismHosts(null);
+                break;
+            case gene_location:
+                builder.geneLocations(null);
+                break;
+            case comment:
+                builder.comments(null);
+                break;
+            case keyword:
+                builder.keywords(null);
+                break;
+            case feature:
+                builder.features(null);
+                break;
+            case sequence:
+                builder.sequence(null);
+                break;
+            case xref:
+                builder.databaseCrossReferences(null);
+                break;
+            case reference:
+                builder.references(null);
+                break;
+            default:
+                break;
+        }
+    }
 
 }
