45e4cebb4e92836b6163aeb0206247c01c69e0bd
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-08-20 13:47:43 +0100

    Initial changes to generate OAS3

--- support-data-rest/src/main/java/org/uniprot/api/keyword/KeywordController.java
+++ support-data-rest/src/main/java/org/uniprot/api/keyword/KeywordController.java
@@ -1,17 +1,8 @@
 package org.uniprot.api.keyword;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.uniprot.api.rest.output.UniProtMediaType.*;
-import static org.uniprot.api.rest.output.context.MessageConverterContextFactory.Resource.KEYWORD;
-
-import java.util.Optional;
-import java.util.stream.Stream;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
-
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Extension;
+import io.swagger.annotations.ExtensionProperty;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.MediaType;
@@ -30,6 +21,17 @@ import org.uniprot.api.rest.validation.ValidReturnFields;
 import org.uniprot.core.cv.keyword.KeywordEntry;
 import org.uniprot.store.search.field.KeywordField;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.uniprot.api.rest.output.UniProtMediaType.*;
+import static org.uniprot.api.rest.output.context.MessageConverterContextFactory.Resource.KEYWORD;
+
 @RestController
 @RequestMapping("/keyword")
 @Validated
@@ -38,77 +40,59 @@ public class KeywordController extends BasicSearchController<KeywordEntry> {
     private final KeywordService keywordService;
     private static final String KEYWORD_ID_REGEX = "^KW-[0-9]{4}";
 
-    public KeywordController(
-            ApplicationEventPublisher eventPublisher,
-            KeywordService keywordService,
-            @Qualifier("keywordMessageConverterContextFactory")
-                    MessageConverterContextFactory<KeywordEntry>
-                            keywordMessageConverterContextFactory,
-            ThreadPoolTaskExecutor downloadTaskExecutor) {
+    public KeywordController(ApplicationEventPublisher eventPublisher, KeywordService keywordService,
+                             @Qualifier("keywordMessageConverterContextFactory") MessageConverterContextFactory<KeywordEntry> keywordMessageConverterContextFactory,
+                             ThreadPoolTaskExecutor downloadTaskExecutor) {
         super(eventPublisher, keywordMessageConverterContextFactory, downloadTaskExecutor, KEYWORD);
         this.keywordService = keywordService;
     }
 
-    @GetMapping(
-            value = "/{keywordId}",
-            produces = {
-                TSV_MEDIA_TYPE_VALUE,
-                LIST_MEDIA_TYPE_VALUE,
-                APPLICATION_JSON_VALUE,
-                XLS_MEDIA_TYPE_VALUE
-            })
-    public ResponseEntity<MessageConverterContext<KeywordEntry>> getById(
-            @PathVariable("keywordId")
-                    @Pattern(
-                            regexp = KEYWORD_ID_REGEX,
-                            flags = {Pattern.Flag.CASE_INSENSITIVE},
-                            message = "{search.keyword.invalid.id}")
-                    String keywordId,
-            @ValidReturnFields(fieldValidatorClazz = KeywordField.ResultFields.class)
-                    @RequestParam(value = "fields", required = false)
-                    String fields,
-            @RequestHeader(value = "Accept", defaultValue = APPLICATION_JSON_VALUE)
-                    MediaType contentType) {
+    @GetMapping(value = "/{keywordId}", produces = {TSV_MEDIA_TYPE_VALUE, LIST_MEDIA_TYPE_VALUE, APPLICATION_JSON_VALUE, XLS_MEDIA_TYPE_VALUE})
+    public ResponseEntity<MessageConverterContext<KeywordEntry>> getById(@PathVariable("keywordId")
+                                                                         @Pattern(regexp = KEYWORD_ID_REGEX, flags = {Pattern.Flag.CASE_INSENSITIVE}, message = "{search.keyword.invalid.id}")
+                                                                                 String keywordId,
+                                                                                 @ValidReturnFields(fieldValidatorClazz = KeywordField.ResultFields.class)
+                                                                         @RequestParam(value = "fields", required = false)
+                                                                                 String fields,
+                                                                         @RequestHeader(value = "Accept", defaultValue = APPLICATION_JSON_VALUE)
+                                                                                 MediaType contentType) {
 
         KeywordEntry keywordEntry = this.keywordService.findById(keywordId);
         return super.getEntityResponse(keywordEntry, fields, contentType);
     }
 
-    @RequestMapping(
-            value = "/search",
-            method = RequestMethod.GET,
-            produces = {
-                TSV_MEDIA_TYPE_VALUE,
-                LIST_MEDIA_TYPE_VALUE,
-                APPLICATION_JSON_VALUE,
-                XLS_MEDIA_TYPE_VALUE
-            })
-    public ResponseEntity<MessageConverterContext<KeywordEntry>> search(
-            @Valid KeywordRequestDTO searchRequest,
-            @RequestHeader(value = "Accept", defaultValue = APPLICATION_JSON_VALUE)
-                    MediaType contentType,
-            HttpServletRequest request,
-            HttpServletResponse response) {
+
+//    @ApiOperation(value = "Search keywords", extensions = {
+//            @Extension(properties = {
+//                    @ExtensionProperty(name = "extra-details", value = "|" +
+//                            "          Allowed fields in the query:" +
+//                            "          | Col1 | Col2 | Col3 |" +
+//                            "          |-----|-----|-------|" +
+//                            "          | a   | b   | c     |" +
+//                            "          | d   | e   | f     |")
+//            })
+//    })
+    @RequestMapping(value = "/search", method = RequestMethod.GET,
+            produces = {TSV_MEDIA_TYPE_VALUE, LIST_MEDIA_TYPE_VALUE, APPLICATION_JSON_VALUE, XLS_MEDIA_TYPE_VALUE})
+    public ResponseEntity<MessageConverterContext<KeywordEntry>> search(@Valid
+                                                                                KeywordRequestDTO searchRequest,
+                                                                        @RequestHeader(value = "Accept", defaultValue = APPLICATION_JSON_VALUE)
+                                                                                MediaType contentType,
+                                                                        HttpServletRequest request,
+                                                                        HttpServletResponse response) {
         QueryResult<KeywordEntry> results = keywordService.search(searchRequest);
-        return super.getSearchResponse(
-                results, searchRequest.getFields(), contentType, request, response);
+        return super.getSearchResponse(results, searchRequest.getFields(), contentType, request, response);
     }
 
-    @RequestMapping(
-            value = "/download",
-            method = RequestMethod.GET,
-            produces = {
-                TSV_MEDIA_TYPE_VALUE,
-                LIST_MEDIA_TYPE_VALUE,
-                APPLICATION_JSON_VALUE,
-                XLS_MEDIA_TYPE_VALUE
-            })
-    public ResponseEntity<ResponseBodyEmitter> download(
-            @Valid KeywordRequestDTO searchRequest,
-            @RequestHeader(value = "Accept", defaultValue = APPLICATION_JSON_VALUE)
-                    MediaType contentType,
-            @RequestHeader(value = "Accept-Encoding", required = false) String encoding,
-            HttpServletRequest request) {
+    @RequestMapping(value = "/download", method = RequestMethod.GET,
+            produces = {TSV_MEDIA_TYPE_VALUE, LIST_MEDIA_TYPE_VALUE, APPLICATION_JSON_VALUE, XLS_MEDIA_TYPE_VALUE})
+    public ResponseEntity<ResponseBodyEmitter> download(@Valid
+                                                                KeywordRequestDTO searchRequest,
+                                                        @RequestHeader(value = "Accept", defaultValue = APPLICATION_JSON_VALUE)
+                                                                MediaType contentType,
+                                                        @RequestHeader(value = "Accept-Encoding", required = false)
+                                                                String encoding,
+                                                        HttpServletRequest request) {
         Stream<KeywordEntry> result = keywordService.download(searchRequest);
         return super.download(result, searchRequest.getFields(), contentType, request, encoding);
     }
