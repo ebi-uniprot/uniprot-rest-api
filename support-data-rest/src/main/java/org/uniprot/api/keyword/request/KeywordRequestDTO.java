a51a7be4bc8f1457f0f43849efbc161288a438e8
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-09-10 14:05:37 +0100

    make meta annotation field level

--- support-data-rest/src/main/java/org/uniprot/api/keyword/request/KeywordRequestDTO.java
+++ support-data-rest/src/main/java/org/uniprot/api/keyword/request/KeywordRequestDTO.java
@@ -3,30 +3,31 @@ package org.uniprot.api.keyword.request;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-import lombok.Data;
-
 import org.uniprot.api.rest.request.SearchRequest;
 import org.uniprot.api.rest.validation.ValidReturnFields;
 import org.uniprot.api.rest.validation.ValidSolrQueryFields;
 import org.uniprot.api.rest.validation.ValidSolrQuerySyntax;
 import org.uniprot.api.rest.validation.ValidSolrSortFields;
+import org.uniprot.extension.ModelFieldMeta;
 import org.uniprot.store.search.field.KeywordField;
+
+import lombok.Data;
 
 @Data
 public class KeywordRequestDTO implements SearchRequest {
 
+    @ModelFieldMeta(path = "src/main/resources/keyword_query_param_meta.json")
     @NotNull(message = "{search.required}")
     @ValidSolrQuerySyntax(message = "{search.invalid.query}")
-    @ValidSolrQueryFields(
-            fieldValidatorClazz = KeywordField.Search.class,
-            messagePrefix = "search.keyword")
+    @ValidSolrQueryFields(fieldValidatorClazz = KeywordField.Search.class, messagePrefix = "search.keyword")
     private String query;
-
+    @ModelFieldMeta(path = "src/main/resources/keyword_sort_param_meta.json")
     @ValidSolrSortFields(sortFieldEnumClazz = KeywordField.Sort.class)
     private String sort;
 
     private String cursor;
 
+    @ModelFieldMeta(path = "src/main/resources/keyword_return_field_meta.json")
     @ValidReturnFields(fieldValidatorClazz = KeywordField.ResultFields.class)
     private String fields;
 
