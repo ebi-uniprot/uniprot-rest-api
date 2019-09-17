23a4b630da95d2824ed848e45a68309412744827
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-09-17 15:16:49 +0100

    some more docs

--- uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/controller/UniprotKBController.java
+++ uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/controller/UniprotKBController.java
@@ -71,9 +71,9 @@ public class UniprotKBController extends BasicSearchController<UniProtEntry> {
     }
 
 
-    @Tag(name = "uniprotkb")
+    @Tag(name = "uniprotkb", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.")
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-    @Operation(responses = {
+    @Operation(summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", responses = {
             @ApiResponse(content = {
                     @Content(mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = UniProtEntry.class))),
                     @Content(mediaType = APPLICATION_XML_VALUE, array = @ArraySchema(schema = @Schema(implementation = Entry.class, name = "entries"))),
@@ -99,7 +99,7 @@ public class UniprotKBController extends BasicSearchController<UniProtEntry> {
 
     @Tag(name = "uniprotkb")
     @RequestMapping(value = "/accession/{accession}", method = RequestMethod.GET)
-    @Operation(responses = {
+    @Operation(summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",responses = {
             @ApiResponse(content = {
                             @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = UniProtEntry.class)),
                             @Content(mediaType = APPLICATION_XML_VALUE, schema = @Schema(implementation = Entry.class)),
@@ -136,7 +136,7 @@ public class UniprotKBController extends BasicSearchController<UniProtEntry> {
      */
     @Tag(name = "uniprotkb")
     @RequestMapping(value = "/download", method = RequestMethod.GET)
-    @Operation(responses = {
+    @Operation(summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", responses = {
             @ApiResponse(content = {
                     @Content(mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = UniProtEntry.class))),
                     @Content(mediaType = APPLICATION_XML_VALUE, array = @ArraySchema(schema = @Schema(implementation = Entry.class, name = "entries"))),
