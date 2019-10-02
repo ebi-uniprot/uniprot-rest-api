ada1b55a3660485856aa9cdedad6ec55e9220f3b
Edward Turner <eddturner@ebi.ac.uk>
2019-10-02 09:11:06 +0100

    code tidy

--- support-data-rest/src/main/java/org/uniprot/api/configure/service/UniProtConfigureService.java
+++ support-data-rest/src/main/java/org/uniprot/api/configure/service/UniProtConfigureService.java
@@ -1,27 +1,19 @@
 package org.uniprot.api.configure.service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Service;
 import org.uniprot.core.cv.xdb.DatabaseCategory;
 import org.uniprot.core.cv.xdb.UniProtXDbTypeDetail;
 import org.uniprot.core.cv.xdb.UniProtXDbTypes;
-import org.uniprot.store.search.domain.DatabaseGroup;
-import org.uniprot.store.search.domain.EvidenceGroup;
-import org.uniprot.store.search.domain.FieldGroup;
-import org.uniprot.store.search.domain.SearchItem;
-import org.uniprot.store.search.domain.Tuple;
-import org.uniprot.store.search.domain.impl.AnnotationEvidences;
-import org.uniprot.store.search.domain.impl.DatabaseGroupImpl;
-import org.uniprot.store.search.domain.impl.GoEvidences;
-import org.uniprot.store.search.domain.impl.TupleImpl;
-import org.uniprot.store.search.domain.impl.UniProtResultFields;
-import org.uniprot.store.search.domain.impl.UniProtSearchItems;
+import org.uniprot.store.search.domain.*;
+import org.uniprot.store.search.domain.impl.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UniProtConfigureService {
+
     private static final String ANY_CROSS_REFERENCE_NAME = "Any cross-reference";
     private static final String ANY_CROSS_REFERENCE_VALUE = "any";
     private static final String ANY_DB_GROUP_NAME = "Any";
@@ -44,11 +36,10 @@ public class UniProtConfigureService {
     }
 
     public List<DatabaseGroup> getDatabases() {
-
         List<DatabaseGroup> databases =
                 Arrays.stream(DatabaseCategory.values())
                         .filter(dbCat -> dbCat != DatabaseCategory.UNKNOWN)
-                        .map(dbCat -> getDatabaseGroup(dbCat))
+                        .map(this::getDatabaseGroup)
                         .filter(dbGroup -> !dbGroup.getItems().isEmpty())
                         .collect(Collectors.toList());
 
