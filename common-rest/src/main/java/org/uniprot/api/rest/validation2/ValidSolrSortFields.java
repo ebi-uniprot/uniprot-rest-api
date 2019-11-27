afdbb4840e64dc314e2c7f053887962ac6ddfebc
Edward Turner <eddturner@ebi.ac.uk>
2019-11-27 12:19:05 +0000

    added new version of queryvalidator annotation to use new SearchFields enum

---
+++ common-rest/src/main/java/org/uniprot/api/rest/validation2/ValidSolrSortFields.java
@@ -0,0 +1,125 @@
+package org.uniprot.api.rest.validation2;
+
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This is the solr query solr validator is responsible to verify if the sort field parameter has
+ * valid field names
+ *
+ * @author lgonzales
+ */
+@Constraint(validatedBy = ValidSolrSortFields.SortFieldValidatorImpl.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidSolrSortFields {
+
+    Class<? extends Enum<?>> sortFieldEnumClazz();
+
+    String message() default "{search.invalid.sort}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    class SortFieldValidatorImpl implements ConstraintValidator<ValidSolrSortFields, String> {
+
+        private static final String SORT_FORMAT =
+                "^([\\w]+)\\s([\\w]+)(\\s*,\\s*([\\w]+)\\s([\\w]+))*$";
+        private static final String SORT_ORDER = "^asc|desc$";
+        private List<String> valueList;
+
+        @Override
+        public void initialize(ValidSolrSortFields constraintAnnotation) {
+            valueList = new ArrayList<>();
+            Class<? extends Enum<?>> enumClass = constraintAnnotation.sortFieldEnumClazz();
+
+            Enum[] enumValArr = enumClass.getEnumConstants();
+
+            for (Enum enumVal : enumValArr) {
+                valueList.add(enumVal.name().toLowerCase());
+            }
+        }
+
+        @Override
+        public boolean isValid(String value, ConstraintValidatorContext context) {
+            ConstraintValidatorContextImpl contextImpl = (ConstraintValidatorContextImpl) context;
+            boolean result = true;
+            if (value != null) {
+                value = value.toLowerCase();
+                if (value.matches(SORT_FORMAT)) {
+                    Pattern pattern = Pattern.compile(SORT_FORMAT);
+                    Matcher matcher = pattern.matcher(value);
+                    if (matcher.matches()) {
+                        int index = 0;
+                        List<String> groups = getMatchGroupList(matcher);
+                        while (groups.size() > index) {
+                            String sortField = groups.get(++index);
+                            if (!valueList.contains(sortField)) {
+                                addInvalidSortFieldErrorMessage(contextImpl, sortField);
+                                result = false;
+                            }
+
+                            String sortOrder = groups.get(++index);
+                            if (!sortOrder.matches(SORT_ORDER)) {
+                                addInvalidSortOrderErrorMessage(contextImpl, sortOrder);
+                                result = false;
+                            }
+                            index++; // the comma is another group
+                        }
+                        if (!result && contextImpl != null) {
+                            contextImpl.disableDefaultConstraintViolation();
+                        }
+                    }
+                } else {
+                    addInvalidSortFormatErrorMessage(contextImpl, value);
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        public void addInvalidSortFormatErrorMessage(
+                ConstraintValidatorContextImpl contextImpl, String value) {
+            String errorMessage = "{search.invalid.sort.format}";
+            contextImpl.addMessageParameter("0", value);
+            contextImpl.buildConstraintViolationWithTemplate(errorMessage).addConstraintViolation();
+        }
+
+        public void addInvalidSortOrderErrorMessage(
+                ConstraintValidatorContextImpl contextImpl, String sortOrder) {
+            String errorMessage = "{search.invalid.sort.order}";
+            contextImpl.addMessageParameter("0", sortOrder);
+            contextImpl.buildConstraintViolationWithTemplate(errorMessage).addConstraintViolation();
+        }
+
+        public void addInvalidSortFieldErrorMessage(
+                ConstraintValidatorContextImpl contextImpl, String sortField) {
+            String errorMessage = "{search.invalid.sort.field}";
+            contextImpl.addMessageParameter("0", sortField);
+            contextImpl.buildConstraintViolationWithTemplate(errorMessage).addConstraintViolation();
+        }
+
+        private List<String> getMatchGroupList(Matcher matcher) {
+            List<String> groups = new ArrayList<>();
+            for (int i = 0; i <= matcher.groupCount(); i++) {
+                if (matcher.group(i) != null) {
+                    groups.add(matcher.group(i));
+                }
+            }
+            return groups;
+        }
+    }
+}
---
+++ common-rest/src/main/java/org/uniprot/api/rest/validation2/error/ResponseExceptionHandler.java
@@ -0,0 +1,264 @@
+package org.uniprot.api.rest.validation2.error;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.uniprot.api.common.exception.InvalidRequestException;
+import org.uniprot.api.common.exception.ResourceNotFoundException;
+import org.uniprot.api.common.exception.ServiceException;
+import org.uniprot.api.common.repository.search.QueryRetrievalException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import static java.util.Collections.singletonList;
+import static org.uniprot.core.util.Utils.notNullOrEmpty;
+
+/**
+ * Captures exceptions raised by the application, and handles them in a tailored way.
+ *
+ * @author lgonzales
+ */
+@ControllerAdvice
+public class ResponseExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ResponseExceptionHandler.class);
+    private static final String NOT_FOUND_MESSAGE = "search.not.found";
+    private static final String INTERNAL_ERROR_MESSAGE = "search.internal.error";
+    private static final String REQUIRED_REQUEST_PARAM = "request.parameter.required";
+    private static final String INVALID_REQUEST = "request.invalid";
+
+    private MessageSource messageSource;
+
+    public ResponseExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    /**
+     * Bad Request exception handler that was caught during request
+     *
+     * @param ex throw exception
+     * @param request http request
+     * @return 400 Bad request error response with error message details
+     */
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    public ResponseEntity<ErrorInfo> constraintViolationException(
+            ConstraintViolationException ex, HttpServletRequest request) {
+        List<String> messages = new ArrayList<>();
+
+        for (ConstraintViolation error : ex.getConstraintViolations()) {
+            if (error.getMessage() != null) {
+                messages.add(error.getMessage());
+            }
+        }
+
+        addDebugError(request, ex, messages);
+
+        return getBadRequestResponseEntity(request, messages);
+    }
+
+    /**
+     * Resource not found exception handler that was caught during request
+     *
+     * @param ex throw exception
+     * @param request http request
+     * @return 404 Not Found error response with error message details
+     */
+    @ExceptionHandler(value = {NoHandlerFoundException.class, ResourceNotFoundException.class})
+    public ResponseEntity<ErrorInfo> noHandlerFoundException(
+            Exception ex, HttpServletRequest request) {
+        List<String> messages = new ArrayList<>();
+        messages.add(messageSource.getMessage(NOT_FOUND_MESSAGE, null, Locale.getDefault()));
+        ErrorInfo error = new ErrorInfo(request.getRequestURL().toString(), messages);
+        addDebugError(request, ex, messages);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .contentType(getContentTypeFromRequest(request))
+                .body(error);
+    }
+
+    /**
+     * Internal Server Error exception handler
+     *
+     * @param ex throw exception
+     * @param request http request
+     * @return 500 Internal server error response
+     */
+    @ExceptionHandler({QueryRetrievalException.class, ServiceException.class, Throwable.class})
+    public ResponseEntity<ErrorInfo> handleInternalServerError(
+            Throwable ex, HttpServletRequest request) {
+        logger.error("handleThrowableBadRequest: ", ex);
+        List<String> messages = new ArrayList<>();
+        messages.add(messageSource.getMessage(INTERNAL_ERROR_MESSAGE, null, Locale.getDefault()));
+        addDebugError(request, ex, messages);
+
+        ErrorInfo error = new ErrorInfo(request.getRequestURL().toString(), messages);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(getContentTypeFromRequest(request))
+                .body(error);
+    }
+
+    /**
+     * Bad Request exception handler that was caught during request
+     *
+     * @param ex throw exception
+     * @param request http request
+     * @return 400 Bad request error response with error message details
+     */
+    @ExceptionHandler({BindException.class})
+    public ResponseEntity<ErrorInfo> handleBindExceptionBadRequest(
+            BindException ex, HttpServletRequest request) {
+        List<String> messages = new ArrayList<>();
+
+        for (FieldError error : ex.getFieldErrors()) {
+            if (error.getDefaultMessage() != null) {
+                messages.add(error.getDefaultMessage().replaceAll("\\{field\\}", error.getField()));
+            }
+        }
+
+        for (ObjectError error : ex.getGlobalErrors()) {
+            messages.add(error.getDefaultMessage());
+        }
+
+        addDebugError(request, ex, messages);
+
+        return getBadRequestResponseEntity(request, messages);
+    }
+
+    /**
+     * Bad Request exception handler that was caught during request
+     *
+     * @param ex throw exception
+     * @param request http request
+     * @return 400 Bad request error response with error message details
+     */
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    public ResponseEntity<ErrorInfo> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex, HttpServletRequest request) {
+        List<String> messages =
+                singletonList(
+                        messageSource.getMessage(
+                                REQUIRED_REQUEST_PARAM,
+                                new Object[] {ex.getParameterName()},
+                                Locale.getDefault()));
+        ErrorInfo error = new ErrorInfo(request.getRequestURL().toString(), messages);
+
+        addDebugError(request, ex, messages);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(getContentTypeFromRequest(request))
+                .body(error);
+    }
+
+    /**
+     * Bad Request exception handler that was caught during request
+     *
+     * @param ex throw exception
+     * @param request http request
+     * @return 400 Bad request error response with error message details
+     */
+    @ExceptionHandler({InvalidRequestException.class})
+    public ResponseEntity<ErrorInfo> handleInvalidRequestExceptionBadRequest(
+            InvalidRequestException ex, HttpServletRequest request) {
+        List<String> messages =
+                singletonList(
+                        messageSource.getMessage(
+                                INVALID_REQUEST,
+                                new Object[] {ex.getMessage()},
+                                Locale.getDefault()));
+
+        addDebugError(request, ex, messages);
+
+        return getBadRequestResponseEntity(request, messages);
+    }
+
+    /**
+     * If there is debugError in the request, this method also print exception causes to help in the
+     * debug error
+     *
+     * @param request Request Object.
+     * @param exception the exception that was captured.
+     * @param error List of existing message.
+     */
+    private static void addDebugError(
+            HttpServletRequest request, Throwable exception, List<String> error) {
+        if (request.getParameter("debugError") != null
+                && request.getParameter("debugError").equalsIgnoreCase("true")) {
+
+            Throwable cause = exception.getCause();
+            while (cause != null) {
+                if (cause.getMessage() != null && !cause.getMessage().isEmpty()) {
+                    error.add("Caused by: " + cause.getMessage());
+                }
+                cause = cause.getCause();
+            }
+        }
+    }
+
+    private MediaType getContentTypeFromRequest(HttpServletRequest request) {
+        MediaType result = MediaType.APPLICATION_JSON;
+        String acceptHeader = request.getHeader(HttpHeaders.ACCEPT);
+        if (notNullOrEmpty(acceptHeader)) {
+            result = MediaType.valueOf(acceptHeader);
+        }
+        return result;
+    }
+
+    private ResponseEntity<ErrorInfo> getBadRequestResponseEntity(
+            HttpServletRequest request, List<String> messages) {
+        ErrorInfo error = new ErrorInfo(request.getRequestURL().toString(), messages);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(getContentTypeFromRequest(request))
+                .body(error);
+    }
+
+    /**
+     * Error response entity that provide error message details
+     *
+     * @author lgonzales
+     */
+    @XmlRootElement
+    public static class ErrorInfo {
+        private final String url;
+        private final List<String> messages;
+
+        private ErrorInfo() {
+            this("", Collections.emptyList());
+        }
+
+        ErrorInfo(String url, List<String> messages) {
+            assert url != null : "Error URL cannot be null";
+            assert messages != null : "Error messages cannot be null";
+
+            this.url = url;
+            this.messages = messages;
+        }
+
+        @XmlElement
+        public String getUrl() {
+            return url;
+        }
+
+        @XmlElement
+        public List<String> getMessages() {
+            return messages;
+        }
+    }
+}
