391200d89bd73d722bdc1e5cedc12c1961ac87ee
Edward Turner <eddturner@ebi.ac.uk>
2020-01-15 16:15:34 +0000

    added test to check that the facets listed in facet.properties are valid search fields.

---
+++ uniprotkb-rest/src/test/java/org/uniprot/api/uniprotkb/repository/facet/ValidateFacetPropertiesAreSearchFieldsTest.java
@@ -0,0 +1,36 @@
+package org.uniprot.api.uniprotkb.repository.facet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
+import org.uniprot.api.uniprotkb.repository.search.impl.UniprotFacetConfig;
+import org.uniprot.store.search.domain2.UniProtKBSearchFields;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created 15/01/2020
+ *
+ * @author Edd
+ */
+@SpringBootTest
+class ValidateFacetPropertiesAreSearchFieldsTest {
+    @Autowired private UniprotFacetConfig config;
+
+    @Test
+    void validateAllFacetFieldsAreSearchFields() {
+        config.getFacetNames()
+                .forEach(
+                        facetField -> {
+                            assertThat(
+                                    UniProtKBSearchFields.INSTANCE.hasField(facetField), is(true));
+                        });
+    }
+
+    @TestConfiguration
+    @Import(UniprotFacetConfig.class)
+    static class TestFacetConfig {}
+}
