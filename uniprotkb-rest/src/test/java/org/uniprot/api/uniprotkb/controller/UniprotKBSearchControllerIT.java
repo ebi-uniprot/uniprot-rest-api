f76781975a7015738f95734f1f0e604180a4d366
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2020-02-11 15:51:07 +0000

    moves files out of domain which are not related to core domain

--- uniprotkb-rest/src/test/java/org/uniprot/api/uniprotkb/controller/UniprotKBSearchControllerIT.java
+++ uniprotkb-rest/src/test/java/org/uniprot/api/uniprotkb/controller/UniprotKBSearchControllerIT.java
@@ -47,17 +47,17 @@ import org.uniprot.api.uniprotkb.repository.search.impl.UniprotFacetConfig;
 import org.uniprot.api.uniprotkb.repository.search.impl.UniprotQueryRepository;
 import org.uniprot.api.uniprotkb.repository.store.UniProtKBStoreClient;
 import org.uniprot.core.cv.xdb.DBXRefTypeAttribute;
-import org.uniprot.core.cv.xdb.UniProtXDbTypes;
 import org.uniprot.core.uniprot.UniProtEntry;
 import org.uniprot.core.uniprot.UniProtEntryType;
 import org.uniprot.core.uniprot.builder.UniProtEntryBuilder;
 import org.uniprot.core.uniprot.comment.CommentType;
 import org.uniprot.core.uniprot.feature.FeatureCategory;
 import org.uniprot.core.uniprot.feature.FeatureType;
-import org.uniprot.core.uniprot.xdb.UniProtXDbType;
 import org.uniprot.core.uniprot.xdb.builder.UniProtDBCrossReferenceBuilder;
 import org.uniprot.cv.chebi.ChebiRepo;
 import org.uniprot.cv.ec.ECRepo;
+import org.uniprot.cv.xdb.UniProtXDbTypeImpl;
+import org.uniprot.cv.xdb.UniProtXDbTypes;
 import org.uniprot.store.datastore.voldemort.uniprot.VoldemortInMemoryUniprotEntryStore;
 import org.uniprot.store.indexer.DataStoreManager;
 import org.uniprot.store.indexer.uniprot.inactiveentry.InactiveUniProtEntry;
@@ -755,7 +755,7 @@ class UniprotKBSearchControllerIT extends AbstractSearchWithFacetControllerIT {
                     new UniProtEntryBuilder("P00001", "ID_P00001", UniProtEntryType.SWISSPROT)
                             .databaseCrossReferencesAdd(
                                     new UniProtDBCrossReferenceBuilder()
-                                            .databaseType(new UniProtXDbType("Proteomes"))
+                                            .databaseType(new UniProtXDbTypeImpl("Proteomes"))
                                             .id("UP000000000")
                                             .propertiesAdd(
                                                     new DBXRefTypeAttribute("a", "a", "a"), "value")
