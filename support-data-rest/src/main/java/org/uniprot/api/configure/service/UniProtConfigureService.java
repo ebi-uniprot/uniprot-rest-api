e76206b42be160d1141535db7f70481ad92b983f
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-02-12 11:57:10 +0000

    update get advance search api to read from new config file

--- support-data-rest/src/main/java/org/uniprot/api/configure/service/UniProtConfigureService.java
+++ support-data-rest/src/main/java/org/uniprot/api/configure/service/UniProtConfigureService.java
@@ -1,17 +1,17 @@
 package org.uniprot.api.configure.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.uniprot.api.configure.uniprot.domain.model.AdvanceSearchTerm;
 import org.uniprot.core.cv.xdb.DatabaseCategory;
 import org.uniprot.core.cv.xdb.UniProtXDbTypeDetail;
 import org.uniprot.cv.xdb.UniProtXDbTypes;
+import org.uniprot.store.config.FieldItem;
+import org.uniprot.store.config.UniProtSearchFieldConfiguration;
 import org.uniprot.store.search.domain.*;
 import org.uniprot.store.search.domain.impl.*;
-import org.uniprot.store.search.domain2.UniProtKBSearchItems;
 
 @Service
 public class UniProtConfigureService {
@@ -34,8 +34,9 @@ public class UniProtConfigureService {
         return SEARCH_ITEMS.getSearchItems();
     }
 
-    public List<org.uniprot.store.search.domain2.SearchItem> getUniProtSearchItems2() {
-        return UniProtKBSearchItems.INSTANCE.getSearchItems();
+    public List<AdvanceSearchTerm> getUniProtSearchItems2() {
+        //        return UniProtKBSearchItems.INSTANCE.getSearchItems();
+        return getUniprotSearchTerms();
     }
 
     public List<EvidenceGroup> getAnnotationEvidences() {
@@ -83,5 +84,57 @@ public class UniProtConfigureService {
 
     public List<UniProtXDbTypeDetail> getAllDatabases() {
         return DBX_TYPES.getAllDBXRefTypes();
+    }
+
+    private List<AdvanceSearchTerm> getUniprotSearchTerms() {
+        UniProtSearchFieldConfiguration config = UniProtSearchFieldConfiguration.getInstance();
+        List<FieldItem> rootFieldItems = config.getTopLevelFieldItems();
+        List<AdvanceSearchTerm> rootSearchTermConfigs = convert(rootFieldItems);
+        Queue<AdvanceSearchTerm> queue = new LinkedList<>(rootSearchTermConfigs);
+        while (!queue.isEmpty()) {
+            AdvanceSearchTerm currentItem = queue.remove();
+            List<AdvanceSearchTerm> children =
+                    convert(config.getChildFieldItems(currentItem.getId()));
+            queue.addAll(children);
+            currentItem.setItems(children);
+        }
+        return rootSearchTermConfigs;
+    }
+
+    private List<AdvanceSearchTerm> convert(List<FieldItem> fieldItems) {
+        return fieldItems.stream().map(fi -> convert(fi)).sorted().collect(Collectors.toList());
+    }
+
+    private AdvanceSearchTerm convert(FieldItem fi) {
+        AdvanceSearchTerm.AdvanceSearchTermBuilder b = AdvanceSearchTerm.builder();
+        b.id(fi.getId()).parentId(fi.getParentId()).childNumber(fi.getChildNumber());
+        b.seqNumber(fi.getSeqNumber()).label(fi.getLabel()).term(fi.getFieldName());
+        b.description(fi.getDescription())
+                .example(fi.getExample())
+                .autoComplete(fi.getAutoComplete());
+        b.regex(fi.getValidRegex());
+        if (fi.getItemType() != null) {
+            b.itemType(fi.getItemType().name());
+        }
+        if (fi.getDataType() != null) {
+            b.dataType(fi.getDataType().name());
+        }
+        if (fi.getFieldType() != null) {
+            b.fieldType(fi.getFieldType().name());
+        }
+
+        List<FieldItem.Value> values = fi.getValues();
+        if (values != null) {
+            List<AdvanceSearchTerm.Value> stcValues =
+                    values.stream()
+                            .map(
+                                    value ->
+                                            new AdvanceSearchTerm.Value(
+                                                    value.getName(), value.getValue()))
+                            .collect(Collectors.toList());
+            b.values(stcValues);
+        }
+
+        return b.build();
     }
 }
