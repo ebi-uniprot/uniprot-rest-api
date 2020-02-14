f60dca0e5491b8289d63a48d4642e52294e9f4e3
eddturner <eddturner@gmai.com>
2020-02-14 16:38:14 +0000

    refactor to put common code in functions

--- benchmark-rest/src/test/scala/advanced-search/benchmark-by-url/write-genes-gatling.scala
+++ benchmark-rest/src/test/scala/advanced-search/benchmark-by-url/write-genes-gatling.scala
@@ -4,23 +4,26 @@ import java.util.Properties
 
 val service = "uniprotkb"
 
-val (inputFile, outputDir) =
+// load configuration properties
+val (inputFile, outputDir, count, contentTypesStr, endPointsStr) =
   try {
     val prop = new Properties()
     prop.load(new FileInputStream("/home/edd/working/intellij/website/uniprot-rest-api/benchmark-rest/src/test/scala/advanced-search/benchmark-by-url/config.properties"))
     (prop.getProperty("uniprotkb.sourceFilePath"),
-      prop.getProperty("uniprotkb.outputDir"))
+      prop.getProperty("uniprotkb.genes.outputDir"),
+      new Integer(prop.getProperty("uniprotkb.genes.count")),
+    prop.getProperty("uniprotkb.genes.contentTypes"),
+    prop.getProperty("uniprotkb.genes.endPoints"))
   } catch {
     case e: Exception =>
       e.printStackTrace()
       sys.exit(1)
   }
 
+val contentTypes=contentTypesStr.split(",")
+val endPoints=endPointsStr.split(",")
 val ff = sc.textFile("file://" + inputFile)
-val contentTypes = Seq("application/json", "text/tsv", "text/flatfile", "application/json", "application/vnd.ms-excel", "text/fasta", "text/gff")
-val endPoints = Seq("/uniprot/api/" + service + "/search?query=XXXX", "/uniprot/api/"+ service + "/download?query=XXXX")
 val pattern = "^GN   Name=([A-Za-z0-9_]+).*".r
-val count = 20000
 val random = new Random
 
 def randomItemFrom(l: Seq[String]): String = {
