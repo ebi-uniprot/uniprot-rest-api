6b5abe1b2dd8ceca1b2749240b5d5021cefe2239
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-02-16 13:18:08 +0000

    uniprotkb download all integ test

---
+++ uniprotkb-rest/src/test/java/org/uniprot/api/uniprotkb/controller/download/IT/BaseUniprotKBDownloadIT.java
@@ -0,0 +1,106 @@
+package org.uniprot.api.uniprotkb.controller.download.IT;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.uniprot.api.common.repository.search.SolrQueryRepository;
+import org.uniprot.api.rest.controller.AbstractDownloadControllerIT;
+import org.uniprot.api.uniprotkb.repository.search.impl.UniprotQueryRepository;
+import org.uniprot.api.uniprotkb.repository.store.UniProtKBStoreClient;
+import org.uniprot.core.uniprot.UniProtEntry;
+import org.uniprot.cv.chebi.ChebiRepo;
+import org.uniprot.cv.ec.ECRepo;
+import org.uniprot.store.datastore.voldemort.uniprot.VoldemortInMemoryUniprotEntryStore;
+import org.uniprot.store.indexer.DataStoreManager;
+import org.uniprot.store.indexer.uniprot.mockers.GoRelationsRepoMocker;
+import org.uniprot.store.indexer.uniprot.mockers.PathwayRepoMocker;
+import org.uniprot.store.indexer.uniprot.mockers.TaxonomyRepoMocker;
+import org.uniprot.store.indexer.uniprot.mockers.UniProtEntryMocker;
+import org.uniprot.store.indexer.uniprotkb.converter.UniProtEntryConverter;
+import org.uniprot.store.indexer.uniprotkb.processor.InactiveEntryConverter;
+import org.uniprot.store.search.SolrCollection;
+
+/** Class to keep things common to all disease download tests */
+public class BaseUniprotKBDownloadIT extends AbstractDownloadControllerIT {
+    @Autowired private UniprotQueryRepository repository;
+    public static String SEARCH_ACCESSION1 =
+            "DI-" + ThreadLocalRandom.current().nextLong(10000, 50000);
+    public static String SEARCH_ACCESSION2 =
+            "DI-" + ThreadLocalRandom.current().nextLong(50001, 99999);
+    public static List<String> SORTED_ACCESSIONS =
+            new ArrayList<>(Arrays.asList(SEARCH_ACCESSION1, SEARCH_ACCESSION2));
+
+    public static final String ACC1 = "DI-12345";
+    public static final String ACC2 = "DI-11111";
+    public static final String ACC3 = "DI-54321";
+
+    private UniProtKBStoreClient storeClient;
+
+    @BeforeAll
+    void setUp() {
+        UniProtEntryConverter uniProtEntryConverter =
+                new UniProtEntryConverter(
+                        TaxonomyRepoMocker.getTaxonomyRepo(),
+                        GoRelationsRepoMocker.getGoRelationRepo(),
+                        PathwayRepoMocker.getPathwayRepo(),
+                        Mockito.mock(ChebiRepo.class),
+                        Mockito.mock(ECRepo.class),
+                        new HashMap<>());
+
+        DataStoreManager dsm = getStoreManager();
+        dsm.addDocConverter(DataStoreManager.StoreType.UNIPROT, uniProtEntryConverter);
+        dsm.addDocConverter(
+                DataStoreManager.StoreType.INACTIVE_UNIPROT, new InactiveEntryConverter());
+        dsm.addSolrClient(DataStoreManager.StoreType.INACTIVE_UNIPROT, SolrCollection.uniprot);
+
+        this.storeClient =
+                new UniProtKBStoreClient(
+                        VoldemortInMemoryUniprotEntryStore.getInstance("avro-uniprot"));
+        dsm.addStore(DataStoreManager.StoreType.UNIPROT, storeClient);
+    }
+
+    @AfterEach
+    void cleanStore() {
+        this.storeClient.truncate();
+    }
+
+    @Override
+    protected DataStoreManager.StoreType getStoreType() {
+        return DataStoreManager.StoreType.UNIPROT;
+    }
+
+    @Override
+    protected SolrCollection getSolrCollection() {
+        return SolrCollection.uniprot;
+    }
+
+    @Override
+    protected SolrQueryRepository getRepository() {
+        return repository;
+    }
+
+    @Override
+    protected String getDownloadRequestPath() {
+        return "/uniprotkb/download";
+    }
+
+    @Override
+    protected void saveEntries(int numberOfEntries) {
+        List<UniProtEntry> entries =
+                UniProtEntryMocker.cloneEntries(UniProtEntryMocker.Type.SP, numberOfEntries);
+        this.getStoreManager().save(DataStoreManager.StoreType.UNIPROT, entries);
+    }
+
+    @Override
+    protected void saveEntry(String accession, long suffix) {
+        //        DiseaseSolrDocumentHelper.createDiseaseDocuments(this.getStoreManager(),
+        // accession, suffix);
+    }
+}
