4d424d009abc8fee1d12d3b505671c3cfc1ec680
lgonzales <lgonzales@ebi.ac.uk>
2020-02-26 11:10:22 +0000

    - add new ReturnField model class

--- common-rest/src/main/java/org/uniprot/api/rest/output/converter/JsonMessageConverter.java
+++ common-rest/src/main/java/org/uniprot/api/rest/output/converter/JsonMessageConverter.java
@@ -2,19 +2,27 @@ package org.uniprot.api.rest.output.converter;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.github.bohnman.squiggly.context.provider.SimpleSquigglyContextProvider;
+import com.github.bohnman.squiggly.filter.SquigglyPropertyFilter;
+import com.github.bohnman.squiggly.parser.SquigglyParser;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.springframework.http.MediaType;
 import org.uniprot.api.rest.output.context.MessageConverterContext;
 import org.uniprot.core.util.Utils;
-import org.uniprot.store.search.field.ReturnField;
+
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.uniprot.store.config.returnfield.model.ReturnField;
 
 /**
  * @param <T> instance of the object that is being written.
@@ -23,11 +31,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JsonMessageConverter<T> extends AbstractEntityHttpMessageConverter<T> {
 
     private static final String COMMA = "\\s*,\\s*";
+    private static final String PATH_PREFIX = "$..";
     private final ObjectMapper objectMapper;
-    private ThreadLocal<Map<String, List<String>>> tlFilters = new ThreadLocal<>();
+    private ThreadLocal<List<ReturnField>> tlFilters = new ThreadLocal<>();
     private ThreadLocal<JsonGenerator> tlJsonGenerator = new ThreadLocal<>();
     private List<ReturnField> allFields;
-    private JsonResponseFieldProjector fieldProjector;
 
     public JsonMessageConverter(
             ObjectMapper objectMapper,
@@ -36,7 +44,6 @@ public class JsonMessageConverter<T> extends AbstractEntityHttpMessageConverter<
         super(MediaType.APPLICATION_JSON, messageConverterEntryClass);
         this.objectMapper = objectMapper;
         this.allFields = allFields;
-        this.fieldProjector = new JsonResponseFieldProjector();
     }
 
     @Override
@@ -78,7 +85,32 @@ public class JsonMessageConverter<T> extends AbstractEntityHttpMessageConverter<
     @Override
     protected void writeEntity(T entity, OutputStream outputStream) throws IOException {
         JsonGenerator generator = tlJsonGenerator.get();
-        generator.writeObject(projectEntryFields(entity));
+        List<ReturnField> fields = getThreadLocalFilterMap();
+        if(Utils.notNullNotEmpty(fields)) {
+            ObjectMapper mapper = objectMapper.copy();
+
+            FilterProvider filterProvider = getFieldsFilterProvider(fields);
+            mapper.setFilterProvider(filterProvider);
+
+            String filteredJson = mapper.writeValueAsString(entity);
+
+            Map<String, String> filterFields = getFieldsWithFilterMap(fields);
+            if(Utils.notNullNotEmpty(filterFields)) {
+                DocumentContext referenceJson = JsonPath.parse(filteredJson);
+                for (Map.Entry<String, String> field: filterFields.entrySet()) {
+                    String path = PATH_PREFIX + field.getKey() + field.getValue();
+                    JsonPath filterPath = JsonPath.compile(path);
+                    Object filteredItems = referenceJson.read(filterPath);
+
+                    JsonPath setPath = JsonPath.compile(PATH_PREFIX + field.getKey());
+                    referenceJson = referenceJson.set(setPath, filteredItems);
+                }
+                filteredJson = referenceJson.jsonString();
+            }
+            generator.writeString(filteredJson);
+        }else{
+            generator.writeObject(entity);
+        }
     }
 
     @Override
@@ -94,29 +126,22 @@ public class JsonMessageConverter<T> extends AbstractEntityHttpMessageConverter<
         generator.close();
     }
 
-    protected Map<String, List<String>> getThreadLocalFilterMap() {
+    protected List<ReturnField> getThreadLocalFilterMap() {
         return tlFilters.get();
     }
 
-    // returns only the required fields asked by the client or all fields in T.ResultFields enum
-    protected Map<String, Object> projectEntryFields(T entity) {
-
-        Map<String, List<String>> filterFieldMap = getThreadLocalFilterMap();
-
-        Map<String, Object> result =
-                this.fieldProjector.project(entity, filterFieldMap, this.allFields);
-        return result;
-    }
-
-    protected Map<String, List<String>> getFilterFieldMap(String fields) {
+    protected List<ReturnField> getFilterFieldMap(String fields) {
         if (Utils.notNullNotEmpty(fields)) {
-            Map<String, List<String>> filters = new HashMap<>();
+            List<ReturnField> filters = new ArrayList<>();
             for (String field : fields.split(COMMA)) {
-                filters.put(field, Collections.emptyList());
+                allFields.stream()
+                        .filter(fieldItem -> fieldItem.getName().equals(field))
+                        .findFirst()
+                        .ifPresent(filters::add);
             }
             return filters;
         } else {
-            return Collections.emptyMap();
+            return Collections.emptyList();
         }
     }
 
@@ -126,5 +151,40 @@ public class JsonMessageConverter<T> extends AbstractEntityHttpMessageConverter<
         } catch (IOException e) {
             throw new StopStreamException("Failed to write Facet JSON object", e);
         }
+    }
+
+    private SimpleFilterProvider getFieldsFilterProvider(List<ReturnField> fields) {
+        String fieldsPath = fields.stream()
+                .map(ReturnField::getPath)
+                .map(path -> path.replaceAll("\\[\\*]", ""))
+                .collect(Collectors.joining(","));
+
+        SquigglyPropertyFilter filter =
+                new SquigglyPropertyFilter(
+                        new SimpleSquigglyContextProvider(new SquigglyParser(), fieldsPath));
+
+        return new SimpleFilterProvider()
+                .addFilter(SquigglyPropertyFilter.FILTER_ID, filter);
+    }
+
+    private Map<String, String> getFieldsWithFilterMap(List<ReturnField> fields) {
+        Map<String, String> filters = new HashMap<>();
+        for (ReturnField resultField:fields) {
+            if(Utils.notNullNotEmpty(resultField.getFilter())){
+                if(filters.containsKey(resultField.getPath())){
+                    String currentFilter = filters.get(resultField.getPath());
+                    Pattern p = Pattern.compile("\\((.*?)\\)");
+                    Matcher existingMatch = p.matcher(currentFilter);
+                    Matcher newMatch = p.matcher(resultField.getFilter());
+                    if(existingMatch.find() && newMatch.find()) {
+                        String newFilter = "[?(" + existingMatch.group(1) + " || " + newMatch.group(1) + ")]";
+                        filters.put(resultField.getPath(),newFilter);
+                    }
+                } else {
+                    filters.put(resultField.getPath(),resultField.getFilter());
+                }
+            }
+        }
+        return filters;
     }
 }
