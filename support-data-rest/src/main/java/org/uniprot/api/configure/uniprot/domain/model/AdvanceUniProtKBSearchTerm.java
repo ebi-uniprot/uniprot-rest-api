d3e479d8d7c30adb7dd2645a7f2c5360d4ac37c0
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-02-27 11:55:26 +0000

    add some test cases and refactoring

---
+++ support-data-rest/src/main/java/org/uniprot/api/configure/uniprot/domain/model/AdvanceUniProtKBSearchTerm.java
@@ -0,0 +1,148 @@
+package org.uniprot.api.configure.uniprot.domain.model;
+
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import org.apache.commons.lang3.StringUtils;
+import org.uniprot.store.config.searchfield.common.SearchFieldConfig;
+import org.uniprot.store.config.searchfield.factory.SearchFieldConfigFactory;
+import org.uniprot.store.config.searchfield.factory.UniProtDataType;
+import org.uniprot.store.config.searchfield.model.FieldItem;
+import org.uniprot.store.config.searchfield.model.FieldType;
+import org.uniprot.store.search.domain.EvidenceGroup;
+import org.uniprot.store.search.domain.impl.AnnotationEvidences;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class AdvanceUniProtKBSearchTerm implements Serializable {
+    private static final long serialVersionUID = -5776203445383103470L;
+    private String id;
+    @JsonIgnore private String parentId;
+    @JsonIgnore private Integer childNumber;
+    @JsonIgnore private Integer seqNumber;
+    private String label;
+    private String itemType;
+    private String term;
+    private String dataType;
+    private String fieldType;
+    @JsonIgnore private String description;
+    private String example;
+    private String autoComplete;
+    private String autoCompleteQueryField;
+    @JsonIgnore private String autoCompleteQueryFieldValidRegex;
+    private String regex;
+    private List<Value> values;
+    private List<AdvanceUniProtKBSearchTerm> items;
+    private List<EvidenceGroup> evidenceGroups;
+
+    @Data
+    @AllArgsConstructor
+    public static class Value {
+        private String name;
+        private String value;
+    }
+
+    public static List<AdvanceUniProtKBSearchTerm> getUniProtKBSearchTerms() {
+        SearchFieldConfig config =
+                SearchFieldConfigFactory.getSearchFieldConfig(UniProtDataType.uniprotkb);
+        List<FieldItem> rootFieldItems = getTopLevelFieldItems(config);
+        Comparator<AdvanceUniProtKBSearchTerm> comparatorBySeqNumber =
+                Comparator.comparing(AdvanceUniProtKBSearchTerm::getSeqNumber);
+        Comparator<AdvanceUniProtKBSearchTerm> comparatorByChildNumber =
+                Comparator.comparing(AdvanceUniProtKBSearchTerm::getChildNumber);
+        List<AdvanceUniProtKBSearchTerm> rootSearchTermConfigs =
+                convert(rootFieldItems, comparatorBySeqNumber);
+
+        Queue<AdvanceUniProtKBSearchTerm> queue = new LinkedList<>(rootSearchTermConfigs);
+
+        while (!queue.isEmpty()) { // BFS logic
+            AdvanceUniProtKBSearchTerm currentItem = queue.remove();
+            List<FieldItem> childFieldItems = getChildFieldItems(config, currentItem.getId());
+            List<AdvanceUniProtKBSearchTerm> children =
+                    convert(childFieldItems, comparatorByChildNumber);
+            queue.addAll(children);
+            currentItem.setItems(children);
+        }
+        return rootSearchTermConfigs;
+    }
+
+    public static List<FieldItem> getTopLevelFieldItems(SearchFieldConfig searchFieldConfig) {
+        return searchFieldConfig.getAllFieldItems().stream()
+                .filter(AdvanceUniProtKBSearchTerm::isTopLevel)
+                .collect(Collectors.toList());
+    }
+
+    public static List<FieldItem> getChildFieldItems(
+            SearchFieldConfig searchFieldConfig, String parentId) {
+        return searchFieldConfig.getAllFieldItems().stream()
+                .filter(fi -> isChildOf(parentId, fi))
+                .collect(Collectors.toList());
+    }
+
+    private static AdvanceUniProtKBSearchTerm from(FieldItem fi) {
+        AdvanceUniProtKBSearchTerm.AdvanceUniProtKBSearchTermBuilder b =
+                AdvanceUniProtKBSearchTerm.builder();
+        b.id(fi.getId()).parentId(fi.getParentId()).childNumber(fi.getChildNumber());
+        b.seqNumber(fi.getSeqNumber()).label(fi.getLabel()).term(fi.getFieldName());
+        b.description(fi.getDescription())
+                .example(fi.getExample())
+                .autoComplete(fi.getAutoComplete());
+        b.autoCompleteQueryField(fi.getAutoCompleteQueryField())
+                .autoCompleteQueryFieldValidRegex(fi.getAutoCompleteQueryFieldValidRegex());
+        b.regex(fi.getValidRegex());
+        if (fi.getItemType() != null) {
+            b.itemType(fi.getItemType().name());
+        }
+        if (fi.getDataType() != null) {
+            b.dataType(fi.getDataType().name());
+        }
+        if (fi.getFieldType() != null) {
+            b.fieldType(fi.getFieldType().name());
+            if (fi.getFieldType() == FieldType.evidence) {
+                b.evidenceGroups(AnnotationEvidences.INSTANCE.getEvidences());
+            }
+        }
+
+        List<FieldItem.Value> values = fi.getValues();
+        if (values != null) {
+            List<AdvanceUniProtKBSearchTerm.Value> stcValues =
+                    values.stream()
+                            .map(
+                                    value ->
+                                            new AdvanceUniProtKBSearchTerm.Value(
+                                                    value.getName(), value.getValue()))
+                            .collect(Collectors.toList());
+            b.values(stcValues);
+        }
+
+        return b.build();
+    }
+
+    private static List<AdvanceUniProtKBSearchTerm> convert(
+            List<FieldItem> fieldItems, Comparator<AdvanceUniProtKBSearchTerm> comparator) {
+        return fieldItems.stream()
+                .map(AdvanceUniProtKBSearchTerm::from)
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isChildOf(String parentId, FieldItem fieldItem) {
+        return parentId.equals(fieldItem.getParentId());
+    }
+
+    private static boolean isTopLevel(FieldItem fi) {
+        return StringUtils.isBlank(fi.getParentId()) && fi.getSeqNumber() != null;
+    }
+}
