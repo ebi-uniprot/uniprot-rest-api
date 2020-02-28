28b53ac0dba82ac54c3e236a611eef9926f98087
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-02-28 18:17:50 +0000

    rebase with master

--- support-data-rest/src/main/java/org/uniprot/api/crossref/service/CrossRefService.java
+++ support-data-rest/src/main/java/org/uniprot/api/crossref/service/CrossRefService.java
@@ -8,7 +8,7 @@ import org.uniprot.api.crossref.config.CrossRefQueryBoostsConfig;
 import org.uniprot.api.crossref.repository.CrossRefRepository;
 import org.uniprot.api.crossref.request.CrossRefEntryConverter;
 import org.uniprot.api.rest.service.BasicSearchService;
-import org.uniprot.core.crossref.CrossRefEntry;
+import org.uniprot.core.cv.xdb.CrossRefEntry;
 import org.uniprot.store.config.searchfield.common.SearchFieldConfig;
 import org.uniprot.store.config.searchfield.factory.SearchFieldConfigFactory;
 import org.uniprot.store.config.searchfield.factory.UniProtDataType;
