98f1256f64f73ef633928910b0cb492503add399
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-02-28 18:17:50 +0000

    rebase with master

--- support-data-rest/src/test/java/org/uniprot/api/configure/uniprot/domain/AdvanceUniProtKBSearchTermIT.java
+++ support-data-rest/src/test/java/org/uniprot/api/configure/uniprot/domain/AdvanceUniProtKBSearchTermIT.java
@@ -1,40 +1,70 @@
 package org.uniprot.api.configure.uniprot.domain;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.uniprot.api.configure.uniprot.domain.model.AdvanceUniProtKBSearchTerm;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import edu.emory.mathcs.backport.java.util.Arrays;
 
 public class AdvanceUniProtKBSearchTermIT {
 
     private static List<AdvanceUniProtKBSearchTerm> SEARCH_TERMS;
-    private final static List<String> EXPECTED_TOP_LEVEL_TERMS = Arrays.asList(new String[]{
-            "UniProtKB AC", "Entry Name [ID]", "Protein Name [DE]", "Gene Name [GN]",
-            "Organism [OS]", "Taxonomy [OC]", "Virus host", "Protein Existence [PE]",
-            "Function", "Subcellular location", "Pathology & Biotech", "PTM/Processing",
-            "Expression", "Interaction", "Structure", "Sequence", "Family and Domains",
-            "Cross-references", "Web Resources", "Date Of", "Gene Ontology [GO]",
-            "Keyword [KW]", "Literature Citation", "Proteomes", "Cited for", "Reviewed",
-            "Active"
-    });
-    private final static List<String> FUNCTION_CHILDREN = Arrays.asList(new String[]{
-            "Enzyme classification [EC]", "Cofactors", "Biophysicochemical properties",
-            "Catalytic Activity", "Activity regulation", "Function [CC]", "Caution",
-            "Sites", "Calcium binding", "DNA binding", "Nucleotide binding", "Pathway",
-            "Miscellaneous [CC]"
-    });
+    private static final List<String> EXPECTED_TOP_LEVEL_TERMS =
+            Arrays.asList(
+                    new String[] {
+                        "UniProtKB AC",
+                        "Entry Name [ID]",
+                        "Protein Name [DE]",
+                        "Gene Name [GN]",
+                        "Organism [OS]",
+                        "Taxonomy [OC]",
+                        "Virus host",
+                        "Protein Existence [PE]",
+                        "Function",
+                        "Subcellular location",
+                        "Pathology & Biotech",
+                        "PTM/Processing",
+                        "Expression",
+                        "Interaction",
+                        "Structure",
+                        "Sequence",
+                        "Family and Domains",
+                        "Cross-references",
+                        "Web Resources",
+                        "Date Of",
+                        "Gene Ontology [GO]",
+                        "Keyword [KW]",
+                        "Literature Citation",
+                        "Proteomes",
+                        "Cited for",
+                        "Reviewed",
+                        "Active"
+                    });
+    private static final List<String> FUNCTION_CHILDREN =
+            Arrays.asList(
+                    new String[] {
+                        "Enzyme classification [EC]",
+                        "Cofactors",
+                        "Biophysicochemical properties",
+                        "Catalytic Activity",
+                        "Activity regulation",
+                        "Function [CC]",
+                        "Caution",
+                        "Sites",
+                        "Calcium binding",
+                        "DNA binding",
+                        "Nucleotide binding",
+                        "Pathway",
+                        "Miscellaneous [CC]"
+                    });
 
     @BeforeAll
     static void setUp() {
@@ -43,8 +73,10 @@ public class AdvanceUniProtKBSearchTermIT {
 
     @ParameterizedTest(name = "[{0}] == \"{1}\" ?")
     @MethodSource("provideIndexAndLabelOfTopLevelTerms")
-    void testTopLevelTermsOrder(Integer index, String label){
-        Assertions.assertEquals(EXPECTED_TOP_LEVEL_TERMS.size(), SEARCH_TERMS.size(),
+    void testTopLevelTermsOrder(Integer index, String label) {
+        Assertions.assertEquals(
+                EXPECTED_TOP_LEVEL_TERMS.size(),
+                SEARCH_TERMS.size(),
                 "Top level fields count does not match");
         Assertions.assertNotNull(SEARCH_TERMS.get(index));
         Assertions.assertEquals(Integer.valueOf(index), SEARCH_TERMS.get(index).getSeqNumber());
@@ -53,20 +85,22 @@ public class AdvanceUniProtKBSearchTermIT {
 
     @ParameterizedTest(name = "Function[{0}] == \"{1}\" ?")
     @MethodSource("provideSeqNumberAndLabelFunctionChildren")
-    void testFunctionChildren(Integer childNumber, String label){
+    void testFunctionChildren(Integer childNumber, String label) {
         Assertions.assertTrue(childNumber < FUNCTION_CHILDREN.size());
         Assertions.assertNotNull(FUNCTION_CHILDREN.get(childNumber));
         Assertions.assertEquals(FUNCTION_CHILDREN.get(childNumber), label);
     }
 
-    private static Stream<Arguments> provideIndexAndLabelOfTopLevelTerms(){
+    private static Stream<Arguments> provideIndexAndLabelOfTopLevelTerms() {
         return IntStream.range(0, EXPECTED_TOP_LEVEL_TERMS.size())
                 .mapToObj(index -> Arguments.of(index, EXPECTED_TOP_LEVEL_TERMS.get(index)));
     }
 
-    private static Stream<Arguments> provideSeqNumberAndLabelFunctionChildren(){
-        Optional<AdvanceUniProtKBSearchTerm> optFuncTerm = SEARCH_TERMS.stream().filter(ft -> "Function".equals(ft.getLabel())).findFirst();
+    private static Stream<Arguments> provideSeqNumberAndLabelFunctionChildren() {
+        Optional<AdvanceUniProtKBSearchTerm> optFuncTerm =
+                SEARCH_TERMS.stream().filter(ft -> "Function".equals(ft.getLabel())).findFirst();
         Assertions.assertTrue(optFuncTerm.isPresent(), "Function term is not present");
-        return optFuncTerm.map(ft -> ft.getItems()).get().stream().map(item -> Arguments.of(item.getChildNumber(), item.getLabel()));
+        return optFuncTerm.map(ft -> ft.getItems()).get().stream()
+                .map(item -> Arguments.of(item.getChildNumber(), item.getLabel()));
     }
 }
