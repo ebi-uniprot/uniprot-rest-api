b1913e4acdf72c16ee5a417cc1957f2e7f48af36
eddturner <eddturner@gmai.com>
2020-04-06 12:02:28 +0100

    add todos

--- common-rest/src/main/java/org/uniprot/api/rest/request/HttpServletRequestContentTypeMutator.java
+++ common-rest/src/main/java/org/uniprot/api/rest/request/HttpServletRequestContentTypeMutator.java
@@ -33,7 +33,8 @@ public class HttpServletRequestContentTypeMutator {
     private static final String SEARCH = "/search";
     private static final String DOWNLOAD = "/download";
     private static final Pattern ENTRY_CONTEXT_PATH_MATCHER =
-            Pattern.compile("^(.*/(.*)/(.*))\\.([\\w-]+)$");
+            // TODO: 03/04/20 test
+            Pattern.compile("^(/[\\w-]+)*/[\\w-]+\\.([\\w-]+)$");
     private static final Set<String> ALLOWED_ACCEPT_HEADERS =
             UniProtMediaType.ALL_TYPES.stream().map(MimeType::toString).collect(Collectors.toSet());
 
@@ -77,6 +78,25 @@ public class HttpServletRequestContentTypeMutator {
 
     private static void addDefaultAcceptHeaderIfRequired(
             boolean mutated, MutableHttpServletRequest request) {
+        /*
+        ensure bean that knows about the end-point's valid content types is able to look up the
+        end-point's valid content types here, and store in X
+
+        1. set content type requested
+        1.1 take from accept header
+        1.2 if request fits entry request pattern, extract from extension
+        1.3 if request contains format parameter, extract from format parameter
+        1.4 if no content type still, set to default content type, json
+
+        2. if content type requested is NOT inside X
+        2.1 if user-agent is a browser, then set content type to json
+        2.2 otherwise
+        2.2.1 if used extension/format to set content type (and this code block => it is not known), then
+              throw HttpMediaTypeNotAcceptableException with unknown format message
+        2.2.2 else (=> not derived from extension/format), throw HttpMediaTypeNotAcceptableException
+              with message unknown content type specified
+         */
+
         // if no accept header was added based on format/extension, then add default content type
         if (!mutated
                 && (Utils.nullOrEmpty(request.getHeader(HttpHeaders.ACCEPT))
