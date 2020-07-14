aac1366787e8ef3b86c068dac769d66a766c8d45
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-07-14 21:12:06 +0100

    add an option to run all collections tests

--- common-rest/src/test/postman/run_postman_test.sh
+++ common-rest/src/test/postman/run_postman_test.sh
@@ -5,60 +5,64 @@
 # please Refer to http://redsymbol.net/articles/unofcial-bash-strict-mode/ for details.
 set -euo pipefail
 
-case $request_type in
-uniprotkb)
-  echo "Running uniprotkb tests.."
-  newman run ../../../../uniprotkb-rest/src/test/postman/uniprotkb.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-proteome)
-  echo "Running proteome tests.."
-  newman run ../../../../proteome-rest/src/test/postman/proteome.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-configure)
-  echo "Running configuration tests.."
-  newman run ../../../../support-data-rest/src/test/postman/configure/configuration.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-crossref)
-  echo "Running crossref tests.."
-  newman run ../../../../support-data-rest/src/test/postman/crossref/crossref.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-disease)
-  echo "Running disease tests.."
-  newman run ../../../../support-data-rest/src/test/postman/disease/disease.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-keyword)
-  echo "Running keyword tests.."
-  newman run ../../../../support-data-rest/src/test/postman/keyword/keyword.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-literature)
-  echo "Running literature tests.."
-  newman run ../../../../support-data-rest/src/test/postman/literature/literature.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-subcell)
-  echo "Running subcell tests.."
-  newman run ../../../../support-data-rest/src/test/postman/subcell/subcellularlocation.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-suggester)
-  echo "Running suggester tests.."
-  newman run ../../../../support-data-rest/src/test/postman/suggester/suggester.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-taxonomy)
-  echo "Running taxonomy tests.."
-  newman run ../../../../support-data-rest/src/test/postman/taxonomy/taxonomy.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-uniparc)
-  echo "Running uniparc tests.."
-  newman run ../../../../uniparc-rest/src/test/postman/uniparc.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-uniref)
-  echo "Running uniref tests.."
-  newman run ../../../../uniref-rest/src/test/postman/uniref.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-unisave)
-  echo "Running unisave tests.."
-  newman run ../../../../unisave-rest/src/test/postman/unisave.postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
-  ;;
-*)
-  echo "Request type '$request_type' not supported. Exiting.."
-  ;;
-esac
+execute_newman_run() {
+  echo "Running $1 tests.."
+  newman run ../../../../"$1"-rest/src/test/postman/"$1".postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
+}
+
+execute_newman_run_for_supporting_data() {
+  echo "Running $1 tests.."
+  newman run ../../../../support-data-rest/src/test/postman/"$1"/"$1".postman_collection.json -e "$run_environment".postman_environment.json -g uniprot_workspace.postman_globals.json
+}
+
+if [ "$request_type" = "all" ]; then
+  echo "Running all collections one after another ...."
+  declare -a all_collections=('proteome' 'uniparc' 'uniprot' 'uniref' 'unisave' 'configure' 'crossref' 'disease' 'keyword'
+  'literature' 'subcell' 'suggester' 'taxonomy')
+  for collection in "${all_collections[@]}"; do execute_newman_run "$collection"; done
+else
+  case $request_type in
+  uniprotkb)
+    execute_newman_run uniprotkb
+    ;;
+  proteome)
+    execute_newman_run proteome
+    ;;
+  uniparc)
+    execute_newman_run uniparc
+    ;;
+  uniref)
+    execute_newman_run uniref
+    ;;
+  unisave)
+    execute_newman_run unisave
+    ;;
+  configure)
+    execute_newman_run_for_supporting_data configure
+    ;;
+  crossref)
+    execute_newman_run_for_supporting_data crossref
+    ;;
+  disease)
+    execute_newman_run_for_supporting_data disease
+    ;;
+  keyword)
+    execute_newman_run_for_supporting_data keyword
+    ;;
+  literature)
+    execute_newman_run_for_supporting_data literature
+    ;;
+  subcell)
+    execute_newman_run_for_supporting_data subcell
+    ;;
+  suggester)
+    execute_newman_run_for_supporting_data suggester
+    ;;
+  taxonomy)
+    execute_newman_run_for_supporting_data taxonomy
+    ;;
+  *)
+    echo "Request type '$request_type' not supported. Exiting.."
+    ;;
+  esac
+fi
