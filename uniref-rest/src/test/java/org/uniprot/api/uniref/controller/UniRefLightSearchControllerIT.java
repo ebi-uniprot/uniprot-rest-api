5264bd10582ec535365152b61b5ecbc8d7681288
lgonzales <lgonzales@ebi.ac.uk>
2020-08-04 12:53:57 +0100

    change from mvc print option to mvn log

--- uniref-rest/src/test/java/org/uniprot/api/uniref/controller/UniRefLightSearchControllerIT.java
+++ uniref-rest/src/test/java/org/uniprot/api/uniref/controller/UniRefLightSearchControllerIT.java
@@ -5,7 +5,6 @@ import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.uniprot.api.uniref.controller.UniRefControllerITUtils.*;
@@ -108,7 +107,7 @@ class UniRefLightSearchControllerIT extends AbstractSearchWithFacetControllerIT 
                                         .header(ACCEPT, APPLICATION_JSON_VALUE));
 
         // then
-        response.andDo(print())
+        response.andDo(log())
                 .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE))
                 .andExpect(
