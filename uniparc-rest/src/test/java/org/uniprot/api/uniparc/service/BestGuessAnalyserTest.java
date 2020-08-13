69ce644b468b1e05d996f4b3d15d0bea600ee457
lgonzales <lgonzales@ebi.ac.uk>
2020-08-13 10:50:41 +0100

    add test class

---
+++ uniparc-rest/src/test/java/org/uniprot/api/uniparc/service/BestGuessAnalyserTest.java
@@ -0,0 +1,16 @@
+package org.uniprot.api.uniparc.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author lgonzales
+ * @since 13/08/2020
+ */
+class BestGuessAnalyserTest {
+
+    @Test
+    void analyseBestGuess() {
+    }
+}
