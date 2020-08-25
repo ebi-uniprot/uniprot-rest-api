7bde898bf34632fa5699a73cc867e9c8c341ef63
eddturner <eddturner@gmai.com>
2020-08-25 12:33:39 +0100

    javadoc

--- common-rest/src/main/java/org/uniprot/api/rest/service/query/processor/UniProtQueryNodeProcessorPipeline.java
+++ common-rest/src/main/java/org/uniprot/api/rest/service/query/processor/UniProtQueryNodeProcessorPipeline.java
@@ -1,13 +1,23 @@
 package org.uniprot.api.rest.service.query.processor;
 
-import java.util.List;
-
+import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
+import org.apache.lucene.queryparser.flexible.core.parser.EscapeQuerySyntax;
 import org.apache.lucene.queryparser.flexible.core.processors.QueryNodeProcessorPipeline;
 import org.apache.lucene.queryparser.flexible.standard.config.StandardQueryConfigHandler;
 import org.uniprot.store.config.searchfield.model.SearchFieldItem;
 
+import java.util.List;
+
 /**
- * Created 22/08/2020
+ * This class contains query node processors, that act to manipulate the query. If we need to do any
+ * client query manipulation, we can add another processor and do it here.
+ *
+ * <p>The primary use case of this class is to manipulate a client's query into a query tree of
+ * {@link QueryNode} instances, which have overridden {@link
+ * QueryNode#toQueryString(EscapeQuerySyntax)} methods, which are called when getting the {@link
+ * String} representation fo the manipulated query.
+ *
+ * <p>Created 22/08/2020
  *
  * @author Edd
  */
