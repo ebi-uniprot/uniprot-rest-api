489851b992f4014226b1ba39b69f5906aa044374
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-01-27 16:11:10 +0000

    add few more test cases

--- common-rest/src/test/java/org/uniprot/api/common/repository/stream/rdf/DefaultRDFStreamerTest.java
+++ common-rest/src/test/java/org/uniprot/api/common/repository/stream/rdf/DefaultRDFStreamerTest.java
@@ -1,11 +1,5 @@
 package org.uniprot.api.common.repository.stream.rdf;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import net.jodah.failsafe.RetryPolicy;
 
 import org.junit.jupiter.api.Assertions;
@@ -13,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.uniprot.api.common.repository.search.SolrQueryRepository;
@@ -20,6 +15,12 @@ import org.uniprot.api.common.repository.search.SolrRequest;
 import org.uniprot.api.common.repository.stream.document.DefaultDocumentIdStream;
 import org.uniprot.api.common.repository.stream.document.TestDocument;
 import org.uniprot.api.rest.service.RDFService;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * @author sahmad
@@ -90,5 +91,54 @@ class DefaultRDFStreamerTest {
                         + "    <someMore>text3</someMore>\n"
                         + "</rdf:RDF>",
                 rdfString);
+    }
+
+    @Test
+    void testEmptyResponse(){
+        when(repository.getAll(any())).thenReturn(Stream.empty());
+
+        DefaultDocumentIdStream<TestDocument> idStream =
+                DefaultDocumentIdStream.<TestDocument>builder()
+                        .documentToId(TestDocument::getDocumentId)
+                        .repository(repository)
+                        .build();
+
+        RDFService<String> rdfService = new RDFService<>(restTemplate, String.class);
+
+        DefaultRDFStreamer.DefaultRDFStreamerBuilder<TestDocument> builder =
+                DefaultRDFStreamer.builder();
+        builder.idStream(idStream).rdfFetchRetryPolicy(new RetryPolicy<>().withMaxRetries(3));
+        builder.rdfProlog(RDF_PRELOG).rdfBatchSize(2).rdfService(rdfService);
+        // then
+        DefaultRDFStreamer<TestDocument> rdfStreamer = builder.build();
+        Stream<String> rdfStream = rdfStreamer.idsToRDFStoreStream(SolrRequest.builder().build());
+        String rdfString = rdfStream.collect(Collectors.joining());
+        String emptyResponse = "<?xml version='1.0' encoding='UTF-8'?>\n" +
+                "<rdf:RDF></rdf:RDF>";
+        Assertions.assertEquals(emptyResponse, rdfString);
+    }
+
+    @Test
+    void testExceptionFromServiceLayer() {
+        SolrRequest solrRequest = SolrRequest.builder().query("*:*").rows(5).totalRows(5).build();
+        when(repository.getAll(solrRequest)).thenReturn(Stream.of(new TestDocument("1", "n")));
+        DefaultDocumentIdStream<TestDocument> idStream =
+                DefaultDocumentIdStream.<TestDocument>builder()
+                        .documentToId(TestDocument::getDocumentId)
+                        .repository(repository)
+                        .build();
+        //RestClientException
+        when(restTemplate.getUriTemplateHandler()).thenReturn(new DefaultUriBuilderFactory());
+        when(restTemplate.getForObject(any(), any())).thenThrow(RestClientException.class);
+        RDFService<String> rdfService = new RDFService<>(restTemplate, String.class);
+
+        DefaultRDFStreamer.DefaultRDFStreamerBuilder<TestDocument> builder =
+                DefaultRDFStreamer.builder();
+        builder.idStream(idStream).rdfFetchRetryPolicy(new RetryPolicy<>().withMaxRetries(3));
+        builder.rdfProlog(RDF_PRELOG).rdfBatchSize(2).rdfService(rdfService);
+        // then
+        DefaultRDFStreamer<TestDocument> rdfStreamer = builder.build();
+        Assertions.assertThrows(RestClientException.class, () -> rdfStreamer.idsToRDFStoreStream(solrRequest)
+                .collect(Collectors.toList()));
     }
 }
