d753df0e2ac33e9579e435da6bf16304a7c91296
lgonzales <lgonzales@ebi.ac.uk>
2021-02-09 15:07:37 +0000

    add allDatabases endpoint for UniParcConfigureController

---
+++ support-data-rest/src/main/java/org/uniprot/api/support/data/configure/response/UniParcDatabaseDetail.java
@@ -0,0 +1,22 @@
+package org.uniprot.api.support.data.configure.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @author lgonzales
+ * @since 09/02/2021
+ */
+@Builder
+@Getter
+@AllArgsConstructor
+public class UniParcDatabaseDetail {
+
+    private final String name;
+
+    private final String displayName;
+
+    private final String uriLink;
+
+}
