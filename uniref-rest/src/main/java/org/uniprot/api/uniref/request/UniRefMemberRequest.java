3e8eae7b8850a6695d352a6130b6ec1667a4bc33
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-02-16 12:20:52 +0000

    allow size>=0 for uniref members

--- uniref-rest/src/main/java/org/uniprot/api/uniref/request/UniRefMemberRequest.java
+++ uniref-rest/src/main/java/org/uniprot/api/uniref/request/UniRefMemberRequest.java
@@ -5,7 +5,7 @@ import static org.uniprot.api.rest.request.SearchRequest.MAX_RESULTS_SIZE;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 
 import lombok.Data;
 
@@ -45,7 +45,7 @@ public class UniRefMemberRequest {
     private String cursor;
 
     @Parameter(description = "Size of the result. Defaults to 25")
-    @Positive(message = "{search.positive}")
+    @PositiveOrZero(message = "{search.positive.or.zero}")
     @Max(value = MAX_RESULTS_SIZE, message = "{search.max.page.size}")
     private Integer size;
 }
