2e34d6f6df4dbb6f5098c53ab9ad618ee6ad2c69
eddturner <eddturner@gmai.com>
2021-02-23 15:14:53 +0000

    implementing EhCacheMappingJobService

--- idmapping/src/main/java/org/uniprot/api/idmapping/service/cache/impl/EhCacheMappingJobService.java
+++ idmapping/src/main/java/org/uniprot/api/idmapping/service/cache/impl/EhCacheMappingJobService.java
@@ -4,6 +4,8 @@ import org.springframework.cache.Cache;
 import org.uniprot.api.idmapping.model.IdMappingJob;
 import org.uniprot.api.idmapping.service.cache.IdMappingJobService;
 
+import java.util.Objects;
+
 /**
  * Created 23/02/2021
  *
@@ -18,21 +20,21 @@ public class EhCacheMappingJobService implements IdMappingJobService {
 
     @Override
     public void put(String key, IdMappingJob value) {
-
+        this.cache.put(key, value);
     }
 
     @Override
     public IdMappingJob get(String key) {
-        return null;
+        return this.cache.get(key, IdMappingJob.class);
     }
 
     @Override
     public boolean exists(String key) {
-        return false;
+        return Objects.isNull(this.cache.get(key, IdMappingJob.class));
     }
 
     @Override
     public void delete(String key) {
-
+        this.cache.evictIfPresent(key);
     }
 }
