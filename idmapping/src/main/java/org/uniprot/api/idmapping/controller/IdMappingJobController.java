a44a8a96c6bf4609ff9bfa11262054e0728bc44f
eddturner <eddturner@gmai.com>
2021-02-23 09:33:03 +0000

    a bit more stubbing

--- idmapping/src/main/java/org/uniprot/api/idmapping/controller/IdMappingJobController.java
+++ idmapping/src/main/java/org/uniprot/api/idmapping/controller/IdMappingJobController.java
@@ -1,16 +1,21 @@
 package org.uniprot.api.idmapping.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.uniprot.api.idmapping.controller.IdMappingJobController.IDMAPPING_PATH;
-
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.uniprot.api.idmapping.controller.request.IdMappingRequest;
+import org.uniprot.api.idmapping.controller.response.JobStatus;
+import org.uniprot.api.idmapping.controller.response.JobStatusResponse;
 import org.uniprot.api.idmapping.controller.response.JobSubmitResponse;
 
 import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.uniprot.api.idmapping.controller.IdMappingJobController.IDMAPPING_PATH;
 
 /**
  * @author sahmad
@@ -25,7 +30,7 @@ public class IdMappingJobController {
             value = "/run",
             produces = {APPLICATION_JSON_VALUE})
     public JobSubmitResponse submitJob(IdMappingRequest request) {
-        String jobId = UUID.randomUUID().toString();//TODO call the service layer
+        String jobId = UUID.randomUUID().toString(); // TODO call the service layer
         JobSubmitResponse response = new JobSubmitResponse(jobId);
         return response;
     }
@@ -34,7 +39,39 @@ public class IdMappingJobController {
     @GetMapping(
             value = "/status/{jobId}",
             produces = {APPLICATION_JSON_VALUE})
-    public Object getStatus(String jobId) {
-        return null;
+    public ResponseEntity<JobStatusResponse> getStatus(String jobId) {
+
+        JobStatus jobStatus =
+                getJobStatus(jobId); // retrieve status from cache, but pretend it's running
+        try {
+            if (jobStatus == JobStatus.RUNNING) {
+                return ResponseEntity.ok(new JobStatusResponse(JobStatus.RUNNING));
+            } else if (jobStatus == JobStatus.FINISHED) {
+                String redirectUrl = "http://myapi/result/" + jobId;
+                return ResponseEntity.status(HttpStatus.SEE_OTHER)
+                        .header(HttpHeaders.LOCATION, redirectUrl)
+                        .body(new JobStatusResponse(JobStatus.FINISHED));
+            } else if (jobStatus == JobStatus.NOT_FOUND) {
+                return ResponseEntity.ok(new JobStatusResponse(JobStatus.NOT_FOUND));
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body(new JobStatusResponse(JobStatus.ERROR));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new JobStatusResponse(JobStatus.ERROR));
+        }
+    }
+
+    private JobStatus getJobStatus(String jobId) {
+        // this should retrieve data from cache, but is stubbed for now
+        if (jobId.startsWith("R")) {
+            return JobStatus.RUNNING;
+        } else if (jobId.startsWith("F")) {
+            return JobStatus.FINISHED;
+        } else if (jobId.startsWith("N")) {
+            return JobStatus.NOT_FOUND;
+        }
+        return JobStatus.ERROR;
     }
 }
