49f72c65e375c9a6d0999e69908cfc5c83272ab8
eddturner <eddturner@gmai.com>
2021-02-25 10:32:41 +0000

    pull ResponseEntity logic into controller

--- idmapping/src/main/java/org/uniprot/api/idmapping/controller/IdMappingJobController.java
+++ idmapping/src/main/java/org/uniprot/api/idmapping/controller/IdMappingJobController.java
@@ -1,20 +1,23 @@
 package org.uniprot.api.idmapping.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.uniprot.api.idmapping.controller.IdMappingJobController.IDMAPPING_PATH;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.uniprot.api.idmapping.controller.request.IdMappingBasicRequest;
+import org.uniprot.api.idmapping.controller.response.JobStatus;
 import org.uniprot.api.idmapping.controller.response.JobStatusResponse;
 import org.uniprot.api.idmapping.controller.response.JobSubmitResponse;
+import org.uniprot.api.idmapping.model.IdMappingJob;
 import org.uniprot.api.idmapping.service.IdMappingJobService;
+
+import javax.validation.Valid;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.uniprot.api.idmapping.controller.IdMappingJobController.IDMAPPING_PATH;
 
 /**
  * @author sahmad
@@ -44,6 +47,31 @@ public class IdMappingJobController {
             value = "/status/{jobId}",
             produces = {APPLICATION_JSON_VALUE})
     public ResponseEntity<JobStatusResponse> getStatus(@PathVariable String jobId) {
-        return idMappingJobService.getStatus(jobId);
+        return createStatus(idMappingJobService.getCompletedJob(jobId));
+    }
+
+    public ResponseEntity<JobStatusResponse> createStatus(IdMappingJob job) {
+        ResponseEntity<JobStatusResponse> response;
+        switch (job.getJobStatus()) {
+            case NEW:
+            case RUNNING:
+                response = ResponseEntity.ok(new JobStatusResponse(job.getJobStatus()));
+                break;
+            case FINISHED:
+                String redirectUrl = idMappingJobService.getRedirectPathToResults(job);
+
+                response =
+                        ResponseEntity.status(HttpStatus.SEE_OTHER)
+                                .header(HttpHeaders.LOCATION, redirectUrl)
+                                .body(new JobStatusResponse(JobStatus.FINISHED));
+                break;
+            default:
+                response =
+                        ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body(new JobStatusResponse(JobStatus.ERROR));
+                break;
+        }
+
+        return response;
     }
 }
--- idmapping/src/main/java/org/uniprot/api/idmapping/service/IdMappingJobService.java
+++ idmapping/src/main/java/org/uniprot/api/idmapping/service/IdMappingJobService.java
@@ -1,24 +1,21 @@
 package org.uniprot.api.idmapping.service;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Date;
-import java.util.Set;
-import java.util.concurrent.BlockingQueue;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
+import org.uniprot.api.common.exception.ResourceNotFoundException;
 import org.uniprot.api.idmapping.controller.request.IdMappingBasicRequest;
 import org.uniprot.api.idmapping.controller.response.JobStatus;
-import org.uniprot.api.idmapping.controller.response.JobStatusResponse;
 import org.uniprot.api.idmapping.controller.response.JobSubmitResponse;
 import org.uniprot.api.idmapping.model.IdMappingJob;
 import org.uniprot.api.idmapping.service.cache.IdMappingJobCacheService;
 import org.uniprot.api.idmapping.service.job.AsyncJobProducer;
 import org.uniprot.api.idmapping.service.job.JobTask;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * @author sahmad
@@ -71,38 +68,15 @@ public class IdMappingJobService {
         return new JobSubmitResponse(jobId);
     }
 
-    public ResponseEntity<JobStatusResponse> getStatus(String jobId) {
-        ResponseEntity<JobStatusResponse> response;
+    public IdMappingJob getCompletedJob(String jobId) {
         if (this.cacheService.exists(jobId)) {
-            IdMappingJob job = this.cacheService.get(jobId);
-            switch (job.getJobStatus()) {
-                case NEW:
-                case RUNNING:
-                    response = ResponseEntity.ok(new JobStatusResponse(job.getJobStatus()));
-                    break;
-                case FINISHED:
-                    String redirectUrl = getRedirectPathToResults(job);
-
-                    response =
-                            ResponseEntity.status(HttpStatus.SEE_OTHER)
-                                    .header(HttpHeaders.LOCATION, redirectUrl)
-                                    .body(new JobStatusResponse(JobStatus.FINISHED));
-                    break;
-                default:
-                    response =
-                            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                    .body(new JobStatusResponse(JobStatus.ERROR));
-                    break;
-            }
-
+            return this.cacheService.get(jobId);
         } else {
-            response = ResponseEntity.notFound().build();
+            throw new ResourceNotFoundException("Id mapping job identifier does not exist.");
         }
-
-        return response;
     }
 
-    private String getRedirectPathToResults(IdMappingJob job) {
+    public String getRedirectPathToResults(IdMappingJob job) {
         String toDB = job.getIdMappingRequest().getTo();
         String dbType = "";
         if (Set.of("NF50", "NF90", "NF100").contains(toDB)) {
