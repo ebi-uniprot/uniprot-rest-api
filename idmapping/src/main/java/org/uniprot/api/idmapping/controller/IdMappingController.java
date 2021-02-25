53b48fcd00b5e095061822c3473b0feb51b5fb31
eddturner <eddturner@gmai.com>
2021-02-25 14:36:08 +0000

    add /stream/{jobId} for IdMappingController

--- idmapping/src/main/java/org/uniprot/api/idmapping/controller/IdMappingController.java
+++ idmapping/src/main/java/org/uniprot/api/idmapping/controller/IdMappingController.java
@@ -1,27 +1,17 @@
 package org.uniprot.api.idmapping.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.uniprot.api.rest.output.UniProtMediaType.TSV_MEDIA_TYPE_VALUE;
-import static org.uniprot.api.rest.output.UniProtMediaType.XLS_MEDIA_TYPE_VALUE;
-import static org.uniprot.api.rest.output.context.MessageConverterContextFactory.Resource.IDMAPPING_PIR;
-
-import java.util.Optional;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.uniprot.api.common.repository.search.QueryResult;
-import org.uniprot.api.idmapping.controller.request.IdMappingBasicRequest;
 import org.uniprot.api.idmapping.controller.request.IdMappingPageRequest;
 import org.uniprot.api.idmapping.model.IdMappingJob;
-import org.uniprot.api.idmapping.model.IdMappingResult;
 import org.uniprot.api.idmapping.model.IdMappingStringPair;
 import org.uniprot.api.idmapping.service.IdMappingJobService;
 import org.uniprot.api.idmapping.service.IdMappingPIRService;
@@ -29,6 +19,16 @@ import org.uniprot.api.idmapping.service.cache.IdMappingJobCacheService;
 import org.uniprot.api.rest.controller.BasicSearchController;
 import org.uniprot.api.rest.output.context.MessageConverterContext;
 import org.uniprot.api.rest.output.context.MessageConverterContextFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.Optional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.uniprot.api.rest.output.UniProtMediaType.TSV_MEDIA_TYPE_VALUE;
+import static org.uniprot.api.rest.output.UniProtMediaType.XLS_MEDIA_TYPE_VALUE;
+import static org.uniprot.api.rest.output.context.MessageConverterContextFactory.Resource.IDMAPPING_PIR;
 
 /**
  * Created 15/02/2021
@@ -72,17 +72,14 @@ public class IdMappingController extends BasicSearchController<IdMappingStringPa
         return super.getSearchResponse(queryResult, null, request, response);
     }
 
-    // TODO: 25/02/2021 NOT IMPLEMENTED
-    @PostMapping(
-            value = "/stream",
+    @GetMapping(
+            value = "/stream/{jobId}",
             produces = {TSV_MEDIA_TYPE_VALUE, APPLICATION_JSON_VALUE, XLS_MEDIA_TYPE_VALUE})
-    public ResponseEntity<MessageConverterContext<IdMappingStringPair>> stream(
-            @Valid @ModelAttribute IdMappingBasicRequest mappingRequest,
-            HttpServletRequest request,
-            HttpServletResponse response) {
-        IdMappingResult idMappingResult = pirIdMapping(mappingRequest);
+    public ResponseEntity<MessageConverterContext<IdMappingStringPair>> streamResults(
+            @PathVariable String jobId, HttpServletRequest request, HttpServletResponse response) {
+        IdMappingJob completedJob = jobService.getCompletedJobAsResource(jobId);
         QueryResult<IdMappingStringPair> queryResult =
-                idMappingService.queryResultAll(idMappingResult);
+                idMappingService.queryResultAll(completedJob.getIdMappingResult());
         return super.getSearchResponse(queryResult, null, request, response);
     }
 
@@ -94,15 +91,5 @@ public class IdMappingController extends BasicSearchController<IdMappingStringPa
     @Override
     protected Optional<String> getEntityRedirectId(IdMappingStringPair entity) {
         return Optional.empty();
-    }
-
-    private IdMappingResult pirIdMapping(IdMappingBasicRequest request) {
-        IdMappingBasicRequest pirRequest = new IdMappingBasicRequest();
-        pirRequest.setFrom(request.getFrom());
-        pirRequest.setTo(request.getTo());
-        pirRequest.setIds(request.getIds());
-        pirRequest.setTaxId(request.getTaxId());
-
-        return idMappingService.mapIds(pirRequest);
     }
 }
