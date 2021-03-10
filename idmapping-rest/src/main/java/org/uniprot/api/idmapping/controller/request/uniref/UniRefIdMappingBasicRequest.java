d786798c8377f5fdb36c8a5971a2e83dbbdf106e
eddturner <eddturner@gmail.com>
2021-03-10 15:42:55 +0000

    add todo

--- idmapping-rest/src/main/java/org/uniprot/api/idmapping/controller/request/uniref/UniRefIdMappingBasicRequest.java
+++ idmapping-rest/src/main/java/org/uniprot/api/idmapping/controller/request/uniref/UniRefIdMappingBasicRequest.java
@@ -23,8 +23,8 @@ import io.swagger.v3.oas.annotations.Parameter;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class UniRefIdMappingBasicRequest extends IdMappingPageRequest {
-
     @ModelFieldMeta(reader = QueryFieldMetaReaderImpl.class, path = "uniref-search-fields.json")
+    // TODO: 10/03/2021 replace "solr" for "lucene"
     @Parameter(description = "Criteria to search the proteins. It can take any valid solr query.")
     @ValidSolrQuerySyntax(message = "{search.invalid.query}")
     @ValidSolrQueryFields(uniProtDataType = UniProtDataType.UNIREF, messagePrefix = "search.uniref")
