fa7ea8566ed5bf29d7c608afc1d5b6411dd163b3
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-03-16 17:53:10 +0000

    update configure idmapping api after talking to frontend team

--- support-data-rest/src/main/java/org/uniprot/api/support/data/configure/response/IdMappingField.java
+++ support-data-rest/src/main/java/org/uniprot/api/support/data/configure/response/IdMappingField.java
@@ -1,9 +1,11 @@
 package org.uniprot.api.support.data.configure.response;
 
 import java.io.Serializable;
+import java.util.List;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Singular;
 
 /**
  * @author sahmad
@@ -12,10 +14,28 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class IdMappingField implements Serializable {
-    private String groupName;
-    private String displayName;
-    private String name;
-    private boolean from;
-    private boolean to;
-    private Integer ruleId;
+    private static final long serialVersionUID = 2187523299497519033L;
+    @Singular private List<Field> fields;
+    private List<Rule> rules;
+
+    @Data
+    @Builder(toBuilder = true)
+    public static class Field implements Serializable {
+        private static final long serialVersionUID = 3296274672007139133L;
+        private String groupName;
+        private String displayName;
+        private String name;
+        private boolean from;
+        private boolean to;
+        private Integer ruleId;
+    }
+
+    @Data
+    @Builder(toBuilder = true)
+    public static class Rule implements Serializable {
+        private static final long serialVersionUID = 1847092547405960734L;
+        private Integer ruleId;
+        @Singular private List<String> tos;
+        private boolean taxonId;
+    }
 }
--- support-data-rest/src/test/java/org/uniprot/api/support/data/configure/controller/IdMappingConfigureControllerIT.java
+++ support-data-rest/src/test/java/org/uniprot/api/support/data/configure/controller/IdMappingConfigureControllerIT.java
@@ -1,5 +1,6 @@
 package org.uniprot.api.support.data.configure.controller;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.iterableWithSize;
 import static org.springframework.http.HttpHeaders.ACCEPT;
@@ -42,63 +43,84 @@ class IdMappingConfigureControllerIT {
         response.andDo(print())
                 .andExpect(status().is(HttpStatus.OK.value()))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()", is(95)))
-                .andExpect(jsonPath("$[?(@.groupName=='UniProt')]", iterableWithSize(9)))
-                .andExpect(jsonPath("$[?(@.groupName=='Sequence databases')]", iterableWithSize(3)))
+                .andExpect(jsonPath("$.fields.length()", is(95)))
+                .andExpect(jsonPath("$.fields.[?(@.groupName=='UniProt')]", iterableWithSize(9)))
                 .andExpect(
                         jsonPath(
-                                "$[?(@.groupName=='3D structure databases')]", iterableWithSize(1)))
+                                "$.fields.[?(@.groupName=='Sequence databases')]",
+                                iterableWithSize(3)))
                 .andExpect(
                         jsonPath(
-                                "$[?(@.groupName=='Protein-protein interaction databases')]",
-                                iterableWithSize(4)))
-                .andExpect(jsonPath("$[?(@.groupName=='Chemistry')]", iterableWithSize(4)))
-                .andExpect(
-                        jsonPath(
-                                "$[?(@.groupName=='Protein family/group databases')]",
-                                iterableWithSize(8)))
-                .andExpect(jsonPath("$[?(@.groupName=='PTM databases')]", iterableWithSize(1)))
-                .andExpect(
-                        jsonPath(
-                                "$[?(@.groupName=='Polymorphism and mutation databases')]",
-                                iterableWithSize(2)))
-                .andExpect(jsonPath("$[?(@.groupName=='2D gel databases')]", iterableWithSize(1)))
-                .andExpect(
-                        jsonPath("$[?(@.groupName=='Proteomic databases')]", iterableWithSize(2)))
-                .andExpect(
-                        jsonPath(
-                                "$[?(@.groupName=='Protocols and materials databases')]",
+                                "$.fields.[?(@.groupName=='3D structure databases')]",
                                 iterableWithSize(1)))
                 .andExpect(
                         jsonPath(
-                                "$[?(@.groupName=='Genome annotation databases')]",
+                                "$.fields.[?(@.groupName=='Protein-protein interaction databases')]",
+                                iterableWithSize(4)))
+                .andExpect(jsonPath("$.fields.[?(@.groupName=='Chemistry')]", iterableWithSize(4)))
+                .andExpect(
+                        jsonPath(
+                                "$.fields.[?(@.groupName=='Protein family/group databases')]",
+                                iterableWithSize(8)))
+                .andExpect(
+                        jsonPath("$.fields.[?(@.groupName=='PTM databases')]", iterableWithSize(1)))
+                .andExpect(
+                        jsonPath(
+                                "$.fields.[?(@.groupName=='Polymorphism and mutation databases')]",
+                                iterableWithSize(2)))
+                .andExpect(
+                        jsonPath(
+                                "$.fields.[?(@.groupName=='2D gel databases')]",
+                                iterableWithSize(1)))
+                .andExpect(
+                        jsonPath(
+                                "$.fields.[?(@.groupName=='Proteomic databases')]",
+                                iterableWithSize(2)))
+                .andExpect(
+                        jsonPath(
+                                "$.fields.[?(@.groupName=='Protocols and materials databases')]",
+                                iterableWithSize(1)))
+                .andExpect(
+                        jsonPath(
+                                "$.fields.[?(@.groupName=='Genome annotation databases')]",
                                 iterableWithSize(12)))
                 .andExpect(
                         jsonPath(
-                                "$[?(@.groupName=='Organism-specific databases')]",
+                                "$.fields.[?(@.groupName=='Organism-specific databases')]",
                                 iterableWithSize(32)))
                 .andExpect(
                         jsonPath(
-                                "$[?(@.groupName=='Phylogenomic databases')]", iterableWithSize(6)))
+                                "$.fields.[?(@.groupName=='Phylogenomic databases')]",
+                                iterableWithSize(6)))
                 .andExpect(
                         jsonPath(
-                                "$[?(@.groupName=='Enzyme and pathway databases')]",
+                                "$.fields.[?(@.groupName=='Enzyme and pathway databases')]",
                                 iterableWithSize(4)))
-                .andExpect(jsonPath("$[?(@.groupName=='Other')]", iterableWithSize(3)))
+                .andExpect(jsonPath("$.fields.[?(@.groupName=='Other')]", iterableWithSize(3)))
                 .andExpect(
                         jsonPath(
-                                "$[?(@.groupName=='Gene expression databases')]",
+                                "$.fields.[?(@.groupName=='Gene expression databases')]",
                                 iterableWithSize(1)))
-                .andExpect(jsonPath("$[?(@.groupName=='Sequence databases')]", iterableWithSize(3)))
-                .andExpect(jsonPath("$[?(@.ruleId==1)]", iterableWithSize(1)))
-                .andExpect(jsonPath("$[?(@.ruleId==2)]", iterableWithSize(4)))
-                .andExpect(jsonPath("$[?(@.ruleId==3)]", iterableWithSize(1)))
-                .andExpect(jsonPath("$[?(@.ruleId==4)]", iterableWithSize(87)))
-                .andExpect(jsonPath("$[?(@.ruleId>=5)]", iterableWithSize(0)))
-                .andExpect(jsonPath("$[?(@.ruleId<=0)]", iterableWithSize(0)))
-                .andExpect(jsonPath("$[?(@.from==true)]", iterableWithSize(93)))
-                .andExpect(jsonPath("$[?(@.from==false)]", iterableWithSize(2)))
-                .andExpect(jsonPath("$[?(@.to==true)]", iterableWithSize(94)))
-                .andExpect(jsonPath("$[?(@.to==false)]", iterableWithSize(1)));
+                .andExpect(
+                        jsonPath(
+                                "$.fields.[?(@.groupName=='Sequence databases')]",
+                                iterableWithSize(3)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId==1)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId==2)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId==3)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId==4)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId==5)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId==6)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId==7)]", iterableWithSize(87)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId>=8)]", iterableWithSize(0)))
+                .andExpect(jsonPath("$.fields.[?(@.ruleId<=0)]", iterableWithSize(0)))
+                .andExpect(jsonPath("$.fields.[?(@.from==true)]", iterableWithSize(93)))
+                .andExpect(jsonPath("$.fields.[?(@.from==false)]", iterableWithSize(2)))
+                .andExpect(jsonPath("$.fields.[?(@.to==true)]", iterableWithSize(94)))
+                .andExpect(jsonPath("$.fields.[?(@.to==false)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.rules.length()", is(7)))
+                .andExpect(jsonPath("$.rules.[?(@.taxonId==false)]", iterableWithSize(6)))
+                .andExpect(jsonPath("$.rules.[?(@.taxonId==true)]", iterableWithSize(1)))
+                .andExpect(jsonPath("$.rules.*.ruleId", containsInAnyOrder(1, 2, 3, 4, 5, 6, 7)));
     }
 }
