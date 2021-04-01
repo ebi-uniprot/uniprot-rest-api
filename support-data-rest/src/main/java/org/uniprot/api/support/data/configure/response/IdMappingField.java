82274fa8ed7e61b1d7d32847cd5a11707c1116ff
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-04-01 16:10:05 +0100

    return the fields in order

--- support-data-rest/src/main/java/org/uniprot/api/support/data/configure/response/IdMappingField.java
+++ support-data-rest/src/main/java/org/uniprot/api/support/data/configure/response/IdMappingField.java
@@ -1,5 +1,7 @@
 package org.uniprot.api.support.data.configure.response;
 
+import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.UNIPROTKB_STR;
+
 import java.io.Serializable;
 import java.util.List;
 
@@ -15,14 +17,21 @@ import lombok.Singular;
 @Builder(toBuilder = true)
 public class IdMappingField implements Serializable {
     private static final long serialVersionUID = 2187523299497519033L;
-    @Singular private List<Field> fields;
+    @Singular private List<Group> groups;
     private List<Rule> rules;
+
+    @Data
+    @Builder(toBuilder = true)
+    public static class Group implements Serializable {
+        private static final long serialVersionUID = 4229413172365901286L;
+        private String groupName;
+        private List<Field> items;
+    }
 
     @Data
     @Builder(toBuilder = true)
     public static class Field implements Serializable {
         private static final long serialVersionUID = 3296274672007139133L;
-        private String groupName;
         private String displayName;
         private String name;
         private boolean from;
@@ -36,6 +45,7 @@ public class IdMappingField implements Serializable {
         private static final long serialVersionUID = 1847092547405960734L;
         private Integer ruleId;
         @Singular private List<String> tos;
+        @Builder.Default private String defaultTo = UNIPROTKB_STR;
         private boolean taxonId;
     }
 }
--- support-data-rest/src/main/java/org/uniprot/api/support/data/configure/service/IdMappingConfigureService.java
+++ support-data-rest/src/main/java/org/uniprot/api/support/data/configure/service/IdMappingConfigureService.java
@@ -1,7 +1,6 @@
 package org.uniprot.api.support.data.configure.service;
 
 import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.GENE_NAME_STR;
-import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.PIR_CRC64;
 import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.UNIPARC_STR;
 import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.UNIPROTKB_AC_ID_STR;
 import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.UNIPROTKB_STR;
@@ -10,10 +9,14 @@ import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.UNIREF_100
 import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.UNIREF_50_STR;
 import static org.uniprot.store.config.idmapping.IdMappingFieldConfig.UNIREF_90_STR;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.uniprot.api.support.data.configure.response.IdMappingField;
+import org.uniprot.core.cv.xdb.UniProtDatabaseCategory;
 import org.uniprot.core.cv.xdb.UniProtDatabaseDetail;
 import org.uniprot.store.config.idmapping.IdMappingFieldConfig;
 
@@ -24,6 +27,7 @@ import org.uniprot.store.config.idmapping.IdMappingFieldConfig;
 @Service
 public class IdMappingConfigureService {
     private static final String UNIPROT_STR = "UniProt";
+
     // ruleId = 1, from UniProtKB AC/ID to any other db type where to=true
     // ruleIds = 2,3,4,5, from UniParc, UniRef50, UniRef90 or UniRef100 to
     // <UniProtKB, UniProtKB/Swiss-Prot, UniParc(2)/UniRef50(3)/UniRef90(4)/UniRef100(5)>
@@ -32,6 +36,40 @@ public class IdMappingConfigureService {
     public IdMappingField getIdMappingFields() {
         List<UniProtDatabaseDetail> allIdMappingTypes = IdMappingFieldConfig.getAllIdMappingTypes();
         IdMappingField.IdMappingFieldBuilder builder = IdMappingField.builder();
+        // groups
+        List<IdMappingField.Group> dbGroups = getDatabaseGroups(allIdMappingTypes);
+        // add UniProt group as the first item in the list
+        IdMappingField.Group unknownGroup =
+                dbGroups.stream()
+                        .filter(
+                                grp ->
+                                        UniProtDatabaseCategory.UNKNOWN
+                                                .getDisplayName()
+                                                .equals(grp.getGroupName()))
+                        .findFirst()
+                        .orElse(IdMappingField.Group.builder().build());
+        dbGroups.removeIf(
+                grp -> UniProtDatabaseCategory.UNKNOWN.getDisplayName().equals(grp.getGroupName()));
+        dbGroups.add(
+                0,
+                IdMappingField.Group.builder()
+                        .groupName(UNIPROT_STR)
+                        .items(unknownGroup.getItems())
+                        .build());
+        // rules
+        List<IdMappingField.Rule> rules = getRules(allIdMappingTypes);
+        return builder.groups(dbGroups).rules(rules).build();
+    }
+
+    private List<IdMappingField.Group> getDatabaseGroups(
+            List<UniProtDatabaseDetail> allIdMappingTypes) {
+        return Arrays.stream(UniProtDatabaseCategory.values())
+                .map(dbCat -> getDatabaseGroup(dbCat, allIdMappingTypes))
+                .filter(dbGroup -> !dbGroup.getItems().isEmpty())
+                .collect(Collectors.toList());
+    }
+
+    private List<IdMappingField.Rule> getRules(List<UniProtDatabaseDetail> allIdMappingTypes) {
         IdMappingField.Rule.RuleBuilder rule1Builder = IdMappingField.Rule.builder().ruleId(1);
         IdMappingField.Rule.RuleBuilder rule2Builder = IdMappingField.Rule.builder().ruleId(2);
         IdMappingField.Rule.RuleBuilder rule3Builder = IdMappingField.Rule.builder().ruleId(3);
@@ -40,44 +78,28 @@ public class IdMappingConfigureService {
         IdMappingField.Rule.RuleBuilder rule6Builder =
                 IdMappingField.Rule.builder().ruleId(6).taxonId(true);
         IdMappingField.Rule.RuleBuilder rule7Builder = IdMappingField.Rule.builder().ruleId(7);
-
         for (UniProtDatabaseDetail detail : allIdMappingTypes) {
-            IdMappingField.Field.FieldBuilder fieldBuilder = IdMappingField.Field.builder();
             switch (detail.getDisplayName()) {
                 case UNIPROTKB_AC_ID_STR:
-                    fieldBuilder.groupName(UNIPROT_STR).name(detail.getName());
-                    fieldBuilder.displayName(detail.getDisplayName()).ruleId(1);
-                    fieldBuilder.from(true).to(false);
                     break;
                 case UNIPARC_STR:
-                    fieldBuilder = createFieldBuilder(detail, 2);
                     rule1Builder.to(detail.getName());
                     rule2Builder.to(detail.getName());
                     break;
                 case UNIREF_50_STR:
-                    fieldBuilder = createFieldBuilder(detail, 3);
                     rule1Builder.to(detail.getName());
                     rule3Builder.to(detail.getName());
                     break;
                 case UNIREF_90_STR:
-                    fieldBuilder = createFieldBuilder(detail, 4);
                     rule1Builder.to(detail.getName());
                     rule4Builder.to(detail.getName());
                     break;
                 case UNIREF_100_STR:
-                    fieldBuilder = createFieldBuilder(detail, 5);
                     rule1Builder.to(detail.getName());
                     rule5Builder.to(detail.getName());
                     break;
-                case GENE_NAME_STR:
-                    fieldBuilder = createFieldBuilder(detail, 6);
-                    rule1Builder.to(detail.getName());
-                    break;
                 case UNIPROTKB_SWISS_STR:
                 case UNIPROTKB_STR:
-                    fieldBuilder.groupName(UNIPROT_STR).name(detail.getName());
-                    fieldBuilder.displayName(detail.getDisplayName());
-                    fieldBuilder.from(false).to(true);
                     rule1Builder.to(detail.getName());
                     rule2Builder.to(detail.getName());
                     rule3Builder.to(detail.getName());
@@ -86,39 +108,96 @@ public class IdMappingConfigureService {
                     rule6Builder.to(detail.getName());
                     rule7Builder.to(detail.getName());
                     break;
-                case PIR_CRC64:
-                    fieldBuilder = createFieldBuilder(detail, 7);
-                    rule1Builder.to(detail.getName());
-                    break;
                 default:
-                    fieldBuilder
-                            .groupName(detail.getCategory().getDisplayName())
-                            .name(detail.getName());
-                    fieldBuilder.displayName(detail.getDisplayName());
-                    fieldBuilder.from(true).to(true).ruleId(7);
                     rule1Builder.to(detail.getName());
             }
-            builder.field(fieldBuilder.build());
         }
-        builder.rules(
-                List.of(
-                        rule1Builder.build(),
-                        rule2Builder.build(),
-                        rule3Builder.build(),
-                        rule4Builder.build(),
-                        rule5Builder.build(),
-                        rule6Builder.build(),
-                        rule7Builder.build()));
-
-        return builder.build();
+        return List.of(
+                rule1Builder.build(),
+                rule2Builder.build(),
+                rule3Builder.build(),
+                rule4Builder.build(),
+                rule5Builder.build(),
+                rule6Builder.build(),
+                rule7Builder.build());
     }
 
-    private IdMappingField.Field.FieldBuilder createFieldBuilder(
-            UniProtDatabaseDetail detail, int ruleId) {
+    private IdMappingField.Group getDatabaseGroup(
+            UniProtDatabaseCategory dbCategory, List<UniProtDatabaseDetail> allIdMappingTypes) {
+        List<UniProtDatabaseDetail> dbDetails =
+                getDatabaseDetailsByCategory(dbCategory, allIdMappingTypes);
+        List<IdMappingField.Field> items;
+        if (dbCategory == UniProtDatabaseCategory.UNKNOWN) {
+            items = dbDetails.stream().map(this::convertToField).collect(Collectors.toList());
+        } else {
+            items =
+                    dbDetails.stream()
+                            .map(this::convertToField)
+                            .sorted(
+                                    Comparator.comparing(
+                                            IdMappingField.Field::getDisplayName,
+                                            String.CASE_INSENSITIVE_ORDER))
+                            .collect(Collectors.toList());
+        }
+        return IdMappingField.Group.builder()
+                .groupName(dbCategory.getDisplayName())
+                .items(items)
+                .build();
+    }
+
+    private List<UniProtDatabaseDetail> getDatabaseDetailsByCategory(
+            UniProtDatabaseCategory dbCategory, List<UniProtDatabaseDetail> allIdMappingTypes) {
+        return allIdMappingTypes.stream()
+                .filter(dbType -> dbType.getCategory() == dbCategory)
+                .collect(Collectors.toList());
+    }
+
+    private IdMappingField.Field convertToField(UniProtDatabaseDetail detail) {
         IdMappingField.Field.FieldBuilder fieldBuilder = IdMappingField.Field.builder();
-        fieldBuilder.groupName(UNIPROT_STR).name(detail.getName());
-        fieldBuilder.displayName(detail.getDisplayName()).ruleId(ruleId);
+        fieldBuilder.name(detail.getName());
+        fieldBuilder
+                .displayName(detail.getDisplayName())
+                .ruleId(getRuleId(detail.getDisplayName()));
         fieldBuilder.from(true).to(true);
-        return fieldBuilder;
+
+        if (UNIPROTKB_AC_ID_STR.equals(detail.getDisplayName())) { // special cases
+            fieldBuilder.to(false);
+        } else if (UNIPROTKB_SWISS_STR.equals(detail.getDisplayName())
+                || UNIPROTKB_STR.equals(detail.getDisplayName())) {
+            fieldBuilder.from(false);
+        }
+
+        return fieldBuilder.build();
+    }
+
+    private Integer getRuleId(String displayName) {
+        Integer ruleId;
+        switch (displayName) {
+            case UNIPROTKB_AC_ID_STR:
+                ruleId = 1;
+                break;
+            case UNIPARC_STR:
+                ruleId = 2;
+                break;
+            case UNIREF_50_STR:
+                ruleId = 3;
+                break;
+            case UNIREF_90_STR:
+                ruleId = 4;
+                break;
+            case UNIREF_100_STR:
+                ruleId = 5;
+                break;
+            case GENE_NAME_STR:
+                ruleId = 6;
+                break;
+            case UNIPROTKB_SWISS_STR:
+            case UNIPROTKB_STR:
+                ruleId = null;
+                break;
+            default:
+                ruleId = 7;
+        }
+        return ruleId;
     }
 }
