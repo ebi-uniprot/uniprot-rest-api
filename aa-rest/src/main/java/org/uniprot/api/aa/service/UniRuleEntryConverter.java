0fcc5ea515de438fc2909a6fcbe590cf39b8b2e6
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-07-19 11:21:16 +0100

    rename module from unirule-rest to aa-rest

---
+++ aa-rest/src/main/java/org/uniprot/api/aa/service/UniRuleEntryConverter.java
@@ -0,0 +1,40 @@
+package org.uniprot.api.aa.service;
+
+import java.io.IOException;
+import java.util.function.Function;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Component;
+import org.uniprot.core.json.parser.unirule.UniRuleJsonConfig;
+import org.uniprot.core.unirule.UniRuleEntry;
+import org.uniprot.store.search.document.unirule.UniRuleDocument;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author sahmad
+ * @created 11/11/2020
+ */
+@Component
+@Slf4j
+public class UniRuleEntryConverter implements Function<UniRuleDocument, UniRuleEntry> {
+    private final ObjectMapper objectMapper;
+
+    public UniRuleEntryConverter() {
+        this.objectMapper = UniRuleJsonConfig.getInstance().getFullObjectMapper();
+    }
+
+    @Override
+    public UniRuleEntry apply(UniRuleDocument uniRuleDocument) {
+        UniRuleEntry entry = null;
+        try {
+            entry =
+                    this.objectMapper.readValue(
+                            uniRuleDocument.getUniRuleObj().array(), UniRuleEntry.class);
+        } catch (IOException e) {
+            log.info("Error converting solr binary to UniRuleEntry: ", e);
+        }
+        return entry;
+    }
+}
---
+++ aa-rest/src/test/java/org/uniprot/api/aa/controller/UniRuleStreamControllerIT.java
@@ -0,0 +1,190 @@
+package org.uniprot.api.aa.controller;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.uniprot.api.aa.UniRuleRestApplication;
+import org.uniprot.api.aa.repository.UniRuleQueryRepository;
+import org.uniprot.api.common.repository.search.SolrQueryRepository;
+import org.uniprot.api.rest.controller.AbstractSolrStreamControllerIT;
+import org.uniprot.api.rest.validation.error.ErrorHandlerConfig;
+import org.uniprot.core.unirule.UniRuleEntry;
+import org.uniprot.core.unirule.impl.UniRuleEntryBuilderTest;
+import org.uniprot.store.indexer.DataStoreManager;
+import org.uniprot.store.indexer.unirule.UniRuleDocumentConverter;
+import org.uniprot.store.search.SolrCollection;
+import org.uniprot.store.search.document.unirule.UniRuleDocument;
+
+/**
+ * @author sahmad
+ * @since 02/12/2020
+ */
+@ContextConfiguration(
+        classes = {
+            DataStoreTestConfig.class,
+            UniRuleRestApplication.class,
+            ErrorHandlerConfig.class
+        })
+@ActiveProfiles(profiles = "offline")
+@WebMvcTest(UniRuleController.class)
+@ExtendWith(value = {SpringExtension.class})
+class UniRuleStreamControllerIT extends AbstractSolrStreamControllerIT {
+
+    @Autowired private UniRuleQueryRepository repository;
+
+    @Override
+    protected DataStoreManager.StoreType getStoreType() {
+        return DataStoreManager.StoreType.UNIRULE;
+    }
+
+    @Override
+    protected SolrCollection getSolrCollection() {
+        return SolrCollection.unirule;
+    }
+
+    @Override
+    protected SolrQueryRepository getRepository() {
+        return repository;
+    }
+
+    @Override
+    protected String getStreamPath() {
+        return "/unirule/stream";
+    }
+
+    @Override
+    protected int saveEntries() {
+        int numberOfEntries = 12;
+        IntStream.rangeClosed(1, numberOfEntries).forEach(this::saveEntry);
+        return numberOfEntries;
+    }
+
+    private void saveEntry(int suffix) {
+        UniRuleEntry entry = UniRuleEntryBuilderTest.createObject(2);
+        UniRuleEntry uniRuleEntry = UniRuleControllerITUtils.updateValidValues(entry, suffix);
+        UniRuleDocumentConverter docConverter = new UniRuleDocumentConverter();
+        UniRuleDocument document = docConverter.convertToDocument(uniRuleEntry);
+        storeManager.saveDocs(DataStoreManager.StoreType.UNIRULE, document);
+    }
+
+    @Test
+    void uniRuleQueryWorks() throws Exception {
+        // when
+        MockHttpServletRequestBuilder requestBuilder =
+                get(getStreamPath())
+                        .header(ACCEPT, MediaType.APPLICATION_JSON)
+                        .param("query", "unirule_id:UR000000002");
+
+        MvcResult response = mockMvc.perform(requestBuilder).andReturn();
+        Assertions.assertNotNull(response);
+
+        // then
+        mockMvc.perform(asyncDispatch(response))
+                .andDo(log())
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.results.size()", is(1)))
+                .andExpect(jsonPath("$.results.*.uniRuleId", contains("UR000000002")));
+    }
+
+    @Test
+    void uniRuleQueryEmptyResults() throws Exception {
+        // when
+        MockHttpServletRequestBuilder requestBuilder =
+                get(getStreamPath())
+                        .header(ACCEPT, MediaType.APPLICATION_JSON)
+                        .param("query", "condition_value:something");
+
+        MvcResult response = mockMvc.perform(requestBuilder).andReturn();
+        Assertions.assertNotNull(response);
+
+        // then
+        mockMvc.perform(asyncDispatch(response))
+                .andDo(log())
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.results.size()", is(0)));
+    }
+
+    @Test
+    void uniRuleSortWorks() throws Exception {
+        // when
+        MockHttpServletRequestBuilder requestBuilder =
+                get(getStreamPath())
+                        .header(ACCEPT, MediaType.APPLICATION_JSON)
+                        .param("query", "*")
+                        .param("sort", "unirule_id desc");
+
+        MvcResult response = mockMvc.perform(requestBuilder).andReturn();
+        Assertions.assertNotNull(response);
+
+        // then
+        mockMvc.perform(asyncDispatch(response))
+                .andDo(log())
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.results.size()", is(12)))
+                .andExpect(
+                        jsonPath(
+                                "$.results.*.uniRuleId",
+                                contains(
+                                        "UR000000012",
+                                        "UR000000011",
+                                        "UR000000010",
+                                        "UR000000009",
+                                        "UR000000008",
+                                        "UR000000007",
+                                        "UR000000006",
+                                        "UR000000005",
+                                        "UR000000004",
+                                        "UR000000003",
+                                        "UR000000002",
+                                        "UR000000001")));
+    }
+
+    @Test
+    void uniRuleFieldsWorks() throws Exception {
+        // when
+        MockHttpServletRequestBuilder requestBuilder =
+                get(getStreamPath())
+                        .header(ACCEPT, MediaType.APPLICATION_JSON)
+                        .param("query", "*")
+                        .param("fields", "uniRuleId,taxonomic_scope");
+
+        MvcResult response = mockMvc.perform(requestBuilder).andReturn();
+        Assertions.assertNotNull(response);
+
+        // then
+        mockMvc.perform(asyncDispatch(response))
+                .andDo(log())
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.results.*.information").doesNotExist())
+                .andExpect(jsonPath("$.results.*.uniRuleId").exists())
+                .andExpect(jsonPath("$.results.*.mainRule").exists())
+                .andExpect(jsonPath("$.results.size()", is(12)));
+    }
+}
