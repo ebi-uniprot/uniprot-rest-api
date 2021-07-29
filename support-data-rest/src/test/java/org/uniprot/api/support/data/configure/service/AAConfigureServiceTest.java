e8ea429538e58ec58aa658886823826046357a21
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-07-29 14:34:26 +0100

    add search and result fields api

---
+++ support-data-rest/src/test/java/org/uniprot/api/support/data/configure/service/AAConfigureServiceTest.java
@@ -0,0 +1,61 @@
+package org.uniprot.api.support.data.configure.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.uniprot.api.support.data.configure.service.UtilServiceTest.CONTEXT_PATH;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.uniprot.api.support.data.configure.response.AdvancedSearchTerm;
+import org.uniprot.api.support.data.configure.response.UniProtReturnField;
+
+/**
+ * @author sahmad
+ * @created 29/07/2021
+ */
+class AAConfigureServiceTest {
+    private static AAConfigureService SERVICE;
+
+    @BeforeAll
+    static void init() {
+        SERVICE = new AAConfigureService();
+    }
+
+    @Test
+    void getUniRuleResultFields() {
+        List<UniProtReturnField> resultGroups = SERVICE.getUniRuleResultFields();
+
+        assertNotNull(resultGroups);
+        assertNotNull(resultGroups);
+        assertEquals(1, resultGroups.size());
+
+        assertEquals(6, resultGroups.get(0).getFields().size());
+    }
+
+    @Test
+    void getUniRuleSearchItems() {
+        List<AdvancedSearchTerm> result = SERVICE.getUniRuleSearchItems(CONTEXT_PATH);
+        assertNotNull(result);
+        assertEquals(6, result.size());
+    }
+
+    @Test
+    void getArbaResultFields() {
+        List<UniProtReturnField> resultGroups = SERVICE.getArbaResultFields();
+
+        assertNotNull(resultGroups);
+        assertNotNull(resultGroups);
+        assertEquals(1, resultGroups.size());
+
+        assertEquals(4, resultGroups.get(0).getFields().size());
+    }
+
+    @Test
+    void getArbaSearchItems() {
+        List<AdvancedSearchTerm> result = SERVICE.getArbaSearchItems(CONTEXT_PATH);
+        assertNotNull(result);
+        assertEquals(4, result.size());
+    }
+}
