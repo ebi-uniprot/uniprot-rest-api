b8b0c6a9aa90bbd0a117069b8144117b3248f180
Shadab Ahmad <sahmad@ebi.ac.uk>
2022-11-23 18:12:56 +0000

    add the missing file

---
+++ common-rest/src/main/java/org/uniprot/api/rest/queue/RabbitMQConfigProperties.java
@@ -0,0 +1,30 @@
+package org.uniprot.api.rest.queue;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+/**
+ * Rabbit MQ properties bean that will be injected with values from application.properties.
+ *
+ * @author sahmad
+ */
+@Data
+@ConfigurationProperties(prefix = "spring.amqp.rabbit")
+public class RabbitMQConfigProperties {
+    private String host;
+
+    private String user;
+
+    private String password;
+
+    private int port;
+
+    private String exchangeName;
+
+    private String queueName;
+
+    private String routingKey;
+
+    private boolean durable;
+}
