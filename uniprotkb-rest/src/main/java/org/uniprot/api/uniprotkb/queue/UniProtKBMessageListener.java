09989c00ea520fe3cdfdd1ed6bbbf1d25fa853a2
Shadab Ahmad <sahmad@ebi.ac.uk>
2023-01-12 12:58:50 +0000

    Add error and retry count before sending message on retry queue

--- uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/queue/UniProtKBMessageListener.java
+++ uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/queue/UniProtKBMessageListener.java
@@ -1,14 +1,12 @@
 package org.uniprot.api.uniprotkb.queue;
 
 import lombok.extern.slf4j.Slf4j;
-import net.jodah.failsafe.Failsafe;
-import net.jodah.failsafe.RetryPolicy;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.uniprot.api.common.repository.stream.store.StoreRequest;
@@ -27,11 +25,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.sql.Timestamp;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -41,6 +38,9 @@ import java.util.stream.Stream;
 @Service("Consumer")
 @Slf4j
 public class UniProtKBMessageListener implements MessageListener {
+
+    private static final String CURRENT_RETRIED_COUNT_HEADER = "x-uniprot-retry-count";
+    private static final String CURRENT_RETRIED_ERROR_HEADER = "x-uniprot-error";
 
     private final MessageConverter converter;
     private final UniProtEntryService service;
@@ -54,7 +54,10 @@ public class UniProtKBMessageListener implements MessageListener {
     private String rejectedQueueName;
 
     @Value("${spring.amqp.rabbit.retryMaxCount}")
-    private Integer retryCount;
+    private Integer maxRetryCount;
+
+    @Value("${spring.amqp.rabbit.retryQueueName}")
+    private String retryQueueName;
 
     public UniProtKBMessageListener(
             MessageConverter converter,
@@ -73,55 +76,52 @@ public class UniProtKBMessageListener implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
-        UniProtKBStreamRequest request = (UniProtKBStreamRequest) converter.fromMessage(message);
-        String jobId = message.getMessageProperties().getHeader("jobId");
-        Optional<DownloadJob> optDownloadJob = this.jobRepository.findById(jobId);
+        String jobId = null;
+        DownloadJob downloadJob = null;
+        try {
+            UniProtKBStreamRequest request = (UniProtKBStreamRequest) converter.fromMessage(message);
+            jobId = message.getMessageProperties().getHeader("jobId");
+            Optional<DownloadJob> optDownloadJob = this.jobRepository.findById(jobId);
+            String errorMsg = "Unable to find jobId " + jobId  + " in db";
+            downloadJob = optDownloadJob.orElseThrow(() -> new MessageListenerException(errorMsg));
 
-        if(optDownloadJob.isPresent()) {
-            DownloadJob downloadJob = optDownloadJob.get();
-            if(isMaxRetriedReached(message)){
+            if (isMaxRetriedReached(message)) {
                 updateDownloadJob(message, downloadJob, JobStatus.ERROR);
                 sendToUndeliveredQueue(jobId, message);
+                log.error("Message with job id {} discarded after max retry {}", jobId, this.maxRetryCount);
                 return;
             }
 
-            if(jobId != null){
-                throw new MessageListenerException("test");
-            }
+            processMessage(message, request, downloadJob);
 
-            String contentType = message.getMessageProperties().getHeader("content-type");
-
-            if (contentType == null) { //TODO: REMOVE IT
-                contentType = "application/json";
-            }
-
-            Path idsFile = Paths.get(downloadConfigProperties.getFolder(), jobId);
-            if (Files.notExists(idsFile)) {
-                updateDownloadJob(message, downloadJob, JobStatus.RUNNING);
-                getAndWriteResult(request, idsFile, jobId, contentType);
-                updateDownloadJob(message, downloadJob, JobStatus.FINISHED);
-            } else {
-                log.info("The job {} is already processed", jobId);
-                updateDownloadJob(message, downloadJob, JobStatus.FINISHED);
-            }
-
-
-            // TESTING TO ALSO CREATE RESULT
-            log.info("Message processed");
-        } else {
-            String errMsg = "Unable to find job id" + jobId + "in db";
-            log.warn(errMsg);
-            throw new MessageListenerException(errMsg);
+            log.info("Message with jobId {} processed successfully", jobId);
+        } catch (Exception ex){
+            log.error("Download job id {} failed with error {}", jobId, ex.getStackTrace());
+            Message updatedMessage = addAdditionalHeaders(message, ex);
+            // TODO test what if this fails. need to x-death in this case
+            updateDownloadJob(updatedMessage, downloadJob, JobStatus.ERROR);
+            // TODO test what if this fails.
+            sendToRetryQueue(jobId, updatedMessage);
         }
     }
 
-    private boolean isMaxRetriedReached(Message message) {
-        return getRetryCount(message) >= this.retryCount;
-    }
+    private void processMessage(Message message, UniProtKBStreamRequest request, DownloadJob downloadJob) {
+        String jobId = downloadJob.getId();
+        String contentType = message.getMessageProperties().getHeader("content-type");
 
-    private void sendToUndeliveredQueue(String jobId, Message message) {
-        log.warn("Maximum retry {} reached for jobId {}. Sending to rejected queue", retryCount, jobId);
-        this.rabbitTemplate.convertAndSend(rejectedQueueName, message);
+        if (contentType == null) { //TODO: REMOVE IT
+            contentType = "application/json";
+        }
+
+        Path idsFile = Paths.get(downloadConfigProperties.getFolder(), jobId);
+        if (Files.notExists(idsFile)) {
+            updateDownloadJob(message, downloadJob, JobStatus.RUNNING);
+            getAndWriteResult(request, idsFile, jobId, contentType);
+            updateDownloadJob(message, downloadJob, JobStatus.FINISHED);
+        } else {
+            log.info("The job {} is already processed", jobId);
+            updateDownloadJob(message, downloadJob, JobStatus.FINISHED);
+        }
     }
 
     private void getAndWriteResult(UniProtKBStreamRequest request, Path idsFile, String jobId, String contentType) {
@@ -155,26 +155,59 @@ public class UniProtKBMessageListener implements MessageListener {
         return service.streamIds(request);
     }
 
-    private DownloadJob updateDownloadJob(Message message, DownloadJob downloadJob, JobStatus jobStatus) {
-        Timestamp now = new Timestamp(System.currentTimeMillis());
-        downloadJob.setUpdated(now);
-        downloadJob.setStatus(jobStatus);
-        downloadJob.setRetried(getRetryCount(message));
-        // TODO get error from x-death and set in job entry
-//        downloadJob.setError(getLastError(message));
-        return this.jobRepository.save(downloadJob);
+    private void updateDownloadJob(Message message, DownloadJob downloadJob, JobStatus jobStatus) {
+        if(Objects.nonNull(downloadJob)) {
+            Timestamp now = new Timestamp(System.currentTimeMillis());
+            downloadJob.setUpdated(now);
+            downloadJob.setStatus(jobStatus);
+            downloadJob.setRetried(getRetryCount(message));
+            String error = getLastError(message);
+            downloadJob.setError(error);
+            this.jobRepository.save(downloadJob);
+        }
+    }
+
+    private boolean isMaxRetriedReached(Message message) {
+        return getRetryCount(message) >= this.maxRetryCount;
     }
 
     private int getRetryCount(Message message) {
-        List<Map<String, ?>> xDeaths = message.getMessageProperties().getHeader("x-death");
-        int count = 0;
-        if(Objects.nonNull(xDeaths)) {
-            Map<String, ?> xDeath = xDeaths.get(0);
-            if (Objects.nonNull(xDeath) && !xDeath.isEmpty()) {
-                Long longCount = (Long) xDeath.get("count");
-                count = longCount.intValue();
-            }
+        Integer retryCount = message.getMessageProperties().getHeader(CURRENT_RETRIED_COUNT_HEADER);
+        return Objects.nonNull(retryCount) ? retryCount : 0;
+    }
+
+    private void sendToUndeliveredQueue(String jobId, Message message) {
+        log.warn("Maximum retry {} reached for jobId {}. Sending to rejected queue", this.maxRetryCount, jobId);
+        this.rabbitTemplate.convertAndSend(rejectedQueueName, message);
+    }
+
+    private void sendToRetryQueue(String jobId, Message message) {
+        log.warn("Sending message for jobId {} to retry queue", jobId);
+        this.rabbitTemplate.convertAndSend(this.retryQueueName, message);
+        log.info("Message with jobId {} sent to retry queue {}", jobId, this.retryQueueName);
+    }
+
+    private String getLastError(Message message) {
+        if(Objects.nonNull(message.getMessageProperties().getHeader(CURRENT_RETRIED_ERROR_HEADER))){
+            return message.getMessageProperties().getHeader(CURRENT_RETRIED_ERROR_HEADER).toString();
         }
-        return count;
+        return null;
+    }
+
+    private Message addAdditionalHeaders(Message message, Exception ex) {
+        MessageBuilder builder = MessageBuilder.fromMessage(message);
+        Integer retryCount = message.getMessageProperties().getHeader(CURRENT_RETRIED_COUNT_HEADER);
+        if(Objects.nonNull(retryCount)){
+            retryCount++;
+        } else {
+            retryCount = 1;
+        }
+
+        String stackTrace = Arrays.stream(ex.getStackTrace())
+                .map(StackTraceElement::toString)
+                .collect(Collectors.joining("\n"));
+        builder.setHeader(CURRENT_RETRIED_COUNT_HEADER, retryCount);
+        builder.setHeader(CURRENT_RETRIED_ERROR_HEADER, stackTrace);
+        return  builder.build();
     }
 }
