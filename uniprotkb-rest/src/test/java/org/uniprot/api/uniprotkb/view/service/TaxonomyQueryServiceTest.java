6f5b89729d27ec10992ecce030b1f9b7acf601b4
supun <supun@ebi.ac.uk>
2023-05-17 10:20:00 +0100

    removed protein api dependency

---
+++ uniprotkb-rest/src/test/java/org/uniprot/api/uniprotkb/view/service/TaxonomyQueryServiceTest.java
@@ -0,0 +1,13 @@
+package org.uniprot.api.uniprotkb.view.service;
+
+class TaxonomyQueryServiceTest {
+
+    /*@Test
+    void test() {
+        String taxId = "9605";
+        TaxonomyQueryService client = new TaxonomyQueryService(new RestTemplate());
+        List<TaxonomyNode> nodes = client.getChildren(taxId);
+        assertNotNull(nodes);
+        assertFalse(nodes.isEmpty());
+    }*/
+}
