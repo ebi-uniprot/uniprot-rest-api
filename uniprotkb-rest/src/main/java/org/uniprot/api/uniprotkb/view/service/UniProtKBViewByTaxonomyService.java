e740fa45a5c191bf7ec2fbdcc00b4ab82e92da33
supun <supun@ebi.ac.uk>
2023-05-25 10:59:31 +0100

    wip

--- uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/view/service/UniProtKBViewByTaxonomyService.java
+++ uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/view/service/UniProtKBViewByTaxonomyService.java
@@ -1,6 +1,5 @@
 package org.uniprot.api.uniprotkb.view.service;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.springframework.stereotype.Service;
 import org.uniprot.api.rest.request.taxonomy.TaxonomyStreamRequest;
@@ -17,100 +16,48 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
-public class UniProtKBViewByTaxonomyService implements UniProtViewByService {
+public class UniProtKBViewByTaxonomyService extends UniProtViewByService<TaxonomyEntry> {
     public static final String TOP_LEVEL_PARENT_QUERY = "-parent:[* TO *] AND active:true";
     public static final String URL_PHRASE = "/taxonomy/";
     private final TaxonomyService taxonomyService;
-    private final UniProtEntryService uniProtEntryService;
 
     public UniProtKBViewByTaxonomyService(TaxonomyService taxonomyService, UniProtEntryService uniProtEntryService) {
-        this.uniProtEntryService = uniProtEntryService;
+        super(uniProtEntryService);
         this.taxonomyService = taxonomyService;
     }
 
     @Override
-    public List<ViewBy> getViewBys(String queryStr, String parent) {
-        List<FacetField.Count> facetCounts = List.of();
-        List<TaxonomyEntry> taxonomyEntries = List.of();
-        String taxonomyId = parent;
-
-        do {
-            List<TaxonomyEntry> childTaxonomyEntries = getChildren(taxonomyId);
-            List<FacetField.Count> childFacetCounts = getFacetCounts(queryStr, childTaxonomyEntries);
-
-            if (!childFacetCounts.isEmpty()) {
-                facetCounts = childFacetCounts;
-                taxonomyEntries = childTaxonomyEntries;
-                taxonomyId = facetCounts.get(0).getName();
-            } else {
-                break;
-            }
-
-        } while (facetCounts.size() == 1 && isTopLevelSearch(parent));
-
-        return getViewBys(facetCounts, taxonomyEntries, queryStr);
+    protected List<TaxonomyEntry> getChildren(String parent) {
+        TaxonomyStreamRequest taxonomyStreamRequest = new TaxonomyStreamRequest();
+        taxonomyStreamRequest.setQuery(isTopLevelSearch(parent) ? TOP_LEVEL_PARENT_QUERY : "parent:" + parent);
+        return taxonomyService.stream(taxonomyStreamRequest).collect(Collectors.toList());
     }
 
-    private static boolean isTopLevelSearch(String parent) {
-        return StringUtils.isEmpty(parent);
+    @Override
+    protected String getFacetFields(List<TaxonomyEntry> entries) {
+        String facetItems = entries.stream()
+                .map(taxonomy -> String.valueOf(taxonomy.getTaxonId()))
+                .collect(Collectors.joining(","));
+        return String.format("{!terms='%s'}taxonomy_id", facetItems);
     }
 
-    private List<ViewBy> getViewBys(
-            List<FacetField.Count> facetCounts,
-            List<TaxonomyEntry> taxonomyEntries,
-            String queryStr) {
-        Map<Long, TaxonomyEntry> taxIdMap =
-                taxonomyEntries.stream()
-                        .collect(Collectors.toMap(Taxonomy::getTaxonId, Function.identity()));
+    @Override
+    protected List<ViewBy> getViewBys(List<FacetField.Count> facetCounts, List<TaxonomyEntry> entries, String query) {
+        Map<Long, TaxonomyEntry> taxIdMap = entries.stream()
+                .collect(Collectors.toMap(Taxonomy::getTaxonId, Function.identity()));
 
         return facetCounts.stream()
-                .map(
-                        fc ->
-                                getViewBy(
-                                        fc,
-                                        taxIdMap.get(Long.parseLong(fc.getName())),
-                                        queryStr))
+                .map(fc -> getViewBy(fc, taxIdMap.get(Long.parseLong(fc.getName())), query))
                 .sorted(ViewBy.SORT_BY_LABEL_IGNORE_CASE)
                 .collect(Collectors.toList());
     }
 
-    private ViewBy getViewBy(
-            FacetField.Count count, TaxonomyEntry taxonomyEntry, String queryStr) {
+    private ViewBy getViewBy(FacetField.Count count, TaxonomyEntry taxonomyEntry, String queryStr) {
         return ViewByImpl.builder().id(count.getName())
                 .count(count.getCount())
                 .link(URL_PHRASE + count.getName())
                 .label(taxonomyEntry.getScientificName())
                 .expand(hasChildren(count, queryStr))
                 .build();
-    }
-
-    private boolean hasChildren(FacetField.Count count, String queryStr) {
-        List<TaxonomyEntry> children = getChildren(count.getName());
-        return !getFacetCounts(queryStr, children).isEmpty();
-    }
-
-    private List<FacetField.Count> getFacetCounts(String query, List<TaxonomyEntry> taxonomyEntries) {
-        if (!taxonomyEntries.isEmpty()) {
-            String facetItems = taxonomyEntries.stream()
-                    .map(taxonomy -> String.valueOf(taxonomy.getTaxonId()))
-                    .collect(Collectors.joining(","));
-            List<FacetField> facetFields = uniProtEntryService
-                    .getFacets(query, String.format("{!terms='%s'}taxonomy_id", facetItems));
-
-            if (!facetFields.isEmpty()) {
-                return facetFields.get(0).getValues().stream()
-                        .filter(count -> count.getCount() > 0)
-                        .collect(Collectors.toList());
-            }
-        }
-
-        return List.of();
-    }
-
-
-    private List<TaxonomyEntry> getChildren(String taxId) {
-        TaxonomyStreamRequest taxonomyStreamRequest = new TaxonomyStreamRequest();
-        taxonomyStreamRequest.setQuery(isTopLevelSearch(taxId) ? TOP_LEVEL_PARENT_QUERY : "parent:" + taxId);
-        return taxonomyService.stream(taxonomyStreamRequest).collect(Collectors.toList());
     }
 }
--- uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/view/service/UniProtViewByService.java
+++ uniprotkb-rest/src/main/java/org/uniprot/api/uniprotkb/view/service/UniProtViewByService.java
@@ -1,9 +1,69 @@
 package org.uniprot.api.uniprotkb.view.service;
 
-import java.util.List;
-
+import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.client.solrj.response.FacetField;
+import org.uniprot.api.uniprotkb.service.UniProtEntryService;
 import org.uniprot.api.uniprotkb.view.ViewBy;
 
-public interface UniProtViewByService {
-    List<ViewBy> getViewBys(String queryStr, String parent);
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class UniProtViewByService<T> {
+    private final UniProtEntryService uniProtEntryService;
+
+    UniProtViewByService(UniProtEntryService uniProtEntryService) {
+        this.uniProtEntryService = uniProtEntryService;
+    }
+
+    public List<ViewBy> getViewBys(String query, String parent) {
+        List<FacetField.Count> facetCounts = List.of();
+        List<T> entries = List.of();
+        String id = parent;
+
+        do {
+            List<T> childEntries = getChildren(id);
+            List<FacetField.Count> childFacetCounts = getFacetCounts(query, childEntries);
+
+            if (!childFacetCounts.isEmpty()) {
+                facetCounts = childFacetCounts;
+                entries = childEntries;
+                id = facetCounts.get(0).getName();
+            } else {
+                break;
+            }
+
+        } while (facetCounts.size() == 1 && isTopLevelSearch(parent));
+
+        return getViewBys(facetCounts, entries, query);
+    }
+
+    protected boolean hasChildren(FacetField.Count count, String queryStr) {
+        List<T> children = getChildren(count.getName());
+        return !getFacetCounts(queryStr, children).isEmpty();
+    }
+
+    private List<FacetField.Count> getFacetCounts(String query, List<T> entries) {
+        if (!entries.isEmpty()) {
+            List<FacetField> facetFields = uniProtEntryService
+                    .getFacets(query, getFacetFields(entries));
+
+            if (!facetFields.isEmpty()) {
+                return facetFields.get(0).getValues().stream()
+                        .filter(count -> count.getCount() > 0)
+                        .collect(Collectors.toList());
+            }
+        }
+
+        return List.of();
+    }
+
+    protected static boolean isTopLevelSearch(String parent) {
+        return StringUtils.isEmpty(parent);
+    }
+
+    protected abstract List<T> getChildren(String parent);
+
+    protected abstract String getFacetFields(List<T> entries);
+
+    protected abstract List<ViewBy> getViewBys(List<FacetField.Count> facetCounts, List<T> entries, String query);
 }
