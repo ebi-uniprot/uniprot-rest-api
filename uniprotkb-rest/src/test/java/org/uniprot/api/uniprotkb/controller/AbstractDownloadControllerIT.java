1d0b7ad25de64b7d11eb3ae96fa7f80fac50589f
Talal Ibrahim <ibrahim@ebi.ac.uk>
2023-09-04 12:42:58 +0100

    resolve conflict for AbstractDownloadControllerIT

--- uniprotkb-rest/src/test/java/org/uniprot/api/uniprotkb/controller/AbstractDownloadControllerIT.java
+++ uniprotkb-rest/src/test/java/org/uniprot/api/uniprotkb/controller/AbstractDownloadControllerIT.java
@@ -12,6 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.uniprot.api.rest.download.TestUtils.uncompressFile;
+import static org.uniprot.api.rest.controller.ControllerITUtils.NO_CACHE_VALUE;
 import static org.uniprot.api.rest.output.UniProtMediaType.*;
 
 import java.io.IOException;
