c5320bb3ed5ed79f9a8e1fbb4a3b3612d450cecd
supun <supun@ebi.ac.uk>
2024-03-19 09:53:00 +0000

    review

--- common-rest/src/main/java/org/uniprot/api/rest/service/query/processor/UniProtFieldQueryNodeProcessor.java
+++ common-rest/src/main/java/org/uniprot/api/rest/service/query/processor/UniProtFieldQueryNodeProcessor.java
@@ -1,13 +1,5 @@
 package org.uniprot.api.rest.service.query.processor;
 
-import static org.uniprot.api.rest.service.query.UniProtQueryProcessor.IMPOSSIBLE_FIELD;
-import static org.uniprot.api.rest.service.query.UniProtQueryProcessor.UNIPROTKB_ACCESSION_FIELD;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-
 import org.apache.lucene.queryparser.flexible.core.nodes.FieldQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.FuzzyQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
@@ -17,6 +9,14 @@ import org.apache.lucene.queryparser.flexible.core.processors.QueryNodeProcessor
 import org.uniprot.store.config.searchfield.common.SearchFieldConfig;
 import org.uniprot.store.config.searchfield.model.SearchFieldItem;
 import org.uniprot.store.search.SolrQueryUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.uniprot.api.rest.service.query.UniProtQueryProcessor.IMPOSSIBLE_FIELD;
+import static org.uniprot.api.rest.service.query.UniProtQueryProcessor.UNIPROTKB_ACCESSION_FIELD;
 
 /**
  * Created 23/08/2020
@@ -84,21 +84,20 @@ class UniProtFieldQueryNodeProcessor extends QueryNodeProcessorImpl {
 
             if (field.equals(IMPOSSIBLE_FIELD)) {
                 return defaultSearchToQueryString(text);
-            } else if (validWhiteListFields(field, text)) {
+            }
+            if (validWhiteListFields(field, text)) {
                 return field.toUpperCase() + "\\:" + text;
-            } else if (UNIPROTKB_ACCESSION_FIELD.equals(field) && validFieldIgnoreCase(field)) {
-                return field + SOLR_FIELD_SEPARATOR + text.toUpperCase();
-            } else if (validFieldIgnoreCase(field)) {
-                return field.toLowerCase() + SOLR_FIELD_SEPARATOR + text;
-            } else if (UNIPROTKB_ACCESSION_FIELD.equals(field)) {
-                text = text.toUpperCase();
             }
 
-            // is an alias => return the field itself
-            Optional<SearchFieldItem> searchFieldItemByAlias =
-                    searchFieldConfig.findSearchFieldItemByAlias(field);
-            if (searchFieldItemByAlias.isPresent()) {
-                return searchFieldItemByAlias.get().getFieldName() + SOLR_FIELD_SEPARATOR + text;
+            if (UNIPROTKB_ACCESSION_FIELD.equals(field)) {
+                text = text.toUpperCase();
+            }
+            if (validFieldIgnoreCase(field)) {
+                return field.toLowerCase() + SOLR_FIELD_SEPARATOR + text;
+            }
+            Optional<SearchFieldItem> searchFieldItem = searchFieldConfig.findSearchFieldItemByAlias(field);
+            if (searchFieldItem.isPresent()) {
+                return searchFieldItem.get().getFieldName() + SOLR_FIELD_SEPARATOR + text;
             }
 
             return super.toQueryString(escaper);
