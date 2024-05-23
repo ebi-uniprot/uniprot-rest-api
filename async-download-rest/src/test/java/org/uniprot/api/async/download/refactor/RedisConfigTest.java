a02125cf58ac8f954f891901a0620b7291ff5f5a
LeonardoGonzales <lgonzales@ebi.ac.uk>
2024-05-23 15:20:49 +0100

    wip for ProducerMessageServiceIT change wiring uniref

---
+++ async-download-rest/src/test/java/org/uniprot/api/async/download/refactor/RedisConfigTest.java
@@ -0,0 +1,19 @@
+package org.uniprot.api.async.download.refactor;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class RedisConfigTest {
+
+    @Bean(destroyMethod = "shutdown")
+    RedissonClient redisson() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://"+System.getProperty("uniprot.redis.host")+":"+System.getProperty("uniprot.redis.port"));
+        return Redisson.create(config);
+    }
+}
