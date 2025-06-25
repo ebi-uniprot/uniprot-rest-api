8607c399a300ee1c8b62a0d150fed93b2cfb14e1
supun <supun@ebi.ac.uk>
2025-06-25 14:49:20 +0100

    add suggestions from spell check when collations are not present

--- common-rest/src/main/java/org/uniprot/api/common/repository/search/suggestion/SuggestionConverter.java
+++ common-rest/src/main/java/org/uniprot/api/common/repository/search/suggestion/SuggestionConverter.java
@@ -2,11 +2,14 @@ package org.uniprot.api.common.repository.search.suggestion;
 
 import static java.util.Collections.emptyList;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.SpellCheckResponse;
+import org.apache.solr.common.util.NamedList;
 import org.springframework.core.convert.converter.Converter;
 import org.uniprot.core.util.Utils;
 
@@ -29,6 +32,40 @@ public class SuggestionConverter implements Converter<QueryResponse, List<Sugges
             return spellCheckResponse.getCollatedResults().stream()
                     .map(this::getSuggestion)
                     .collect(Collectors.toList());
+        } else if (resultHits == 0L
+                && Utils.notNull(spellCheckResponse)
+                && Utils.notNullNotEmpty(spellCheckResponse.getSuggestions())) {
+            Map<String, SpellCheckResponse.Suggestion> suggestionMap =
+                    spellCheckResponse.getSuggestionMap();
+            String query =
+                    String.valueOf(
+                            ((NamedList) (queryResponse.getResponseHeader().get("params")))
+                                    .get("q"));
+
+            List<Suggestion> suggestions = new LinkedList<>();
+            suggestionMap.keySet().stream()
+                    .filter(query::contains)
+                    .forEach(
+                            key -> {
+                                SpellCheckResponse.Suggestion suggestion = suggestionMap.get(key);
+                                for (int i = 0; i < suggestion.getNumFound(); i++) {
+                                    suggestions.add(
+                                            Suggestion.builder()
+                                                    .query(
+                                                            query.replace(
+                                                                            key,
+                                                                            suggestion
+                                                                                    .getAlternatives()
+                                                                                    .get(i))
+                                                                    .replace(" AND ", " "))
+                                                    .hits(
+                                                            suggestion
+                                                                    .getAlternativeFrequencies()
+                                                                    .get(i))
+                                                    .build());
+                                }
+                            });
+            return suggestions;
         } else {
             return emptyList();
         }
