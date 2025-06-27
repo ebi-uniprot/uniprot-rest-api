7e09f2856ba2049a60a12a713f837149083907db
Shadab Ahmad <sahmad@ebi.ac.uk>
2025-06-27 14:54:15 +0100

    make includeIsoform field optional

--- mapto-rest/src/main/java/org/uniprot/api/mapto/request/MapToSearchRequest.java
+++ mapto-rest/src/main/java/org/uniprot/api/mapto/request/MapToSearchRequest.java
@@ -2,15 +2,12 @@ package org.uniprot.api.mapto.request;
 
 import org.uniprot.store.config.UniProtDataType;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+public interface MapToSearchRequest {
+    UniProtDataType getFrom();
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class MapToSearchRequest {
-    private UniProtDataType from;
-    private UniProtDataType to;
-    private String query;
+    UniProtDataType getTo();
+
+    String getQuery();
+
+    boolean isIncludeIsoform();
 }
