6bee7bddf744e2e9bd54f775e47fe9a29ebbad64
Shadab Ahmad <sahmad@ebi.ac.uk>
2025-06-30 13:52:09 +0100

    add uniprotkb validation on query field of run api

--- mapto-rest/src/main/java/org/uniprot/api/mapto/request/UniProtKBMapToSearchRequest.java
+++ mapto-rest/src/main/java/org/uniprot/api/mapto/request/UniProtKBMapToSearchRequest.java
@@ -1,5 +1,7 @@
 package org.uniprot.api.mapto.request;
 
+import org.uniprot.api.rest.validation.ValidSolrQueryFields;
+import org.uniprot.api.rest.validation.ValidSolrQuerySyntax;
 import org.uniprot.store.config.UniProtDataType;
 
 import lombok.Data;
@@ -8,6 +10,10 @@ import lombok.Data;
 public class UniProtKBMapToSearchRequest implements MapToSearchRequest {
     private boolean includeIsoform;
     private UniProtDataType to;
+    @ValidSolrQuerySyntax
+    @ValidSolrQueryFields(
+            uniProtDataType = UniProtDataType.UNIPROTKB,
+            messagePrefix = "search.uniprot")
     private String query;
 
     @Override
