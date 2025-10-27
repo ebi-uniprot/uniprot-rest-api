cf0ae8ee76e414fb1fb5b34eecc64d77f9840492
Shadab Ahmad <sahmad@ebi.ac.uk>
2025-10-27 15:50:59 +0000

    add CIViC cross ref

--- support-data-rest/src/test/java/org/uniprot/api/support/data/configure/service/UniProtKBConfigureServiceTest.java
+++ support-data-rest/src/test/java/org/uniprot/api/support/data/configure/service/UniProtKBConfigureServiceTest.java
@@ -92,7 +92,7 @@ class UniProtKBConfigureServiceTest {
                         .collect(Collectors.toList());
         assertNotNull(multiValuesXrefs);
 
-        assertEquals(86, multiValuesXrefs.size());
+        assertEquals(87, multiValuesXrefs.size());
         for (UniProtReturnField xref : multiValuesXrefs) {
             assertTrue(xref.getName().startsWith("xref_"));
         }
