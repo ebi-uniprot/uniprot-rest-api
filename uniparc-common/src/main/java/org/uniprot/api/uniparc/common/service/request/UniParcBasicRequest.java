a489e7fa5ff5ce460aa9d054e9ff5d6c3736972f
supun <supun@ebi.ac.uk>
2025-11-25 13:10:51 +0000

    add tests

--- uniparc-common/src/main/java/org/uniprot/api/uniparc/common/service/request/UniParcBasicRequest.java
+++ uniparc-common/src/main/java/org/uniprot/api/uniparc/common/service/request/UniParcBasicRequest.java
@@ -2,6 +2,10 @@ package org.uniprot.api.uniparc.common.service.request;
 
 import static org.uniprot.api.rest.openapi.OpenAPIConstants.*;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.validation.constraints.NotNull;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,15 +16,14 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.uniprot.api.rest.request.BasicRequest;
 import org.uniprot.api.rest.request.UniProtKBRequestUtil;
-import org.uniprot.api.rest.validation.*;
+import org.uniprot.api.rest.validation.ValidReturnFields;
+import org.uniprot.api.rest.validation.ValidSolrQueryFields;
+import org.uniprot.api.rest.validation.ValidSolrQuerySyntax;
+import org.uniprot.api.rest.validation.ValidSolrSortFields;
 import org.uniprot.store.config.UniProtDataType;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.Data;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author lgonzales
@@ -30,6 +33,7 @@ import java.util.stream.Collectors;
 public class UniParcBasicRequest implements BasicRequest {
 
     public static final String PROTEOME_COMPONENT = "proteomecomponent";
+
     @Parameter(description = QUERY_UNIPARC_DESCRIPTION, example = QUERY_UNIPARC_EXAMPLE)
     @NotNull(message = "{search.required}")
     @ValidSolrQuerySyntax(message = "{search.invalid.query}")
@@ -55,20 +59,29 @@ public class UniParcBasicRequest implements BasicRequest {
 
     public void setQuery(String query) {
         this.query = query;
-        Query parsedQuery = ValidSolrQueryFields.QueryFieldValidator.getParsedQuery(query);
-        if (parsedQuery instanceof BooleanQuery booleanQuery) {
-            List<BooleanClause> clauses = booleanQuery.clauses();
-            Map<String, String> queryFields = clauses.stream().map(clause -> clause.getQuery().toString().split(":"))
-                    .collect(Collectors.toMap(split -> split[0], split -> split[1]));
-            if (queryFields.containsKey(PROTEOME_COMPONENT)) {
+        if (query.contains(PROTEOME_COMPONENT)) {
+            Query parsedQuery = ValidSolrQueryFields.QueryFieldValidator.getParsedQuery(query);
+            if (parsedQuery instanceof BooleanQuery booleanQuery) {
+                List<BooleanClause> clauses = booleanQuery.clauses();
+                Map<String, String> queryFields =
+                        clauses.stream()
+                                .map(clause -> clause.getQuery().toString().split(":"))
+                                .collect(Collectors.toMap(split -> split[0], split -> split[1]));
                 BooleanQuery.Builder queryBuilder = new BooleanQuery.Builder();
                 for (BooleanClause clause : clauses) {
                     String fieldName = clause.getQuery().toString().split(":")[0];
                     if (PROTEOME_COMPONENT.equals(fieldName)) {
-                        String proteomeComponent = StringUtils.unwrap(queryFields.get(PROTEOME_COMPONENT), '"');
+                        String proteomeComponent =
+                                StringUtils.unwrap(queryFields.get(PROTEOME_COMPONENT), '"');
                         String proteomeId = queryFields.get("proteome");
-                        String proteomeComponentWithAddedProteome = StringUtils.wrap(proteomeId + " " + proteomeComponent, '"');
-                        queryBuilder.add(new TermQuery(new Term(PROTEOME_COMPONENT, proteomeComponentWithAddedProteome)), clause.getOccur());
+                        String proteomeComponentWithAddedProteome =
+                                StringUtils.wrap(proteomeId + " " + proteomeComponent, '"');
+                        queryBuilder.add(
+                                new TermQuery(
+                                        new Term(
+                                                PROTEOME_COMPONENT,
+                                                proteomeComponentWithAddedProteome)),
+                                clause.getOccur());
                     } else {
                         queryBuilder.add(clause);
                     }
--- uniparc-common/src/test/java/org/uniprot/api/uniparc/common/service/request/UniParcBasicRequestTest.java
+++ uniparc-common/src/test/java/org/uniprot/api/uniparc/common/service/request/UniParcBasicRequestTest.java
@@ -1,8 +1,8 @@
 package org.uniprot.api.uniparc.common.service.request;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class UniParcBasicRequestTest {
 
