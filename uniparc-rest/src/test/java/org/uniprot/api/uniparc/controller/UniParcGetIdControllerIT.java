81c89938eb3ec2cf4a98c5b8fd3473a01e9f46fc
Shadab Ahmad <sahmad@ebi.ac.uk>
2025-12-12 15:49:27 +0000

    add test to verify proteomeidcompoenent

--- uniparc-rest/src/test/java/org/uniprot/api/uniparc/controller/UniParcGetIdControllerIT.java
+++ uniparc-rest/src/test/java/org/uniprot/api/uniparc/controller/UniParcGetIdControllerIT.java
@@ -124,6 +124,20 @@ public class UniParcGetIdControllerIT extends AbstractGetSingleUniParcByIdTest {
                     .resultMatcher(jsonPath("$.uniParcId", is(UNIPARC_ID)))
                     .resultMatcher(jsonPath("$.oldestCrossRefCreated").exists())
                     .resultMatcher(jsonPath("$.mostRecentCrossRefUpdated").exists())
+                    .resultMatcher(
+                            jsonPath("$.uniParcCrossReferences", is(not(empty()))))
+                    .resultMatcher(
+                            jsonPath("$.uniParcCrossReferences", hasSize(25)))
+                    .resultMatcher(
+                            jsonPath("$.uniParcCrossReferences[*].proteomeIdComponents", everyItem(not(empty()))))
+                    .resultMatcher(
+                            jsonPath("$.uniParcCrossReferences[*].proteomeIdComponents[*].proteomeId", not(empty())))
+                    .resultMatcher(
+                            jsonPath("$.uniParcCrossReferences[*].proteomeIdComponents[*].component", not(empty())))
+                    .resultMatcher(
+                            jsonPath("$.uniParcCrossReferences[*].proteomeIdComponents[*].proteomeId", notNullValue()))
+                    .resultMatcher(
+                            jsonPath("$.uniParcCrossReferences[*].proteomeIdComponents[*].component", notNullValue()))
                     .build();
         }
 
