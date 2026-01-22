90068ebaf58ebd770c5b01f323abda8845aecaa2
Shadab Ahmad <sahmad@ebi.ac.uk>
2026-01-22 14:59:28 +0000

    return source as a search field under cross ref. it should be first item

--- support-data-rest/src/main/java/org/uniprot/api/support/data/configure/service/UniProtKBConfigureService.java
+++ support-data-rest/src/main/java/org/uniprot/api/support/data/configure/service/UniProtKBConfigureService.java
@@ -56,7 +56,7 @@ public class UniProtKBConfigureService {
                                 () ->
                                         new ServiceException(
                                                 "Unable to find cross_references for advanced search"));
-        xRef.getItems().addAll(0, getCrossReferencesSearchItem());
+        xRef.getItems().addAll(1, getCrossReferencesSearchItem());
         return result;
     }
 
