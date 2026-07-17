b3e6d6d0a493294138c0018849729442f4b2bd3b
Rajind <ruparathna@ebi.ac.uk>
2026-07-17 15:20:01 +0100

    feat: TRM-34196: Initial commit for solr testcontainers

---
+++ common-rest/src/main/java/org/uniprot/api/common/test/SolrTestContainerSetup.java
@@ -0,0 +1,96 @@
+package org.uniprot.api.common.test;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+import javax.annotation.PreDestroy;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.testcontainers.solr.SolrContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+public class SolrTestContainerSetup implements EnvironmentPostProcessor {
+    private static final Logger LOGGER = getLogger(SolrTestContainerSetup.class);
+
+    private SolrContainer solr;
+
+    private static final String SOLR_USER = "solr_admin";
+    private static final String SOLR_PASS = "nimda";
+
+    private static final String IT_SOLR_CONTAINER_IMAGE_NAME = "it.solr-container.image-name";
+    private static final String IT_SOLR_CONTAINER_ENABLED = "it.solr-container.enabled";
+    private static final String IT_SOLR_CONTAINER_COMMA_SEPARATED_ZK_HOST_PROPERTIES =
+            "it.solr-container.comma-separated.zk-host-properties";
+
+    @Override
+    public void postProcessEnvironment(
+            ConfigurableEnvironment environment, SpringApplication application) {
+        String containerEnabled =
+                System.getProperty(IT_SOLR_CONTAINER_ENABLED, String.valueOf(false));
+        if (containerEnabled.trim().isEmpty() || !containerEnabled.equalsIgnoreCase("true")) {
+            LOGGER.info(
+                    "Solr test container is not enabled. {}: {}",
+                    IT_SOLR_CONTAINER_ENABLED,
+                    containerEnabled);
+            return;
+        } else {
+            LOGGER.info(
+                    "Solr test container is enabled. {}: {}",
+                    IT_SOLR_CONTAINER_ENABLED,
+                    containerEnabled);
+        }
+
+        String containerImageName = System.getProperty(IT_SOLR_CONTAINER_IMAGE_NAME, "solr:8.11.3");
+
+        solr =
+                new SolrContainer(DockerImageName.parse(containerImageName))
+                        .withEnv(
+                                "SOLR_AUTHENTICATION_OPTS",
+                                "-Dbasicauth=" + SOLR_USER + ":" + SOLR_PASS)
+                        .withZookeeper(true);
+        solr.start();
+
+        solr.withCopyFileToContainer(
+                MountableFile.forClasspathResource("security.json"), "/tmp/security.json");
+        try {
+            solr.execInContainer(
+                    "/opt/solr/bin/solr",
+                    "zk",
+                    "cp",
+                    "/tmp/security.json",
+                    "zk:/security.json",
+                    "-z",
+                    getZkHost());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Properties properties = new Properties();
+
+        String commaSeparatedZkHostProperties =
+                System.getProperty(
+                        IT_SOLR_CONTAINER_COMMA_SEPARATED_ZK_HOST_PROPERTIES,
+                        "spring.data.solr.zkHost");
+        Arrays.stream(commaSeparatedZkHostProperties.split(","))
+                .forEach(it -> properties.put(it, getZkHost()));
+
+        System.setProperties(properties);
+    }
+
+    private @NotNull String getZkHost() {
+        return solr.getHost() + ":" + solr.getZookeeperPort();
+    }
+
+    @PreDestroy
+    public void stop() {
+        solr.stop();
+    }
+}
